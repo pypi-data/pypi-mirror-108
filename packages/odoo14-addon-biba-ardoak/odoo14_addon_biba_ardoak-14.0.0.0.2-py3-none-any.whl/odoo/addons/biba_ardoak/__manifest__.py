a576777678b10da1686a1941842faded1d15f654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:37 +0000

    Add package odoo14-addon-biba-ardoak/14.0.0.0.2/odoo14_addon_biba_ardoak-14.0.0.0.2-py3-none-any.whl

---
+++ packages/odoo14-addon-biba-ardoak/odoo14_addon_biba_ardoak-14.0.0.0.2-py3-none-any.whl/odoo/addons/biba_ardoak/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Odoo Biba Ardoak customizations",
+    'version': '14.0.0.0.2',
+    'depends': ['base', 'sale_stock'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': """
+    Odoo Biba Ardoak customizations.
+    """,
+    "license": "AGPL-3",
+    'data': [
+        "views/report_view.xml",
+    ],
+}
