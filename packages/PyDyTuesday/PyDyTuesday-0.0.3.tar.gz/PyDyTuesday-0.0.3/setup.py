90ad676f264d4cadeb1ec719968dd96e638d4c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:11 +0000

    Add package PyDyTuesday/0.0.3/PyDyTuesday-0.0.3.tar.gz

---
+++ packages/PyDyTuesday/PyDyTuesday-0.0.3.tar.gz/PyDyTuesday-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3' 
+DESCRIPTION = 'A Python port of the TidyTuesday Downloader'
+LONG_DESCRIPTION = 'A port of the tidytuesdayR package to download TidyTuesday datasets'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="PyDyTuesday", 
+        version=VERSION,
+        author="Andreas Varotsis",
+        author_email="andreas.varotsis@gmail.com",
+        description=DESCRIPTION,
+        long_description= open('README.rst').read(),
+        packages=find_packages(),
+        license='MIT',
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        keywords=['python', 'Tidy Tuesday', 'learning'],
+        classifiers= [
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
