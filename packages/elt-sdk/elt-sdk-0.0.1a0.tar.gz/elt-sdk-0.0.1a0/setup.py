e921bc2fa85ac764eb31a9786012cfacfd95993e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:51 +0000

    Add package elt-sdk/0.0.1a0/elt-sdk-0.0.1a0.tar.gz

---
+++ packages/elt-sdk/elt-sdk-0.0.1a0.tar.gz/elt-sdk-0.0.1a0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['elt_sdk']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'elt-sdk',
+    'version': '0.0.1a0',
+    'description': 'SDK under development',
+    'long_description': None,
+    'author': 'AJ Steers',
+    'author_email': 'aj@meltano.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
