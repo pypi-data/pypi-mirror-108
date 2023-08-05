05bf7674a7ff5bfdef40a2c861e26bc327a9b88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:24 +0000

    Add package MAST-U-DMS-GUI/0.1.2/MAST-U_DMS_GUI-0.1.2.tar.gz

---
+++ packages/MAST-U-DMS-GUI/MAST-U_DMS_GUI-0.1.2.tar.gz/MAST-U_DMS_GUI-0.1.2/README.md
@@ -0,0 +1,25 @@
+# Sigma Coding Python Library
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Setup](#setup)
+- [Usage](#usage)
+- [Support These Projects](#support-these-projects)
+
+## Overview
+
+## Setup
+
+Setup text 
+```console
+pip install *include the pip install path here* .
+```
+or if on Freia
+'''
+pip3 install ****** --user
+'''
+
+## Usage
+
+Describe using the loading in data stuff here somehow.
---
+++ packages/MAST-U-DMS-GUI/MAST-U_DMS_GUI-0.1.2.tar.gz/MAST-U_DMS_GUI-0.1.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+#pull from readme the information
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'MAST-U_DMS_GUI',
+
+    version = '0.1.2',
+
+    #Who to put as the author?
+    author = 'Gareth Williams',
+
+    author_email = 'gw878@york.ac.uk',
+
+    description = 'A GUI for spectral data analysis at MAST-U',
+
+    long_description = long_description,
+
+    long_description_content_type = 'text/markdown',
+
+    install_requires=[
+        'tkinter==',
+        'matplotlib==',
+        'pandas==asa',
+        'time==',
+        'spe2py==',
+        'numpy==',
+        'scipy==',
+    ],
+
+    packages=find_packages(
+        include=['MAST-U_DMS_GUI', 'MAST-U_DMS_GUI']
+
+    ),
+    include_package_data=True,
+
+    python_requires='>=3.7',
+
+
+)
+
+
