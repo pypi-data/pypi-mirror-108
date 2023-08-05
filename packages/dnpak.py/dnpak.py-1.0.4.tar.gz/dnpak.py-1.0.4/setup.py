59f9acd251ba0e5da435b59229d05af9072757c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:42 +0000

    Add package dnpak.py/1.0.4/dnpak.py-1.0.4.tar.gz

---
+++ packages/dnpak.py/dnpak.py-1.0.4.tar.gz/dnpak.py-1.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md") as file:
+    LONG_DESCRIPTION = file.read()
+
+VERSION = "1.0.4"
+DESCRIPTION = "A python package to manipulate DragonNest pak file"
+
+setup(
+      name="dnpak.py",
+      version=VERSION,
+      author="Muhammad Dony Mulya",
+      author_email="exlog@protonmail.com",
+      url="https://github.com/ExLog/dnpak-py",
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
+      license="WTFPL",
+      keywords=["dragonnest"],
+      packages=["dnpak"],
+      package_dir={"": "src"},
+      classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Intended Audience :: Developers",
+            "License :: Other/Proprietary License"
+      ]
+)
