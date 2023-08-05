b0ceecb1bcec9ee3c461f31dd3217de3ac34bba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:50 +0000

    Add package cloudrail-knowledge/0.0.27b3/cloudrail-knowledge-0.0.27b3.tar.gz

---
+++ packages/cloudrail-knowledge/cloudrail-knowledge-0.0.27b3.tar.gz/cloudrail-knowledge-0.0.27b3/cloudrail/knowledge/context/azure/azure_resources/databases/azure_sql_server.py
@@ -0,0 +1,28 @@
+from typing import Optional, List
+from cloudrail.knowledge.context.azure.azure_resource import AzureResource
+from cloudrail.knowledge.context.azure.constants.azure_resource_type import AzureResourceType
+
+
+class AzureSqlServer(AzureResource):
+
+    def __init__(self, server_name: str, public_network_access_enable: bool) -> None:
+        super().__init__(AzureResourceType.AZURERM_SQL_SERVER)
+        self.server_name: str = server_name
+        self.with_aliases(server_name)
+        self.public_network_access_enable: bool = public_network_access_enable
+
+    def get_keys(self) -> List[str]:
+        return [self.get_name()]
+
+    def get_name(self) -> str:
+        return self.server_name
+
+    def get_cloud_resource_url(self) -> Optional[str]:
+        pass
+
+    def get_friendly_name(self) -> str:
+        return self.get_name()
+
+    @property
+    def is_tagable(self) -> bool:
+        return False
