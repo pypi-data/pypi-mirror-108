b0b7fcbc4a68078eca8103839696455c67eb5b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:28 +0000

    Add package p-y-p-k-g/0.0.7/p_y_p_k_g-0.0.7-py3-none-any.whl

---
+++ packages/p-y-p-k-g/p_y_p_k_g-0.0.7-py3-none-any.whl/pypkg/__init__.py
@@ -0,0 +1,3 @@
+"""Python package template."""
+from pypkg.__version__ import __commit__ as commit
+from pypkg.__version__ import __version__ as version
---
+++ packages/p-y-p-k-g/p_y_p_k_g-0.0.7-py3-none-any.whl/pypkg/__version__.py
@@ -0,0 +1,3 @@
+"""Version file."""
+__version__ = '0.0.7'
+__commit__ = 'aea7e04c67844fe5fbb536653f7109d63808a8bf'
