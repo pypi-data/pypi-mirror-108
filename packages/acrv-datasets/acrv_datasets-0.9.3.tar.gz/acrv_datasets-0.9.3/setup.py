8c47d85478f33bf1c504863f938faa29451f0392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:22 +0000

    Add package acrv-datasets/0.9.3/acrv_datasets-0.9.3.tar.gz

---
+++ packages/acrv-datasets/acrv_datasets-0.9.3.tar.gz/acrv_datasets-0.9.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='acrv_datasets',
+      version='0.9.3',
+      author='Ben Talbot',
+      author_email='b.talbot@qut.edu.au',
+      url='https://github.com/best-of-acrv/acrv-datasets',
+      description='Datasets manager for the Best of ACRV',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      package_data={'acrv_datasets': ['*.yaml']},
+      install_requires=['colorama', 'pyyaml', 'requests', 'tqdm'],
+      classifiers=(
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ))
