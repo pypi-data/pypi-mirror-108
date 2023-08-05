44ff9b66fcbe5b39c5100a8a7fe9275bb1404186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:02 +0000

    Add package autoreduce-qp/22.0.0.dev0/autoreduce_qp-22.0.0.dev0.tar.gz

---
+++ packages/autoreduce-qp/autoreduce_qp-22.0.0.dev0.tar.gz/autoreduce_qp-22.0.0.dev0/setup.py
@@ -0,0 +1,23 @@
+# pylint:skip-file
+"""
+Wrapper for the functionality for various installation and project setup commands
+see:
+    `python setup.py help`
+for more details
+"""
+from setuptools import setup, find_packages
+
+setup(name="autoreduce_qp",
+      version="22.0.0.dev",
+      description="ISIS Autoreduction queue processor",
+      author="ISIS Autoreduction Team",
+      url="https://github.com/ISISScientificComputing/autoreduce/",
+      install_requires=[
+          "autoreduce_utils==0.1.3", "autoreduce_db==0.1.4", "chardet==3.0.4", "Django==3.2.2", "fire==0.4.0",
+          "plotly==4.14.3", "stomp.py==6.1.0"
+      ],
+      packages=find_packages(),
+      entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
+      classifiers=[
+          "Programming Language :: Python :: 3 :: Only",
+      ])
