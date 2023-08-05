aa9ead5d409fc8c5d88e59d14f92f0cdc6ee37be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:47 +0000

    Add package django-qr-toolkit-core/1.20/django_qr_toolkit_core-1.20-py3-none-any.whl

---
+++ packages/django-qr-toolkit-core/django_qr_toolkit_core-1.20-py3-none-any.whl/qrtoolkit_core/generator/zip.py
@@ -0,0 +1,39 @@
+import io
+import zipfile
+from django.utils.text import get_valid_filename
+from django.conf import settings
+
+from .qr import create_qr_code
+
+
+def generate_qr_zip(codes, form):
+    s = io.BytesIO()
+
+    zf = zipfile.ZipFile(s, mode='w', compression=zipfile.ZIP_DEFLATED)
+
+    for code in codes:
+        code_buffer = create_qr_code(code, form)
+        filename = create_filename(code, form)
+        zf.writestr(filename, code_buffer)
+
+    zf.close()
+
+    return s.getvalue()
+
+
+def create_filename(code, form):
+    filename_setting = form.cleaned_data['filename']
+    ext = form.cleaned_data["kind"]
+    name = f'{code.title}.{ext}'
+    if filename_setting == 'envtitle':
+        name = f'{settings.ENVIRONMENT}-{code.title}.{ext}'
+    if filename_setting == 'basic':
+        name = f'{code.basic_info[:200]}.{ext}'
+    if filename_setting == 'envbasic':
+        name = f'{settings.ENVIRONMENT}-{code.basic_info[:200]}.{ext}'
+    if filename_setting == 'uuid':
+        name = f'{code.short_uuid}.{ext}'
+    if filename_setting == 'envuuid':
+        name = f'{settings.ENVIRONMENT}-{code.short_uuid}.{ext}'
+
+    return get_valid_filename(name)
