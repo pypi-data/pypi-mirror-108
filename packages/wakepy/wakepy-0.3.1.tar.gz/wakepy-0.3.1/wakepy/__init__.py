f381d5ff12224f763db8ae3c7505f10aa814adbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:17 +0000

    Add package wakepy/0.3.1/wakepy-0.3.1.tar.gz

---
+++ packages/wakepy/wakepy-0.3.1.tar.gz/wakepy-0.3.1/wakepy/__init__.py
@@ -0,0 +1,58 @@
+"""This module provides 
+
+start() 
+- Function that starts a "keepawake". Keeps the computer
+  awake. Exit with Ctrl-C.
+
+set_keepawake()
+unset_keepawake()
+- The lower level functions that can be used in any script to
+   set or unset the keepawake.
+"""
+from functools import wraps
+import platform
+import time
+
+SYSTEM = platform.system().lower()
+
+if SYSTEM == "windows":
+    from ._win import set_keepawake, unset_keepawake
+elif SYSTEM == "linux":
+    from ._linux import set_keepawake, unset_keepawake
+elif SYSTEM == "darwin":
+    from ._darwin import set_keepawake, unset_keepawake
+else:
+    NotImplementedError(
+        f"wakepy has not yet a {SYSTEM} implementation. Pull requests welcome: https://github.com/np-8/wakepy"
+    )
+
+p = ["|", "/", "-", "\\"]
+
+
+def wait_until_keyboardinterrupt():
+    try:
+        while True:
+            for i in range(0, 4):
+                print("\r" + p[i] + r" [Press Ctrl+C to exit]", end="")
+                time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+
+def start(keep_screen_awake=False):
+    """
+    Start the keep-awake. During keep-awake, the CPU is not allowed to
+    go to sleep automatically until the CTRL+C is pressed.
+
+    Parameters
+    -----------
+    keep_screen_awake: bool
+        If True, keeps also the screen awake.
+    """
+    set_keepawake(keep_screen_awake=keep_screen_awake)
+    print(
+        "Started wakepy. Your computer will not sleep automatically (unless battery goes under critical level)"
+    )
+    wait_until_keyboardinterrupt()
+    print("Stopped wakepy. Your computer is allowed to sleep.")
+    unset_keepawake()
