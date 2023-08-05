30968c48e878ed70149440b9d2571e6a59d1b3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:58 +0000

    Add package EpicEndpoints/0.1.1/EpicEndpoints-0.1.1.tar.gz

---
+++ packages/EpicEndpoints/EpicEndpoints-0.1.1.tar.gz/EpicEndpoints-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+   name='EpicEndpoints',
+   version='0.1.1',
+   description=""" An easy way to access Epic Games\' endpoints without clutter. """,
+   long_description_content_type="text/markdown",
+   long_description=open('README.md').read(),
+   url='https://github.com/AtomicXYZ/EpicEndpoints',
+   author='AtomicXYZ',
+   packages=['EpicEndpoints'],
+   
+   license='LICENSE',
+   
+   install_requires=[
+       "requests",
+   ],
+)
