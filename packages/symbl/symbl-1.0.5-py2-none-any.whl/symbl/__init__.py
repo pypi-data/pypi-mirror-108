070a9d07094512d4de1e4e897c74d67ba64cf782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:34 +0000

    Add package symbl/1.0.5/symbl-1.0.5-py2-none-any.whl

---
+++ packages/symbl/symbl-1.0.5-py2-none-any.whl/symbl/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from symbl_rest import  JobsApi, ApiClient
+
+from symbl.async_api.Audio import Audio
+from symbl.async_api.Video import Video
+from symbl.async_api.Text import Text
+
+from symbl.Connection import Connection
+from symbl.streaming_api.StreamingConnection import StreamingConnection
+
+from symbl.conversations_api.ConversationsApi import ConversationsApi
+from symbl.telephony_api.TelephonyApi import TelephonyApi
+from symbl.streaming_api.StreamingApi import StreamingApi
+from symbl.jobs_api.Job import Job
+from symbl.jobs_api.JobStatus import JobStatus
+
+Audio = Audio()
+Video = Video()
+Text = Text()
+Conversations = ConversationsApi()
+Telephony = TelephonyApi()
+Streaming = StreamingApi()
