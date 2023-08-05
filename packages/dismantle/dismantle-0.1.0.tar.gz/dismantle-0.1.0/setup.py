5b68cfcdb117fa70947db56c1344720b8ad97252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:24 +0000

    Add package dismantle/0.1.0/dismantle-0.1.0.tar.gz

---
+++ packages/dismantle/dismantle-0.1.0.tar.gz/dismantle-0.1.0/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['dismantle',
+ 'dismantle.extension',
+ 'dismantle.index',
+ 'dismantle.package',
+ 'dismantle.plugin']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.25.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['area28 = dismantle.cli:main']}
+
+setup_kwargs = {
+    'name': 'dismantle',
+    'version': '0.1.0',
+    'description': 'Python package / plugin / extension manager',
+    'long_description': '# Dismantle\n\nPython package / plugin / extension manager.\n',
+    'author': 'Gary Stidston-Broadbent',
+    'author_email': 'dismantle@garysb.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://garysb.com',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
