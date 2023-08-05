7b6402f2af2878f8bf4d442cdf864b7cc1a5ae33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:59 +0000

    Add package meshkernel/0.9.2/meshkernel-0.9.2-py3-none-win_amd64.whl

---
+++ packages/meshkernel/meshkernel-0.9.2-py3-none-win_amd64.whl/meshkernel-0.9.2.data/purelib/meshkernel/__init__.py
@@ -0,0 +1,20 @@
+# If you change these imports,
+# do not forget to sync the docs at "docs/api"
+from meshkernel.errors import InputError, MeshKernelError
+from meshkernel.factories import Mesh2dFactory
+from meshkernel.meshkernel import MeshKernel
+from meshkernel.py_structures import (
+    AveragingMethod,
+    Contacts,
+    DeleteMeshOption,
+    GeometryList,
+    Mesh1d,
+    Mesh2d,
+    Mesh2dLocation,
+    MeshRefinementParameters,
+    OrthogonalizationParameters,
+    ProjectToLandBoundaryOption,
+    RefinementType,
+)
+
+__version__ = "0.9.2"
