be99771e97deb88e03aab1d1fa273812290eb453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:11 +0000

    Add package fw-gear-dicom-fixer/0.3.3/fw_gear_dicom_fixer-0.3.3-py3-none-any.whl

---
+++ packages/fw-gear-dicom-fixer/fw_gear_dicom_fixer-0.3.3-py3-none-any.whl/fw_gear_dicom_fixer/main.py
@@ -0,0 +1,50 @@
+"""Main module."""
+
+import functools
+import logging
+import tempfile
+import typing as t
+import zipfile
+from contextlib import contextmanager
+from pathlib import Path
+
+from fw_file.dicom import DICOMCollection
+from pydicom import config as pydicom_config
+from pydicom.datadict import keyword_for_tag
+
+from .callbacks import decode_dcm, setup_callbacks
+from .metadata import add_missing_uid, update_modified_dicom_info
+
+log = logging.getLogger(__name__)
+
+
+def run(dicom: t.Dict, out_dir: Path) -> t.List[t.Dict[str, t.List[str]]]:
+    dicom_path = Path(dicom["location"]["path"]).resolve()
+    events = []
+    with setup_callbacks():
+        if zipfile.is_zipfile(str(dicom_path)):
+            dcms = DICOMCollection.from_zip(dicom_path, track=True)
+        else:
+            dcms = DICOMCollection(dicom_path, track=True)
+        for dcm in dcms:
+            decode_dcm(dcm)
+            dcm.tracker.trim()
+            for element in dcm.tracker.data_elements:
+                if element.events:
+                    tagname = str(element.tag).replace(",", "")
+                    kw = keyword_for_tag(element.tag)
+                    if kw:
+                        tagname = kw
+                    events.append({tagname: [str(ev) for ev in element.events]})
+            update_modified_dicom_info(dcm)
+
+    added_uid = add_missing_uid(dcms)
+
+    if (len(events) > 0 and all([len(ev) > 0 for ev in events])) or added_uid:
+        log.info(f"Writing output to {out_dir / dicom_path.name}")
+        if len(dcms) > 1:
+            dcms.to_zip(out_dir / dicom_path.name)
+        else:
+            dcms[0].save(out_dir / dicom_path.name)
+
+    return events
