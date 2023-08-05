471ab47fd9a9a4ce59cafabe82d011258c8c46b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:02 +0000

    Add package api2db/1.0.1/api2db-1.0.1.tar.gz

---
+++ packages/api2db/api2db-1.0.1.tar.gz/api2db-1.0.1/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://readthedocs.org/projects/api2db/badge/?version=latest
+
+
+``pip install api2db``
+
+Read The Docs
+-------------
+
+https://api2db.readthedocs.io/en/latest/
---
+++ packages/api2db/api2db-1.0.1.tar.gz/api2db-1.0.1/src/api2db/stream/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
++-----------------+--------------+
+| Original Author | Tristen Harr |
++-----------------+--------------+
+| Creation Date   | 04/27/2021   |
++-----------------+--------------+
+| Revisions       | None         |
++-----------------+--------------+
+"""
+from .stream2local import Stream2Local
+from .stream2bigquery import Stream2Bigquery
+from .stream2sql import Stream2Sql
+from .stream2omnisci import Stream2Omnisci
