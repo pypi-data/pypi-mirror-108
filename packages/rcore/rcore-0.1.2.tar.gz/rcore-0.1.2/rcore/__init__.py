acaeca5ce308775520e3beb4bc0833c034a6a428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:36 +0000

    Add package rcore/0.1.2/rcore-0.1.2.tar.gz

---
+++ packages/rcore/rcore-0.1.2.tar.gz/rcore-0.1.2/rcore/__init__.py
@@ -0,0 +1,8 @@
+""" Ядро для приложений python. Сборник утилит. """
+
+import rlogging
+
+rlogging.registration_logger('mainLogger', rlogging.loggers.Logger)
+
+# Alpha release
+__version__ = '0.1.2'
