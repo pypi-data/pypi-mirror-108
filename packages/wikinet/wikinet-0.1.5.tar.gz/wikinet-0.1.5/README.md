345a0525314d78d083e990ab15ea7de9af92938e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:50 +0000

    Add package wikinet/0.1.5/wikinet-0.1.5.tar.gz

---
+++ packages/wikinet/wikinet-0.1.5.tar.gz/wikinet-0.1.5/README.md
@@ -0,0 +1,11 @@
+# WikiNet
+This repository contains code for analysis used in [Ju et al. (2020)](https://arxiv.org/abs/2010.08381).
+
+See [documentation](https://wikinet.readthedocs.io/en/latest/) to get started.
+
+## Other options
+* `gensim` added a [`WikiCorpus`](https://radimrehurek.com/gensim/corpora/wikicorpus.html#module-gensim.corpora.wikicorpus) class that parses through Wikipedia dumps.
+
+## To-do
+
+* fix dependency issue with `dionysus` and `python`
---
+++ packages/wikinet/wikinet-0.1.5.tar.gz/wikinet-0.1.5/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wikinet",
+    version="0.1.5",
+    author="Harang Ju",
+    author_email="harangju@gmail.com",
+    description="Network of wikipedia articles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harangju/wikinet",
+    project_urls={
+        "Bug Tracker": "https://github.com/harangju/wikinet/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["wikinet"],
+    python_requires=">=3.7",
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas>1.0.0',
+        'networkx',
+        'gensim',
+        'python-Levenshtein', # gensim warning
+        'bctpy>=0.5.2',
+        'mwparserfromhell',
+        'sklearn',
+        'dionysus<=2.0.7', # weird dependency issue with python>3.7
+        'cpnet<=0.0.6',
+        'pytest'
+    ]
+)
