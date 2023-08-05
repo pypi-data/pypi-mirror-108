edb9f126e472cabd92562b105a91dde725654b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:00 +0000

    Add package sharestats-item-editor/0.1.22/sharestats-item-editor-0.1.22.tar.gz

---
+++ packages/sharestats-item-editor/sharestats-item-editor-0.1.22.tar.gz/sharestats-item-editor-0.1.22/sharestats_item_editor/__init__.py
@@ -0,0 +1,15 @@
+"""
+This module contains all modification of the item editor for the stats share
+projects.
+
+(c) O. Lindemann
+"""
+
+__author__ = "Oliver Lindemann"
+
+from . import item_editor
+#item_editor.__version__ += "-2"
+item_editor.APPNAME = "ShareStats Item Editor"
+item_editor.WEBSITE = "https://github.com/essb-mt-section/sharestats-item-editor"
+
+__version__ = item_editor.__version__
