c46ee5a3c4460d2b11e9a3ce4b5c586b87b03ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:07 +0000

    Add package pyxllib/0.0.79/pyxllib-0.0.79.tar.gz

---
+++ packages/pyxllib/pyxllib-0.0.79.tar.gz/pyxllib-0.0.79/pyxllib/debug/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author : 陈坤泽
+# @Email  : 877362867@qq.com
+# @Date   : 2020/06/02 15:48
+
+"""调试功能，通用底层功能
+
+文中所有前缀4个下划线_的是模块划分标记，且
+    前4个下划线_是一级结构
+    前8个下划线_是二级结构
+
+相关文档： https://blog.csdn.net/code4101/article/details/83269101
+"""
