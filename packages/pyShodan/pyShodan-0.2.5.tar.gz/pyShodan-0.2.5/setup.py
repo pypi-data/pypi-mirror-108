f209c5cd42b2280c13201613492f32a52151ce8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:27 +0000

    Add package pyShodan/0.2.5/pyShodan-0.2.5.tar.gz

---
+++ packages/pyShodan/pyShodan-0.2.5.tar.gz/pyShodan-0.2.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = str(fh.read())
+
+setuptools.setup(
+    name="pyShodan",
+    version="0.2.5",
+    author="Shane Scott",
+    author_email="sscott@gvit.com",
+    description="Python library for querying the Shodan API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GoVanguard/pyShodan",
+    packages=['pyShodan'],
+    install_requires=["shodan", "IPy"],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+    ),
+)
