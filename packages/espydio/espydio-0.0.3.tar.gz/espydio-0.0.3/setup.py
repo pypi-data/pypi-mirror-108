95e10f1b062c22a45adeba3f86ec20da352ac3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:34 +0000

    Add package espydio/0.0.3/espydio-0.0.3.tar.gz

---
+++ packages/espydio/espydio-0.0.3.tar.gz/espydio-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='espydio',
+      version='0.0.3',
+      description='A command line utility built using python to automate audio file conversions, thereby assisting audio playing on ESP32',
+      long_description=long_description,
+      long_description_content_type = 'text/markdown',
+      url='https://github.com/namanPuri/espydio',
+      author='Naman Puri',
+      author_email='namanpuri1712@gmail.com',
+      include_package_data=True,
+      keywords='esp32 sound conversions',
+      license='BSD-3-Clause',
+      entry_points ={
+            'console_scripts': [
+                'espydio = espydio.script_espydio:main'
+            ]
+        },
+      packages=find_packages(),
+      install_requires=['Flask==1.1.2','sox==1.4.1','requests==2.25.1'],
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+      ],
+      zip_safe=False)
