f5a95f3ed93796e74223b721717a2f6de10d0b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:30 +0000

    Add package distributions-awesome-package/0.1/distributions_awesome_package-0.1.tar.gz

---
+++ packages/distributions-awesome-package/distributions_awesome_package-0.1.tar.gz/distributions_awesome_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_awesome_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_awesome_package'],
+      zip_safe=False)
