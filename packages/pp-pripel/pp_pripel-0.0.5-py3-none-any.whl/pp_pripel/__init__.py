cc7ebd5d0d6695fbc7299dc8d7672c64b97583d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:43 +0000

    Add package pp-pripel/0.0.5/pp_pripel-0.0.5-py3-none-any.whl

---
+++ packages/pp-pripel/pp_pripel-0.0.5-py3-none-any.whl/pp_pripel/__init__.py
@@ -0,0 +1,9 @@
+from pp_pripel import pripel,tracematcher,trace_variant_query,levenshtein,attributeAnonymizier
+
+__name__ = 'pp_pripel'
+__version__ = '0.0.5'
+__doc__ = "Privacy-preserving Event Log Publishing with contextual Information"
+__author__ = 'Stephan Fahrenkrog-Petersen'
+__author_email__ = 'fahrenks@hu-berlin.de'
+__maintainer__ = 'Stephan Fahrenkrog-Petersen'
+__maintainer_email__ = "fahrenks@hu-berlin.de"
