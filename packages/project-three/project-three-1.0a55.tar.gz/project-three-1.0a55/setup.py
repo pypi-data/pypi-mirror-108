728e322c577a686bbd74296c825a3f3f469d9dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:07 +0000

    Add package project-three/1.0a55/project-three-1.0a55.tar.gz

---
+++ packages/project-three/project-three-1.0a55.tar.gz/project-three-1.0a55/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='project-three',
+    version='1.0a55',
+    packages=['v2'],
+    url='https://google.com',
+    author='ML',
+    author_email='',
+    install_requires=[
+        "numpy==1.19",
+    ]
+)
