02008ff0f2afe90155a0b7810443ea4d0847c844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:11 +0000

    Add package dimppy/0.0.0.1/dimppy-0.0.0.1.tar.gz

---
+++ packages/dimppy/dimppy-0.0.0.1.tar.gz/dimppy-0.0.0.1/README.md
@@ -0,0 +1,10 @@
+# Data Structure and in Algorithms in Python 
+
+A Python package of data structure codes
+
+## Usage
+
+This package contains ready to use Data Structure and Algorithm implementations. The primary focus is provide pre-coded functions that can be used on the go for various Data Structures and Algorithm problems.
+
+-----------------------------------
+Note: This package is developed and maintained by a student. Please don't mind the mistakes :)
---
+++ packages/dimppy/dimppy-0.0.0.1.tar.gz/dimppy-0.0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'dimppy', 
+    version='0.0.0.1',
+    author='Armaan Chauhan',
+    author_email='armaanchauhan268@gmail.com',
+    description='Data Structures and Algorithms implementation in Python', 
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Armaan-268/DSA_in_Python',
+    license="MIT",
+    classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Education',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+    ], 
+    keywords='datastructures,algorithms',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['']
+)
