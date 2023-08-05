fb1b43d9b6c610757e379150ac5040000360eff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:37 +0000

    Add package cerebunit/0.0.1/cerebunit-0.0.1.tar.gz

---
+++ packages/cerebunit/cerebunit-0.0.1.tar.gz/cerebunit-0.0.1/cerebunit/capabilities/__init__.py
@@ -0,0 +1,4 @@
+# ~/cerebunit/cerebunit/capabilities/__init__.py
+#from cerebunit.capabilities import cells
+#from . import microcircuit
+#from . import network
---
+++ packages/cerebunit/cerebunit-0.0.1.tar.gz/cerebunit-0.0.1/cerebunit/validation_tests/__init__.py
@@ -0,0 +1,4 @@
+# ~/cerebunit/cerebunit/validation_tests/__init__.py
+#from cerebunit.validation_tests import cells
+#from . import microcircuit
+#from . import network
