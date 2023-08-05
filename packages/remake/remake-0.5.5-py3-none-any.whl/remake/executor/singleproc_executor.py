31e0c82240608b07d6bbeec8c86bc6b9625aa34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:42 +0000

    Add package remake/0.5.5/remake-0.5.5-py3-none-any.whl

---
+++ packages/remake/remake-0.5.5-py3-none-any.whl/remake/executor/singleproc_executor.py
@@ -0,0 +1,24 @@
+from remake.executor.base_executor import Executor
+
+
+class SingleprocExecutor(Executor):
+    def __init__(self, task_ctrl):
+        super().__init__(task_ctrl)
+        self.completed_task = None
+
+    def can_accept_task(self):
+        return self.completed_task is None
+
+    def enqueue_task(self, task):
+        task.run(force=True)
+        self.completed_task = task
+
+    def get_completed_task(self):
+        completed_task = self.completed_task
+        assert completed_task is not None, 'completed_task is None'
+        self.completed_task = None
+        return completed_task
+
+    def has_finished(self):
+        return not self.completed_task
+
