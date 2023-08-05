7a13b9bb9cb4158b0f4a85c6bf1c7227364bff60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:48 +0000

    Add package worklab/1.7.0/worklab-1.7.0.tar.gz

---
+++ packages/worklab/worklab-1.7.0.tar.gz/worklab-1.7.0/worklab/__init__.py
@@ -0,0 +1,13 @@
+"""Worklab data processing package"""
+
+__version__ = "1.7.0"
+
+__all__ = ['com', 'kin', 'move', 'physio', 'utils', 'plots', 'imu', 'ana']
+
+from . import com
+from . import kin
+from . import move
+from . import physio
+from . import utils
+from . import plots
+from . import imu
