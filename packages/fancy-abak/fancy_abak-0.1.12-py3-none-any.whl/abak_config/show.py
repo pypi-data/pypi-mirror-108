4cdb67ca88a82eb6f41d5e42bca494f874b6578a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:06 +0000

    Add package fancy-abak/0.1.12/fancy_abak-0.1.12-py3-none-any.whl

---
+++ packages/fancy-abak/fancy_abak-0.1.12-py3-none-any.whl/abak_config/show.py
@@ -0,0 +1,20 @@
+import click
+from abak_shared_functions import get_config
+
+@click.command(name="show")
+@click.argument('key', required=False)
+@click.pass_context
+def show_config(ctx, key):
+    '''
+    Shows the current configuration file for JIRA communication and default values
+
+    Args:
+        key (str): the key to show
+    '''
+    configuration = get_config()
+    configuration['token'] = "**********"
+    del configuration['headers']
+    if not key:
+        print(configuration)
+    elif configuration.get(key):
+        print(configuration[key])
