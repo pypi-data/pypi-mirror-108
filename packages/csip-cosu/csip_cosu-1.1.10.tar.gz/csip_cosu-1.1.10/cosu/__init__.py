12a89086e2b3cf8f03c6d093bbb24c5bd8987b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:50 +0000

    Add package csip-cosu/1.1.10/csip_cosu-1.1.10.tar.gz

---
+++ packages/csip-cosu/csip_cosu-1.1.10.tar.gz/csip_cosu-1.1.10/cosu/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.1.10'
+__built__ = '1.1.10 0141a85d2181e875b22805952beec4b10549ab03 2021-06-01 14:54 -0600 od'
+__author__ = 'Olaf David'
+__email__ = 'odavid@colostate.edu'
+
+from .pso import pso
+__all__ = ["pso"]
+
