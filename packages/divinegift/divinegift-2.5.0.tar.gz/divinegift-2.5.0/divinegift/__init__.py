ab7ae7b14983c52cb472b0a31b0c4fbf677de0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package divinegift/2.5.0/divinegift-2.5.0.tar.gz

---
+++ packages/divinegift/divinegift-2.5.0.tar.gz/divinegift-2.5.0/divinegift/__init__.py
@@ -0,0 +1,21 @@
+name = "DivineGift"
+
+version = '2.5.0'
+
+__all__ = [
+    "main",
+    "logger",
+    "banner",
+    "config",
+    "sender",
+    "db",
+    "translit",
+    "excel",
+    "templator",
+    "kafka_client",
+    "cipher",
+    "zabbix_agent",
+    "kthreads",
+    "app",
+    "aims",
+]
