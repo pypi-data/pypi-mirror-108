4073c6ace5c01162f9cffe2b9aca5bd99ab99eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:51 +0000

    Add package abitfrosty/0.1/abitfrosty-0.1.tar.gz

---
+++ packages/abitfrosty/abitfrosty-0.1.tar.gz/abitfrosty-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='abitfrosty',
+      version='0.1',
+      description='Gaussian and binomial distributions. Test package.',
+      packages=['abitfrosty'],
+      author='abitfrosty',
+      author_email='abitfrosty@gmail.com',
+      zip_safe=False)
