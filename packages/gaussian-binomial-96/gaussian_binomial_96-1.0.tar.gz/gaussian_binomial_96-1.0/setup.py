edaec3589874c41feb61ce40f6b350ef0cd45ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:56 +0000

    Add package gaussian-binomial-96/1.0/gaussian_binomial_96-1.0.tar.gz

---
+++ packages/gaussian-binomial-96/gaussian_binomial_96-1.0.tar.gz/gaussian_binomial_96-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_96',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['gaussian_binomial_96'],
+      author = 'Paulo Akira',
+      author_email = 'pauloakira.simul@gmail.com',
+      zip_safe=False)
