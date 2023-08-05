6fab76543bbe41c649cc9463f0113b21af8662eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:49 +0000

    Add package micro5125a/0.1.3/micro5125a-0.1.3-py3-none-any.whl

---
+++ packages/micro5125a/micro5125a-0.1.3-py3-none-any.whl/micro5125a/version.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+""" micro5125a/version.py """
+__version__ = "0.1.3"
+
+# 0.1.3 (01/06/2021): Add phase rate choice in continous acquisition mode.
+# 0.1.2 (05/05/2021): Use of OrderedDict instead of dict to correct bug with
+#                     Python version < 3.7.
+# 0.1.1 (03/05/2021): Correct parsing problem of adev outpouts when length of
+#                     adev value and adev noise floor is different.
+#                     Also correct display problem of argument parser.
+# 0.1.0 (16/10/2019): First version.
