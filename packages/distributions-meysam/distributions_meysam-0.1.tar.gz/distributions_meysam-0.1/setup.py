74da17b1bcf81d1a82d04abd1825834aac532293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:07 +0000

    Add package distributions-meysam/0.1/distributions_meysam-0.1.tar.gz

---
+++ packages/distributions-meysam/distributions_meysam-0.1.tar.gz/distributions_meysam-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_meysam',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_meysam'],
+      author='meysam hadigheh',
+      author_email='hadigheh.meysam@gmail.com',
+      zip_safe=False)
