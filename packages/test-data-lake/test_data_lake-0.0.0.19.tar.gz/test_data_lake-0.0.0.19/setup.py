22e89b8315cfc43081042f9142a59b7fc709f130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:35 +0000

    Add package test-data-lake/0.0.0.19/test_data_lake-0.0.0.19.tar.gz

---
+++ packages/test-data-lake/test_data_lake-0.0.0.19.tar.gz/test_data_lake-0.0.0.19/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+from setuptools import setup
+
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
+
+setup(name='test_data_lake',
+      version='0.0.0.19',
+      description='processes Datalake to Bronze-Silver_Gold',
+      url='https://upload.pypi.org/test_alex',
+      author='Imparnert',
+      author_email='alex-12-04@hotmail.com',
+      license='MIT',
+      packages=['delta_lake', 'delta_lake/test'],
+      # entry_points={'console_scripts': ['Package = Package.__main__:main'], },
+      # install_requires=required,
+      classifiers=["Programming Language :: Python :: 3"],
+      python_requires='>=3',
+      zip_safe=False,
+      # scripts=[
+      #           'requirements.txt',
+      #           'README.md',
+      #          ],
+      long_description=open('README.md').read(),
+      # include_package_data=True,
+      )
