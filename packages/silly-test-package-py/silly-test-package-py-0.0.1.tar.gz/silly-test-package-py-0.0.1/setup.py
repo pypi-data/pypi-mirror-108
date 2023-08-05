8e7681cae64e024f3c2776160f2ae11da415431c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:32 +0000

    Add package silly-test-package-py/0.0.1/silly-test-package-py-0.0.1.tar.gz

---
+++ packages/silly-test-package-py/silly-test-package-py-0.0.1.tar.gz/silly-test-package-py-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'test package'
+LONG_DESCRIPTION = 'test package long description.'
+
+# Setting up
+setup(
+    name="silly-test-package-py",
+    version=VERSION,
+    author="Andrew King",
+    author_email="<andrewking1597@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[], # list dependencies by pip install name
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
