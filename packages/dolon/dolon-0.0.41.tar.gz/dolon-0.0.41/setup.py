e74c9de75fa526a487b0a11239104c432af9f825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:08 +0000

    Add package dolon/0.0.41/dolon-0.0.41.tar.gz

---
+++ packages/dolon/dolon-0.0.41.tar.gz/dolon-0.0.41/setup.py
@@ -0,0 +1,21 @@
+"""Builds the library.
+
+To upload to cheese:
+
+sudo pip3 install twine
+sudo python3 setup.py sdist bdist_wheel
+twine upload  --skip-existing dist/* --verbose
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="dolon",
+    description="A performance tracer application.",
+    version='0.0.41',
+    packages=find_packages(),
+    install_requires=[
+        "asyncpg",
+        "psutil"
+    ],
+)
