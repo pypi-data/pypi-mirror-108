f131022ffc343a839f382092d580c0b02c20aa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:06 +0000

    Add package logger-es-handler/0.1.2/logger-es-handler-0.1.2.tar.gz

---
+++ packages/logger-es-handler/logger-es-handler-0.1.2.tar.gz/logger-es-handler-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['logger_es_handler']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['elasticsearch>=7.13.0,<8.0.0', 'requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'logger-es-handler',
+    'version': '0.1.2',
+    'description': 'ES handler for logging module ',
+    'long_description': None,
+    'author': 'Gustavo Freitas',
+    'author_email': 'gustavo@gmf-tech.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
