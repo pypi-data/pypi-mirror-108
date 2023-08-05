d4a12ac6c66bcc1bf8a54cad8c9777a2656fa5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:12 +0000

    Add package dbnd-airflow/0.41.4/dbnd-airflow-0.41.4.tar.gz

---
+++ packages/dbnd-airflow/dbnd-airflow-0.41.4.tar.gz/dbnd-airflow-0.41.4/src/dbnd_airflow/constants.py
@@ -0,0 +1,11 @@
+from distutils.version import LooseVersion
+
+import airflow
+
+
+AIRFLOW_VERSION_2 = LooseVersion(airflow.version.version) >= LooseVersion("2.0.0")
+AIRFLOW_VERSION_1 = LooseVersion(airflow.version.version) < LooseVersion("2.0.0")
+
+AIRFLOW_ABOVE_13 = LooseVersion(airflow.version.version) > LooseVersion("1.10.13")
+AIRFLOW_ABOVE_10 = LooseVersion(airflow.version.version) > LooseVersion("1.10.10")
+AIRFLOW_ABOVE_9 = LooseVersion(airflow.version.version) > LooseVersion("1.10.9")
