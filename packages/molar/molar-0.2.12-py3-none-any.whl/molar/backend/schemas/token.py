cd3b47e0953e6735d21389c6692e4e3d0314c2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:08 +0000

    Add package molar/0.2.12/molar-0.2.12-py3-none-any.whl

---
+++ packages/molar/molar-0.2.12-py3-none-any.whl/molar/backend/schemas/token.py
@@ -0,0 +1,18 @@
+# std
+from typing import Optional
+
+# external
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenVersionControl(Token):
+    user_header: str
+
+
+class TokenPayload(BaseModel):
+    sub: Optional[int] = None
