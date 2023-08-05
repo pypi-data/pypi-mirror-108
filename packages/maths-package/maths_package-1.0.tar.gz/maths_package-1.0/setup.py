b410cb47157bd6276efd10d266a481f54769e933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:17 +0000

    Add package maths-package/1.0/maths_package-1.0.tar.gz

---
+++ packages/maths-package/maths_package-1.0.tar.gz/maths_package-1.0/setup.py
@@ -0,0 +1,14 @@
+# TODO: Fill out this file with information about your package
+from setuptools import setup
+
+setup(name = 'maths_package',
+      version = '1.0',
+      description = 'Maths calculator package',
+      packages = ['maths_package'],
+      author = 'Darren Gidado',
+      author_email = 'myemail@something.com',
+    zip_safe=False)
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
