60f4e57206206e1da6d7875f0b077eb100856f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:30 +0000

    Add package lfview-api-client/0.1.2/lfview_api_client-0.1.2-py3-none-any.whl

---
+++ packages/lfview-api-client/lfview_api_client-0.1.2-py3-none-any.whl/lfview/client/convert/omf.py
@@ -0,0 +1,38 @@
+"""Function for converting OMF files to Views"""
+from __future__ import absolute_import
+
+import omf
+import steno3d
+from .steno3d import steno3d_to_view
+
+
+def omf_to_view(omf_file):
+    """Translate an OMF file into a View
+
+    Input:
+    **omf_file** - Valid OMF file or instance of OMF project; see
+    :class:`omf.base.Project`
+    """
+    steno3d_project = steno3d.Project.from_omf(omf_file)
+    view = steno3d_to_view(steno3d_project)
+    return view
+
+
+def view_to_omf(view, filename='view.omf'):
+    """Save a View as an OMF file
+
+    All object types, data/texture types, and legends are supported. Only
+    custom colormaps are unsupported due to limitations of OMF v1.
+
+    Input:
+    **view** - Valid View instance; see
+    :class:`lfview.resources.manifests.manifests.View
+    **filename** - Path and filename for output OMF file (default: 'view.omf')`
+    """
+
+    omf_project = omf.Project(
+        name=view.name or '',
+        description=view.description or '',
+        elements=[elem.to_omf() for elem in view.elements],
+    )
+    omf.OMFWriter(omf_project, filename)
