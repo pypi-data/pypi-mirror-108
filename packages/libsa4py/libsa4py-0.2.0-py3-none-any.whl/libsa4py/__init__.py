e0009c260d412f108f1ac9c8347d831ccab5842d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:47 +0000

    Add package libsa4py/0.2.0/libsa4py-0.2.0-py3-none-any.whl

---
+++ packages/libsa4py/libsa4py-0.2.0-py3-none-any.whl/libsa4py/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "0.2.0"
+
+# Package constants
+DEV_TYPE_ANNOT = 'D'
+INF_TYPE_ANNOT = 'I'
+UNK_TYPE_ANNOT = 'U'
+
+PY_TYPING_MOD = {'AbstractSet', 'Any', 'AnyStr', 'AsyncContextManager', 'AsyncGenerator', 'AsyncIterable',
+                 'AsyncIterator', 'Awaitable', 'BinaryIO', 'ByteString', 'CT_co', 'Callable', 'ChainMap', 'ClassVar',
+                 'Collection', 'Container', 'ContextManager', 'Coroutine', 'Counter', 'DefaultDict', 'Deque', 'Dict',
+                 'ForwardRef', 'FrozenSet', 'Generator', 'Generic', 'Hashable', 'IO', 'ItemsView', 'Iterable',
+                 'Iterator', 'KT', 'KeysView', 'List', 'Mapping', 'MappingView', 'Match', 'MethodDescriptorType',
+                 'MethodWrapperType', 'MutableMapping', 'MutableSequence', 'MutableSet', 'NamedTuple', 'NamedTupleMeta',
+                 'NewType', 'NoReturn', 'Optional', 'OrderedDict', 'Pattern', 'Reversible', 'Sequence', 'Set', 'Sized',
+                 'SupportsAbs', 'SupportsBytes', 'SupportsComplex', 'SupportsFloat', 'SupportsInt', 'SupportsRound',
+                 'TYPE_CHECKING', 'T_co', 'T_contra', 'Text', 'TextIO', 'Tuple', 'Type', 'TypeVar', 'Union', 'VT',
+                 'VT_co', 'V_co', 'ValuesView', 'WrapperDescriptorType', '_Final', '_GenericAlias', '_Immutable',
+                 '_Protocol', '_ProtocolMeta', '_SpecialForm', '_TypingEllipsis', '_TypingEmpty',
+                 '_VariadicGenericAlias'}
+
+PY_COLLECTION_MOD = {'ChainMap', 'Counter', 'OrderedDict', 'UserDict', 'UserList', 'UserString', '_Link',
+                     '_OrderedDictItemsView', '_OrderedDictKeysView', '_OrderedDictValuesView', 'defaultdict',
+                     'deque', 'namedtuple'} - PY_TYPING_MOD
+
+
+
