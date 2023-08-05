7961e111f6ac42d77cfe02af67cef3d7e82eb493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:22 +0000

    Add package mypy-boto3-pinpoint-sms-voice/1.17.85/mypy-boto3-pinpoint-sms-voice-1.17.85.tar.gz

---
+++ packages/mypy-boto3-pinpoint-sms-voice/mypy-boto3-pinpoint-sms-voice-1.17.85.tar.gz/mypy-boto3-pinpoint-sms-voice-1.17.85/mypy_boto3_pinpoint_sms_voice/literals.py
@@ -0,0 +1,27 @@
+"""
+Type annotations for pinpoint-sms-voice service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_pinpoint_sms_voice.literals import EventTypeType
+
+    data: EventTypeType = "ANSWERED"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("EventTypeType",)
+
+
+EventTypeType = Literal[
+    "ANSWERED", "BUSY", "COMPLETED_CALL", "FAILED", "INITIATED_CALL", "NO_ANSWER", "RINGING"
+]
