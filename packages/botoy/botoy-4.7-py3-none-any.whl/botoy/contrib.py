e55cec21ca26382ebfd21a7bf7561348c29a0e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:35 +0000

    Add package botoy/4.7/botoy-4.7-py3-none-any.whl

---
+++ packages/botoy/botoy-4.7-py3-none-any.whl/botoy/contrib.py
@@ -0,0 +1,52 @@
+import base64
+import inspect
+import os
+import re
+from pathlib import Path
+
+
+def file_to_base64(path):
+    """获取文件base64编码"""
+    with open(path, "rb") as f:
+        content = f.read()
+    return base64.b64encode(content).decode()
+
+
+def check_schema(url: str) -> str:
+    url = url.strip("/")
+    if not re.findall(r"(http://|https://)", url):
+        return "http://" + url
+    return url
+
+
+def get_cache_dir(dir_name: str) -> Path:
+    """获取缓存目录
+    :param dir_name: the cache directory's name.
+    :return: A Path object of the cache directory
+    """
+    # 确定主缓存目录
+    for i in os.listdir(Path(".")):
+        if i in ["botoy.json", "REMOVED_PLUGINS", "bot.py", "plugins"]:
+            main_dir = Path(".")
+            break
+    else:
+        cf = inspect.currentframe()
+        bf = cf.f_back  # type:ignore
+        file = bf.f_globals["__file__"]  # type:ignore
+        dir_ = Path(file).absolute()
+        while "plugins" in str(dir_):
+            dir_ = dir_.parent
+            if dir_.name == "plugins":
+                main_dir = dir_.parent
+                break
+        else:
+            main_dir = Path(".")
+
+    cache_dir = main_dir / "botoy-cache"
+    if not cache_dir.exists():
+        cache_dir.mkdir()
+
+    this_cache_dir = cache_dir / dir_name
+    if not this_cache_dir.exists():
+        os.makedirs(this_cache_dir)
+    return this_cache_dir
