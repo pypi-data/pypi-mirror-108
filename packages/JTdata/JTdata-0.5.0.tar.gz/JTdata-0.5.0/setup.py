b5ded883e9a4c630cd19b7d910533a7a6eb77fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:52 +0000

    Add package JTdata/0.5.0/JTdata-0.5.0.tar.gz

---
+++ packages/JTdata/JTdata-0.5.0.tar.gz/JTdata-0.5.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='JTdata',
+      version='0.5.0',
+      packages=['DataLoader'],
+      maintainer='wcc',
+      maintainer_email='wangcc@csc.com.cn',
+      url="https://github.com/jimmy")
