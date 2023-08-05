24ad791961a0ee6c55afd189ee781febcb1ec7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:22 +0000

    Add package rl-toolkit/3.0.1/rl-toolkit-3.0.1.tar.gz

---
+++ packages/rl-toolkit/rl-toolkit-3.0.1.tar.gz/rl-toolkit-3.0.1/setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+long_description += "\n\n"
+
+with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
+    long_description += fh.read()
+
+setup(
+    name="rl-toolkit",
+    version="3.0.1",
+    description="The RL-Toolkit: A toolkit for developing and comparing your reinforcement learning agents in various games (OpenAI Gym or Pybullet).",  # noqa
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/markub3327/rl-toolkit",
+    project_urls={
+        "Bug Tracker": "https://github.com/markub3327/rl-toolkit/issues",
+    },
+    author="Martin Kubovčík",
+    author_email="markub3327@gmail.com",
+    license="mit",
+    packages=["rl-toolkit"],
+    keywords=[
+        "reinforcement-learning",
+        "ml",
+        "openai-gym",
+        "pybullet",
+        "reverb",
+        "docker",
+        "rl-agents",
+        "rl",
+        "sac",
+        "rl-algorithms",
+        "soft-actor-critic",
+        "gsde",
+        "rl-toolkit",
+        "games",
+        "tensorflow",
+        "wandb",
+    ],
+    install_requires=[
+        "gym",
+        "box2d",
+        "pybullet",
+        "tensorflow",
+        "tensorflow_probability",
+        "opencv-python",
+        "wandb",
+        "dm-reverb",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Environment :: GPU :: NVIDIA CUDA",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
