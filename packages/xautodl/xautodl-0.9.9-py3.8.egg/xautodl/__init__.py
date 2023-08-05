423b78b2729c95f78e9125d90442864a64e85076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:22 +0000

    Add package xautodl/0.9.9/xautodl-0.9.9-py3.8.egg

---
+++ packages/xautodl/xautodl-0.9.9-py3.8.egg/xautodl/__init__.py
@@ -0,0 +1,11 @@
+#####################################################
+# Copyright (c) Xuanyi Dong [GitHub D-X-Y], 2021.05 #
+#####################################################
+# An Automated Deep Learning Package to support     #
+# research activities.                              #
+#####################################################
+
+
+def version():
+    versions = ["0.9.9"]  # 2021.05.19
+    return versions[-1]
