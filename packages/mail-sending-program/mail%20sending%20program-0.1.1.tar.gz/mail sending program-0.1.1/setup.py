b12714ca59fedc40004b5b7479a1cb5ee4a35c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:01 +0000

    Add package mail-sending-program/0.1.1/mail%20sending%20program-0.1.1.tar.gz

---
+++ packages/mail-sending-program/mail%20sending%20program-0.1.1.tar.gz/mail sending program-0.1.1/setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup, find_packages
+import codecs
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION  = f.read()
+    
+VERSION = '0.1.1'
+DESCRIPTION = 'gmail sending lib'
+URL = 'https://pypi.org/project/mail-sending-program/'
+# Setting up
+setup(
+    name="mail sending program",
+    version=VERSION,
+    author="py devoleper",
+    author_email="<mailsender.help@gmail.com>",
+    url=URL,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'gmail', 'html', 'stmlp'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
