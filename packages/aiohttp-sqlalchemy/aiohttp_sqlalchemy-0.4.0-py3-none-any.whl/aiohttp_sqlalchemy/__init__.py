9ada85d6946d7d9129a693832f3a40a975e0ca6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:44 +0000

    Add package aiohttp-sqlalchemy/0.4.0/aiohttp_sqlalchemy-0.4.0-py3-none-any.whl

---
+++ packages/aiohttp-sqlalchemy/aiohttp_sqlalchemy-0.4.0-py3-none-any.whl/aiohttp_sqlalchemy/__init__.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING
+import warnings
+from aiohttp_sqlalchemy.constants import DEFAULT_KEY
+from aiohttp_sqlalchemy.decorators import sa_decorator
+from aiohttp_sqlalchemy.exceptions import DuplicateAppKeyError
+from aiohttp_sqlalchemy.middlewares import sa_middleware
+from aiohttp_sqlalchemy.views import SAView, SAViewMixin
+
+
+if TYPE_CHECKING:
+    from aiohttp.web import Application
+    from sqlalchemy.ext.asyncio import AsyncEngine
+    from typing import Iterable, Tuple
+
+    TSABinding = Tuple[AsyncEngine, str, bool]
+
+
+__version__ = '0.4.0'
+
+
+def sa_bind(engine: 'AsyncEngine', key: str = DEFAULT_KEY, *,
+            middleware: bool = True) -> 'TSABinding':
+    """ AsyncEngine wrapper for binding in setup function. """
+    return engine, key, middleware
+
+
+def sa_engine(engine: 'AsyncEngine', key: str = DEFAULT_KEY, *,
+              middleware: bool = True) -> 'TSABinding':
+    msg = "aiohttp_sqlalchemy.sa_engine() is deprecated. " \
+          "Use aiohttp_sqlalchemy.sa_bind()."
+    warnings.warn(msg, DeprecationWarning, stacklevel=2)
+    return engine, key, middleware
+
+
+def setup(app: 'Application', bindings: 'Iterable[TSABinding]'):
+    """ Setup function for binding SQLAlchemy engines. """
+    for engine, key, middleware in bindings:
+        if key in app:
+            raise DuplicateAppKeyError(key)
+
+        app[key] = engine
+
+        if middleware:
+            app.middlewares.append(sa_middleware(key))
