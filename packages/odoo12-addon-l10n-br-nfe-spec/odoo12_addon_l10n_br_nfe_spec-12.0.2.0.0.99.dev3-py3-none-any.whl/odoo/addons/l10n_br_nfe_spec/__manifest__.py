e08cf6db0a96a48a4c44f01c8ca85f957afa6f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:50 +0000

    Add package odoo12-addon-l10n-br-nfe-spec/12.0.2.0.0.99.dev3/odoo12_addon_l10n_br_nfe_spec-12.0.2.0.0.99.dev3-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-br-nfe-spec/odoo12_addon_l10n_br_nfe_spec-12.0.2.0.0.99.dev3-py3-none-any.whl/odoo/addons/l10n_br_nfe_spec/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    "name": "nfe spec",
+    "version": "12.0.2.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "category": "Accounting",
+    "summary": "nfe spec",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+    "development_status": "Production/Stable",
+    "maintainers": ["rvalyi"],
+    "website": "https://github.com/OCA/l10n-brazil",
+}
