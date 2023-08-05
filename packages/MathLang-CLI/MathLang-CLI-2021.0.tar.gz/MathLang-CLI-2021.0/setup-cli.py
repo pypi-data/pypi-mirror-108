dc1e2625fe4c5488dffa2ab8760c5c1cab5d4cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:28 +0000

    Add package MathLang-CLI/2021.0/MathLang-CLI-2021.0.tar.gz

---
+++ packages/MathLang-CLI/MathLang-CLI-2021.0.tar.gz/MathLang-CLI-2021.0/setup-cli.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_desc = f.read()
+
+setup(
+    name='MathLang-CLI',
+    version='2021.0',
+    author='GreaterGoodCorp',
+    author_email='binhnt.mdev@gmail.com',
+    license='MIT License',
+    description='MathLang-CLI is an extension for MathLang to support CLI application.',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Education",
+        "Topic :: Utilities",
+    ],
+    url='https://github.com/GreaterGoodCorp/MathLang',
+    project_urls={
+        "Bug Tracker": "https://github.com/GreaterGoodCorp/MathLang/issues",
+    },
+    packages=['MathLang.Interface.CLI'],
+    package_dir={'': 'src'},
+    install_requires=[
+        "MathLang~=2021.0",
+        "click~=8.0.1",
+    ],
+    python_requires=">=3.6",
+)
