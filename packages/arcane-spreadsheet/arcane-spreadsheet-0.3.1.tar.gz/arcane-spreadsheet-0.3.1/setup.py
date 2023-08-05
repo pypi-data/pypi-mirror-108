bfd5179c0ee16f4fd2a5c5ec84866ca03ab8f5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:32 +0000

    Add package arcane-spreadsheet/0.3.1/arcane-spreadsheet-0.3.1.tar.gz

---
+++ packages/arcane-spreadsheet/arcane-spreadsheet-0.3.1.tar.gz/arcane-spreadsheet-0.3.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arcane', 'arcane.spreadsheet']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['arcane-core>=1.0.7,<2.0.0',
+ 'arcane-requests>=0.1.2,<0.2.0',
+ 'google-api-python-client==1.7.8',
+ 'oauth2client==4.1.3']
+
+setup_kwargs = {
+    'name': 'arcane-spreadsheet',
+    'version': '0.3.1',
+    'description': 'Arcane spreadsheet tools',
+    'long_description': '# Arcane spreadsheet\n\nUtility package to use google spreadsheet\n',
+    'author': 'Arcane',
+    'author_email': 'product@arcane.run',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
