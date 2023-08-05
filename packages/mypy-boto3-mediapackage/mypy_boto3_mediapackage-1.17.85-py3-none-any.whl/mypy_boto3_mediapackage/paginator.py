26bacf727b3d94bc65cd602482acbacbf0e3ffa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:17 +0000

    Add package mypy-boto3-mediapackage/1.17.85/mypy_boto3_mediapackage-1.17.85-py3-none-any.whl

---
+++ packages/mypy-boto3-mediapackage/mypy_boto3_mediapackage-1.17.85-py3-none-any.whl/mypy_boto3_mediapackage/paginator.py
@@ -0,0 +1,81 @@
+"""
+Type annotations for mediapackage service client paginators.
+
+[Open documentation](./paginators.md)
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_mediapackage import MediaPackageClient
+    from mypy_boto3_mediapackage.paginator import (
+        ListChannelsPaginator,
+        ListHarvestJobsPaginator,
+        ListOriginEndpointsPaginator,
+    )
+
+    client: MediaPackageClient = boto3.client("mediapackage")
+
+    list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+    list_harvest_jobs_paginator: ListHarvestJobsPaginator = client.get_paginator("list_harvest_jobs")
+    list_origin_endpoints_paginator: ListOriginEndpointsPaginator = client.get_paginator("list_origin_endpoints")
+    ```
+"""
+from typing import Iterator
+
+from botocore.paginate import Paginator as Boto3Paginator
+
+from .type_defs import (
+    ListChannelsResponseTypeDef,
+    ListHarvestJobsResponseTypeDef,
+    ListOriginEndpointsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = ("ListChannelsPaginator", "ListHarvestJobsPaginator", "ListOriginEndpointsPaginator")
+
+
+class ListChannelsPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.85/reference/services/mediapackage.html#MediaPackage.Paginator.ListChannels)[Show boto3-stubs documentation](./paginators.md#listchannelspaginator)
+    """
+
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListChannelsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.85/reference/services/mediapackage.html#MediaPackage.Paginator.ListChannels.paginate)
+        [Show boto3-stubs documentation](./paginators.md#listchannelspaginator)
+        """
+
+
+class ListHarvestJobsPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.85/reference/services/mediapackage.html#MediaPackage.Paginator.ListHarvestJobs)[Show boto3-stubs documentation](./paginators.md#listharvestjobspaginator)
+    """
+
+    def paginate(
+        self,
+        IncludeChannelId: str = None,
+        IncludeStatus: str = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Iterator[ListHarvestJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.85/reference/services/mediapackage.html#MediaPackage.Paginator.ListHarvestJobs.paginate)
+        [Show boto3-stubs documentation](./paginators.md#listharvestjobspaginator)
+        """
+
+
+class ListOriginEndpointsPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.85/reference/services/mediapackage.html#MediaPackage.Paginator.ListOriginEndpoints)[Show boto3-stubs documentation](./paginators.md#listoriginendpointspaginator)
+    """
+
+    def paginate(
+        self, ChannelId: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListOriginEndpointsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.85/reference/services/mediapackage.html#MediaPackage.Paginator.ListOriginEndpoints.paginate)
+        [Show boto3-stubs documentation](./paginators.md#listoriginendpointspaginator)
+        """
