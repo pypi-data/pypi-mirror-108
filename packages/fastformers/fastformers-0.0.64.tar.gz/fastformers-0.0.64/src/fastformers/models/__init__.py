1c43b315559b5524c0bc6451053ed432d410590b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:00 +0000

    Add package fastformers/0.0.64/fastformers-0.0.64.tar.gz

---
+++ packages/fastformers/fastformers-0.0.64.tar.gz/fastformers-0.0.64/src/fastformers/models/__init__.py
@@ -0,0 +1,5 @@
+from .bart import BartForSequenceClassification
+from .distilbert import DistilBertForMaskedLM
+from .electra import ElectraForSequenceClassification
+from .roberta import RobertaForQuestionAnswering
+from .coref import CoreferencePredictor
