42dc8d4f1f4eeeae30c1b3a8b9aade9648b4c888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:36 +0000

    Add package livemark/0.1.3/livemark-0.1.3-py2.py3-none-any.whl

---
+++ packages/livemark/livemark-0.1.3-py2.py3-none-any.whl/livemark/config.py
@@ -0,0 +1,17 @@
+import os
+
+
+# Helpers
+
+
+def read_asset(*paths):
+    dirname = os.path.dirname(__file__)
+    with open(os.path.join(dirname, "assets", *paths)) as file:
+        return file.read().strip()
+
+
+# General
+
+
+VERSION = read_asset("VERSION")
+LAYOUT = read_asset("templates", "layout.html")
---
+++ packages/livemark/livemark-0.1.3-py2.py3-none-any.whl/livemark/renderer.py
@@ -0,0 +1,15 @@
+#  import io
+#  import sys
+#  import subprocess
+#  import contextlib
+#  from copy import copy
+#  from marko.inline import RawText
+#  from marko.block import FencedCode
+from marko.html_renderer import HTMLRenderer
+
+
+class LivemarkRenderer(HTMLRenderer):
+
+    # Render
+
+    pass
