5e355d7756237f738b0d86dce3ae728e58b9872e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:04 +0000

    Add package Tabata/0.1.1/Tabata-0.1.1.tar.gz

---
+++ packages/Tabata/Tabata-0.1.1.tar.gz/Tabata-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Tabata',
+    version='0.1.1',
+    packages=['tabata',],
+    license='MIT',
+    long_description='Programmatic, Template Driven Tableau Workbooks',
+    url='https://github.com/realitybytez/Tabata',
+    install_requires=['lxml>=4.5.1',]
+)
