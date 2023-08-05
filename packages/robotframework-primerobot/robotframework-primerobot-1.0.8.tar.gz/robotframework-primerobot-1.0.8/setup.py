40697611720978085bf512acf351a50d2355bf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:19 +0000

    Add package robotframework-primerobot/1.0.8/robotframework-primerobot-1.0.8.tar.gz

---
+++ packages/robotframework-primerobot/robotframework-primerobot-1.0.8.tar.gz/robotframework-primerobot-1.0.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='robotframework-primerobot',
+    version='1.0.8',
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'PyYAML',
+        'typing_extensions',
+        'pandas==1.2.3',
+        'gql==3.0.0a2'
+    ],
+)
