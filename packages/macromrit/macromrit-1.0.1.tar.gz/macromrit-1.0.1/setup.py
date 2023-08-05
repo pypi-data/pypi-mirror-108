8a4d5ba2d7e0cb6ddadf98f8a37fafe241870aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:26 +0000

    Add package macromrit/1.0.1/macromrit-1.0.1.tar.gz

---
+++ packages/macromrit/macromrit-1.0.1.tar.gz/macromrit-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools 
+
+with open('README.txt', 'r') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="macromrit",
+  version="1.0.1",
+  author="C.Amrit Subramanian",
+  author_email="amritsubramanian.c@gmail.com",
+  description="A tiny but compact file writting library... if file writting create's dings, then oppose it with wings :)",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/macromrit/python_package_amrit",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
+ 
