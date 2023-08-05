c3224d4c9a38797aed6f76ff0877f31043955aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:52 +0000

    Add package evalml/0.25.0/evalml-0.25.0.tar.gz

---
+++ packages/evalml/evalml-0.25.0.tar.gz/evalml-0.25.0/evalml/demos/diabetes.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import woodwork as ww
+from sklearn.datasets import load_diabetes as load_diabetes_sk
+
+
+def load_diabetes():
+    """Load diabetes dataset. Regression problem
+
+    Returns:
+        (pd.Dataframe, pd.Series): X and y
+    """
+    data = load_diabetes_sk()
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = pd.Series(data.target)
+
+    X.ww.init()
+    y = ww.init_series(y)
+
+    return X, y
