409e0facc561000ee8b06ce7ec45913a8a83b678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:26 +0000

    Add package datalake-scripts/2.3.1/datalake-scripts-2.3.1.tar.gz

---
+++ packages/datalake-scripts/datalake-scripts-2.3.1.tar.gz/datalake-scripts-2.3.1/datalake_scripts/common/logger.py
@@ -0,0 +1,21 @@
+import logging
+
+logger = logging.getLogger("OCD_DTL")
+
+DEBUG_FORMAT = '%(name)s:%(levelname)-4s %(message)s'
+INFO_FORMAT = '%(message)s'
+
+
+def configure_logging(loglevel: int):
+    handler = logging.StreamHandler()
+
+    if loglevel == logging.DEBUG:
+        formatter = logging.Formatter(DEBUG_FORMAT)
+    else:
+        formatter = logging.Formatter(INFO_FORMAT)
+    handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(handler)
+
+    logger.setLevel(loglevel)
