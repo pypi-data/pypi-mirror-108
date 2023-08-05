b8b0f0f103dc847f30d73b18d26a24acad981bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:19 +0000

    Add package binance-bot/0.1.1/binance-bot-0.1.1.tar.gz

---
+++ packages/binance-bot/binance-bot-0.1.1.tar.gz/binance-bot-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE.md') as f:
+    license = f.read()
+
+setup(
+    name='binance-bot',
+    version='0.1.1',
+    description='Binance Bot Trade',
+    long_description=readme,
+    author='Nguyen Doan Cuong',
+    author_email='cuongnd86@gmail.com',
+    url='https://google.com.vn',
+    license=license,
+    packages=find_packages(exclude=('tests', '_docs'))
+)
+
