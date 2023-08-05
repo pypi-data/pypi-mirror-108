332647129b08da438166ac0fdef34a58c1d4c650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:04 +0000

    Add package project-two/1.0a0/project-two-1.0a0.tar.gz

---
+++ packages/project-two/project-two-1.0a0.tar.gz/project-two-1.0a0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='project-two',
+    version='1.0a0',
+    packages=['v1'],
+    url='https://google.com',
+    author='ML',
+    author_email='',
+)
