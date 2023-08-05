5024bb30cb13e10087cc0682c1a96881597e4cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:20 +0000

    Add package pyformatters-xml-rf/0.2.31/pyformatters-xml-rf-0.2.31.tar.gz

---
+++ packages/pyformatters-xml-rf/pyformatters-xml-rf-0.2.31.tar.gz/pyformatters-xml-rf-0.2.31/tests/test_xml_rf.py
@@ -0,0 +1,50 @@
+import json
+from pathlib import Path
+
+import pytest
+from starlette.responses import Response
+
+from pyformatters_xml_rf.xml_rf import RFXmlFormatter, RFXmlParameters
+from pymultirole_plugins.v1.schema import Document
+import lxml.etree as ET
+
+
+def test_files():
+    testdir = Path(__file__).parent / 'data'
+    return testdir.glob('*.json')
+
+
+@pytest.mark.parametrize('test_file', test_files())
+def test_xml_rf(test_file):
+    with test_file.open("r") as fin:
+        docs = json.load(fin)
+        doc = Document(**docs[0])
+        formatter = RFXmlFormatter()
+        options = RFXmlParameters()
+        resp: Response = formatter.format(doc, options)
+        assert resp.status_code == 200
+        assert resp.media_type == "application/xml"
+        root = ET.fromstring(resp.body)
+        baseNs = root.nsmap.get(None, None)
+        all_descs = list(root.iterdescendants(f"{{{baseNs}}}DESCRIPTEUR"))
+        all_forms = list(root.iterdescendants(f"{{{baseNs}}}FORME"))
+        assert len(all_descs) > 0
+        assert len(all_forms) > 0
+
+
+@pytest.mark.parametrize('test_file', test_files())
+def test_xml_rf_without_forms(test_file):
+    with test_file.open("r") as fin:
+        docs = json.load(fin)
+        doc = Document(**docs[0])
+        formatter = RFXmlFormatter()
+        options = RFXmlParameters(with_forms=False)
+        resp: Response = formatter.format(doc, options)
+        assert resp.status_code == 200
+        assert resp.media_type == "application/xml"
+        root = ET.fromstring(resp.body)
+        baseNs = root.nsmap.get(None, None)
+        all_descs = list(root.iterdescendants(f"{{{baseNs}}}DESCRIPTEUR"))
+        all_forms = list(root.iterdescendants(f"{{{baseNs}}}FORME"))
+        assert len(all_descs) > 0
+        assert len(all_forms) == 0
