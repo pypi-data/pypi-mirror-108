4d985ec2be01f28d7594860cf0f171c7db9aa69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:03 +0000

    Add package azure-cli/2.24.1/azure-cli-2.24.1.tar.gz

---
+++ packages/azure-cli/azure-cli-2.24.1.tar.gz/azure-cli-2.24.1/azure/cli/command_modules/rdbms/flexible_server_custom_common.py
@@ -0,0 +1,123 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=unused-argument, line-too-long
+from datetime import datetime
+from knack.log import get_logger
+from knack.util import CLIError
+from azure.cli.core.util import user_confirmation
+
+logger = get_logger(__name__)
+# pylint: disable=raise-missing-from
+
+
+# Common functions used by other providers
+def flexible_server_update_get(client, resource_group_name, server_name):
+    return client.get(resource_group_name, server_name)
+
+
+def flexible_server_stop(cmd, client, resource_group_name=None, server_name=None):
+    logger.warning("Server will be automatically started after 7 days "
+                   "if you do not perform a manual start operation")
+    return client.begin_stop(resource_group_name, server_name)
+
+
+def flexible_server_update_set(client, resource_group_name, server_name, parameters):
+    return client.begin_update(resource_group_name, server_name, parameters)
+
+
+def server_list_custom_func(client, resource_group_name=None):
+    if resource_group_name:
+        return client.list_by_resource_group(resource_group_name)
+    return client.list()
+
+
+def firewall_rule_create_func(client, resource_group_name, server_name, firewall_rule_name=None, start_ip_address=None, end_ip_address=None):
+
+    if end_ip_address is None and start_ip_address is not None:
+        end_ip_address = start_ip_address
+        logger.warning('Configuring server firewall rule to accept connections from \'%s\'...', start_ip_address)
+
+    if firewall_rule_name is None:
+        now = datetime.now()
+        firewall_rule_name = 'FirewallIPAddress_{}-{}-{}_{}-{}-{}'.format(now.year, now.month, now.day, now.hour, now.minute,
+                                                                          now.second)
+        if start_ip_address == '0.0.0.0' and end_ip_address == '0.0.0.0':
+            logger.warning('Configuring server firewall rule, \'azure-access\', to accept connections from all '
+                           'Azure resources...')
+            firewall_rule_name = 'AllowAllAzureServicesAndResourcesWithinAzureIps_{}-{}-{}_{}-{}-{}'.format(now.year, now.month,
+                                                                                                            now.day, now.hour,
+                                                                                                            now.minute,
+                                                                                                            now.second)
+        else:
+            if start_ip_address == '0.0.0.0' and end_ip_address == '255.255.255.255':
+                firewall_rule_name = 'AllowAll_{}-{}-{}_{}-{}-{}'.format(now.year, now.month, now.day,
+                                                                         now.hour, now.minute, now.second)
+            logger.warning('Configuring server firewall rule to accept connections from \'%s\' to \'%s\'...', start_ip_address,
+                           end_ip_address)
+
+    parameters = {
+        'name': firewall_rule_name,
+        'start_ip_address': start_ip_address,
+        'end_ip_address': end_ip_address
+    }
+
+    return client.begin_create_or_update(
+        resource_group_name,
+        server_name,
+        firewall_rule_name,
+        parameters)
+
+
+def firewall_rule_delete_func(client, resource_group_name, server_name, firewall_rule_name, yes=None):
+    result = None
+    if not yes:
+        user_confirmation(
+            "Are you sure you want to delete the firewall-rule '{0}' in server '{1}', resource group '{2}'".format(
+                firewall_rule_name, server_name, resource_group_name))
+    try:
+        result = client.begin_delete(resource_group_name, server_name, firewall_rule_name)
+    except Exception as ex:  # pylint: disable=broad-except
+        logger.error(ex)
+    return result
+
+
+def flexible_firewall_rule_custom_getter(client, resource_group_name, server_name, firewall_rule_name):
+    return client.get(resource_group_name, server_name, firewall_rule_name)
+
+
+def flexible_firewall_rule_custom_setter(client, resource_group_name, server_name, firewall_rule_name, parameters):
+    return client.begin_create_or_update(
+        resource_group_name,
+        server_name,
+        firewall_rule_name,
+        parameters)
+
+
+def flexible_firewall_rule_update_custom_func(instance, start_ip_address=None, end_ip_address=None):
+    if start_ip_address is not None:
+        instance.start_ip_address = start_ip_address
+    if end_ip_address is not None:
+        instance.end_ip_address = end_ip_address
+    return instance
+
+
+def database_delete_func(client, resource_group_name=None, server_name=None, database_name=None, yes=None):
+    result = None
+    if resource_group_name is None or server_name is None or database_name is None:
+        raise CLIError("Incorrect Usage : Deleting a database needs resource-group, server-name and database-name."
+                       "If your parameter persistence is turned ON, make sure these three parameters exist in "
+                       "persistent parameters using \'az config param-persist show\'. "
+                       "If your parameter persistence is turned OFF, consider passing them explicitly.")
+    if not yes:
+        user_confirmation(
+            "Are you sure you want to delete the server '{0}' in resource group '{1}'".format(server_name,
+                                                                                              resource_group_name), yes=yes)
+
+    try:
+        result = client.begin_delete(resource_group_name, server_name, database_name)
+    except Exception as ex:  # pylint: disable=broad-except
+        logger.error(ex)
+    return result
