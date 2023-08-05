fe5116a8774ee3533b385c09a2c82e25a486b9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:50 +0000

    Add package nam/0.0.2/nam-0.0.2.tar.gz

---
+++ packages/nam/nam-0.0.2.tar.gz/nam-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='nam',
+    version='0.0.2',
+    description="Neural Additive Models (Google Research)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Amr Kayid",
+    url="https://github.com/AmrMKayid/nam",
+    packages=find_packages(),
+    install_requires=[
+        "torch==1.7.0",
+        "pytorch-lightning",
+        "wandb",
+        "matplotlib",
+        "sklearn",
+        "numpy",
+        "pandas",
+        "loguru",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
