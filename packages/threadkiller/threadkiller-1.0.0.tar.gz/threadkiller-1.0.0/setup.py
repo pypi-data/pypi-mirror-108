a95bad37956e49e49636474a2248c10e5b19fd18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:44 +0000

    Add package threadkiller/1.0.0/threadkiller-1.0.0.tar.gz

---
+++ packages/threadkiller/threadkiller-1.0.0.tar.gz/threadkiller-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['threadkiller']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-telegram-bot>=13.5,<14.0']
+
+setup_kwargs = {
+    'name': 'threadkiller',
+    'version': '1.0.0',
+    'description': 'Telegram bot to delete messages outside threads',
+    'long_description': None,
+    'author': 'Stefano Pigozzi',
+    'author_email': 'me@steffo.eu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
