b0d195cf8cb70653335db23f679c35cd4b3b33cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:10 +0000

    Add package watchmen-storage-engine/0.1.1/watchmen-storage-engine-0.1.1.tar.gz

---
+++ packages/watchmen-storage-engine/watchmen-storage-engine-0.1.1.tar.gz/watchmen-storage-engine-0.1.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['storage',
+ 'storage.common',
+ 'storage.config',
+ 'storage.mongo',
+ 'storage.mysql',
+ 'storage.mysql.dbscript',
+ 'storage.mysql.model',
+ 'storage.oracle',
+ 'storage.oracle.dbscript',
+ 'storage.snowflake',
+ 'storage.storage',
+ 'storage.storage.engine',
+ 'storage.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cx-Oracle>=8.2.1,<9.0.0', 'pydantic>=1.8.2,<2.0.0', 'pymongo>=3.11.4,<4.0.0']
+
+setup_kwargs = {
+    'name': 'watchmen-storage-engine',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'luke0623',
+    'author_email': 'luke0623@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
