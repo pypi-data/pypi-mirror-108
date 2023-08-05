6bbe42437372b5c42820d86926de14d3de9bd71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package smoldyn/2.66.dev20210602/smoldyn-2.66.dev20210602-cp38-cp38-win_amd64.whl

---
+++ packages/smoldyn/smoldyn-2.66.dev20210602-cp38-cp38-win_amd64.whl/smoldyn/__main__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Run script.
+"""
+
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2019-, Dilawar Singh"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+
+import smoldyn._smoldyn
+import argparse
+
+def main():
+    # Argument parser.
+    description = '''Command-line interface to Smoldyn.'''
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('input', help = 'Input file')
+    parser.add_argument('--overwrite', '-w', action='store_true', help="Overwrite exsiting data file")
+    parser.add_argument('--quit-at-end', '-q', action='store_true', help="Quit at end")
+    parser.add_argument('--args', '-A', default='', type=str, help="smoldyn command line arguments")
+    args = parser.parse_args()
+    smoldyn._smoldyn.loadModel(args.input, args.args, args.overwrite, args.quit_at_end)
+
+if __name__ == '__main__':
+    main()
