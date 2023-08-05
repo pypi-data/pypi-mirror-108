822c1c1ceae7d03fbb9eaf7d4a7080557825ec13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:20 +0000

    Add package dipdup/0.4.2/dipdup-0.4.2.tar.gz

---
+++ packages/dipdup/dipdup-0.4.2.tar.gz/dipdup-0.4.2/src/dipdup/dipdup.py
@@ -0,0 +1,152 @@
+import asyncio
+import hashlib
+import importlib
+import logging
+from copy import copy
+from typing import Dict, cast
+
+from tortoise import Tortoise
+from tortoise.exceptions import OperationalError
+from tortoise.utils import get_schema_sql
+
+import dipdup.codegen as codegen
+import dipdup.utils as utils
+from dipdup import __version__
+from dipdup.config import (
+    ROLLBACK_HANDLER,
+    ContractConfig,
+    DipDupConfig,
+    DynamicTemplateConfig,
+    PostgresDatabaseConfig,
+    SqliteDatabaseConfig,
+    StaticTemplateConfig,
+    TzktDatasourceConfig,
+)
+from dipdup.datasources.tzkt.datasource import TzktDatasource
+from dipdup.exceptions import ConfigurationError
+from dipdup.hasura import configure_hasura
+from dipdup.models import IndexType, State
+
+
+class DipDup:
+    def __init__(self, config: DipDupConfig) -> None:
+        self._logger = logging.getLogger(__name__)
+        self._config = config
+
+    async def init(self) -> None:
+        await codegen.create_package(self._config)
+        await codegen.resolve_dynamic_templates(self._config)
+        await codegen.fetch_schemas(self._config)
+        await codegen.generate_types(self._config)
+        await codegen.generate_handlers(self._config)
+        await codegen.cleanup(self._config)
+
+    async def run(self, reindex: bool) -> None:
+        url = self._config.database.connection_string
+        cache = isinstance(self._config.database, SqliteDatabaseConfig)
+        models = f'{self._config.package}.models'
+
+        try:
+            rollback_fn = getattr(importlib.import_module(f'{self._config.package}.handlers.{ROLLBACK_HANDLER}'), ROLLBACK_HANDLER)
+        except ModuleNotFoundError as e:
+            raise ConfigurationError(f'Package `{self._config.package}` not found. Have you forgot to call `init`?') from e
+
+        async with utils.tortoise_wrapper(url, models):
+            await self.initialize_database(reindex)
+
+            await self._config.initialize()
+
+            datasources: Dict[TzktDatasourceConfig, TzktDatasource] = {}
+
+            self._logger.info('Processing dynamic templates')
+            has_dynamic_templates = False
+            for index_name, index_config in copy(self._config.indexes).items():
+                if isinstance(index_config, DynamicTemplateConfig):
+                    if not self._config.templates:
+                        raise ConfigurationError('`templates` section is missing')
+                    has_dynamic_templates = True
+                    template = self._config.templates[index_config.template]
+                    # NOTE: Datasource and other fields are str as we haven't initialized DynamicTemplateConfigs yet
+                    datasource_config = self._config.datasources[cast(str, template.datasource)]
+                    if datasource_config not in datasources:
+                        datasources[datasource_config] = TzktDatasource(datasource_config.url, cache)
+                        datasources[datasource_config].set_rollback_fn(rollback_fn)
+                        datasources[datasource_config].set_package(self._config.package)
+
+                    contract_config = self._config.contracts[cast(str, index_config.similar_to)]
+                    await datasources[datasource_config].add_contract_subscription(
+                        contract_config, index_name, template, index_config.strict
+                    )
+                    similar_contracts = await datasources[datasource_config].get_similar_contracts(contract_config.address)
+                    for contract_address in similar_contracts:
+                        self._config.contracts[contract_address] = ContractConfig(
+                            address=contract_address,
+                            typename=contract_config.typename,
+                        )
+
+                        generated_index_name = f'{index_name}_{contract_address}'
+                        template_config = StaticTemplateConfig(template=index_config.template, values=dict(contract=contract_address))
+                        self._config.indexes[generated_index_name] = template_config
+
+                    del self._config.indexes[index_name]
+
+            # NOTE: We need to initialize config one more time to process generated indexes
+            if has_dynamic_templates:
+                self._config.pre_initialize()
+                await self._config.initialize()
+
+            for index_name, index_config in self._config.indexes.items():
+                if isinstance(index_config, StaticTemplateConfig):
+                    raise RuntimeError('Config is not initialized')
+                if isinstance(index_config, DynamicTemplateConfig):
+                    raise RuntimeError('Dynamic templates must be resolved before this step')
+
+                self._logger.info('Processing index `%s`', index_name)
+                if isinstance(index_config.datasource, TzktDatasourceConfig):
+                    if index_config.datasource_config not in datasources:
+                        datasources[index_config.datasource_config] = TzktDatasource(index_config.datasource_config.url, cache)
+                        datasources[index_config.datasource_config].set_rollback_fn(rollback_fn)
+                        datasources[index_config.datasource_config].set_package(self._config.package)
+                    await datasources[index_config.datasource_config].add_index(index_name, index_config)
+                else:
+                    raise NotImplementedError(f'Datasource `{index_config.datasource}` is not supported')
+
+            self._logger.info('Starting datasources')
+            run_tasks = [asyncio.create_task(d.start()) for d in datasources.values()]
+
+            if self._config.hasura:
+                hasura_task = asyncio.create_task(configure_hasura(self._config))
+                run_tasks.append(hasura_task)
+
+            await asyncio.gather(*run_tasks)
+
+    async def initialize_database(self, reindex: bool = False) -> None:
+        self._logger.info('Initializing database')
+
+        if isinstance(self._config.database, PostgresDatabaseConfig) and self._config.database.schema_name:
+            await Tortoise._connections['default'].execute_script(f"CREATE SCHEMA IF NOT EXISTS {self._config.database.schema_name}")
+            await Tortoise._connections['default'].execute_script(f"SET search_path TO {self._config.database.schema_name}")
+
+        connection_name, connection = next(iter(Tortoise._connections.items()))
+        schema_sql = get_schema_sql(connection, False)
+
+        # NOTE: Column order could differ in two generated schemas for the same models, drop commas and sort strings to eliminate this
+        processed_schema_sql = '\n'.join(sorted(schema_sql.replace(',', '').split('\n'))).encode()
+        schema_hash = hashlib.sha256(processed_schema_sql).hexdigest()
+
+        if reindex:
+            self._logger.warning('Started with `--reindex` argument, reindexing')
+            await utils.reindex()
+
+        try:
+            schema_state = await State.get_or_none(index_type=IndexType.schema, index_name=connection_name)
+        except OperationalError:
+            schema_state = None
+
+        if schema_state is None:
+            await Tortoise.generate_schemas()
+            schema_state = State(index_type=IndexType.schema, index_name=connection_name, hash=schema_hash)
+            await schema_state.save()
+        elif schema_state.hash != schema_hash:
+            self._logger.warning('Schema hash mismatch, reindexing')
+            await utils.reindex()
---
+++ packages/dipdup/dipdup-0.4.2.tar.gz/dipdup-0.4.2/src/dipdup/models.py
@@ -0,0 +1,177 @@
+import logging
+from copy import deepcopy
+from datetime import datetime
+from enum import Enum
+from typing import Any, Dict, Generic, List, Optional, Type, TypeVar
+
+from pydantic import BaseModel
+from pydantic.dataclasses import dataclass
+from tortoise import Model, fields
+
+from dipdup.exceptions import ConfigurationError
+
+ParameterType = TypeVar('ParameterType', bound=BaseModel)
+StorageType = TypeVar('StorageType', bound=BaseModel)
+KeyType = TypeVar('KeyType', bound=BaseModel)
+ValueType = TypeVar('ValueType', bound=BaseModel)
+
+
+_logger = logging.getLogger(__name__)
+
+
+class IndexType(Enum):
+    operation = 'operation'
+    big_map = 'big_map'
+    block = 'block'
+    schema = 'schema'
+
+
+class State(Model):
+    index_name = fields.CharField(256)
+    index_type = fields.CharEnumField(IndexType)
+    hash = fields.CharField(256)
+    level = fields.IntField(default=0)
+
+    class Meta:
+        table = 'dipdup_state'
+
+
+@dataclass
+class OperationData:
+    type: str
+    id: int
+    level: int
+    timestamp: datetime
+    hash: str
+    counter: int
+    sender_address: str
+    target_address: Optional[str]
+    amount: Optional[int]
+    status: str
+    has_internals: Optional[bool]
+    storage: Dict[str, Any]
+    block: Optional[str] = None
+    sender_alias: Optional[str] = None
+    nonce: Optional[int] = None
+    target_alias: Optional[str] = None
+    entrypoint: Optional[str] = None
+    parameter_json: Optional[Any] = None
+    originated_contract_address: Optional[str] = None
+    originated_contract_type_hash: Optional[int] = None
+    originated_contract_code_hash: Optional[int] = None
+    diffs: Optional[List[Dict[str, Any]]] = None
+
+    def _merge_bigmapdiffs(self, storage_dict: Dict[str, Any], bigmap_name: str, array: bool) -> None:
+        if self.diffs is None:
+            raise Exception('`bigmaps` field missing')
+        _logger.debug(bigmap_name)
+        bigmapdiffs = [bm for bm in self.diffs if bm['path'] == bigmap_name]
+        bigmap_key = bigmap_name.split('.')[-1]
+        for diff in bigmapdiffs:
+            _logger.debug('Applying bigmapdiff: %s', diff)
+            if diff['action'] in ('add_key', 'update_key'):
+                key = diff['content']['key']
+                if array is True:
+                    storage_dict[bigmap_key].append({'key': key, 'value': diff['content']['value']})
+                else:
+                    storage_dict[bigmap_key][key] = diff['content']['value']
+
+    def _process_storage(self, storage_type: Type[StorageType], storage: Dict, prefix: str = None):
+        for key, field in storage_type.__fields__.items():
+
+            if key == '__root__':
+                continue
+
+            if field.alias:
+                key = field.alias
+
+            bigmap_name = key if prefix is None else '.'.join([prefix, key])
+
+            # NOTE: TzKT could return bigmaps as object or as array of key-value objects. We need to guess this from storage.
+            # TODO: This code should be a part of datasource module.
+            try:
+                value = storage[key]
+            except KeyError as e:
+                if not field.required:
+                    continue
+                raise ConfigurationError(f'Type `{storage_type.__name__}` is invalid: `{key}` field does not exists') from e
+
+            if field.type_ not in (int, bool) and isinstance(value, int):
+                if hasattr(field.type_, '__fields__') and 'key' in field.type_.__fields__ and 'value' in field.type_.__fields__:
+                    storage[key] = []
+                    if self.diffs:
+                        self._merge_bigmapdiffs(storage, bigmap_name, array=True)
+                else:
+                    storage[key] = {}
+                    if self.diffs:
+                        self._merge_bigmapdiffs(storage, bigmap_name, array=False)
+            elif hasattr(field.type_, '__fields__') and isinstance(storage[key], dict):
+                storage[key] = self._process_storage(field.type_, storage[key], bigmap_name)
+
+        return storage
+
+    def get_merged_storage(self, storage_type: Type[StorageType]) -> StorageType:
+        if self.storage is None:
+            raise Exception('`storage` field missing')
+
+        storage = deepcopy(self.storage)
+        _logger.debug('Merging storage')
+        _logger.debug('Before: %s', storage)
+        _logger.debug('Diffs: %s', self.diffs)
+
+        storage = self._process_storage(storage_type, storage, None)
+
+        _logger.debug('After: %s', storage)
+
+        return storage_type.parse_obj(storage)
+
+
+@dataclass
+class TransactionContext(Generic[ParameterType, StorageType]):
+    data: OperationData
+    parameter: ParameterType
+    storage: StorageType
+
+
+@dataclass
+class OriginationContext(Generic[StorageType]):
+    data: OperationData
+    storage: StorageType
+
+
+class BigMapAction(Enum):
+    ADD = 'add_key'
+    UPDATE = 'update_key'
+    REMOVE = 'remove_key'
+
+
+@dataclass
+class BigMapContext(Generic[KeyType, ValueType]):
+    action: BigMapAction
+    key: KeyType
+    value: Optional[ValueType]
+
+
+@dataclass
+class BigMapData:
+    id: int
+    level: int
+    operation_id: int
+    timestamp: datetime
+    bigmap: int
+    contract_address: str
+    path: str
+    action: str
+    key: Optional[Any] = None
+    value: Optional[Any] = None
+
+
+@dataclass
+class OperationHandlerContext:
+    operations: List[OperationData]
+    template_values: Optional[Dict[str, str]]
+
+
+@dataclass
+class BigMapHandlerContext:
+    template_values: Optional[Dict[str, str]]
