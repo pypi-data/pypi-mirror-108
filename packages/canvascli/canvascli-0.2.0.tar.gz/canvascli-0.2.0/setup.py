df61cff1f36cc587554812eef32fa34c1243a704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:06 +0000

    Add package canvascli/0.2.0/canvascli-0.2.0.tar.gz

---
+++ packages/canvascli/canvascli-0.2.0.tar.gz/canvascli-0.2.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from canvascli import __version__
+
+
+setup(
+    name='canvascli',
+    version=__version__,
+
+    description=' A CLI to reformat and review Canvas grades',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    author='Joel Ostblom',
+    author_email='joelostblom@protonmail.com',
+    url='https://github.com/joelostblom/canvascli',
+
+    python_requires='>=3.6',
+    py_modules=['canvascli'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'altair',
+        'canvasapi',
+        'click',
+        'pandas',
+        'tabulate',
+        'dataclassy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'canvascli = canvascli.main:cli',
+        ],
+    },
+)
