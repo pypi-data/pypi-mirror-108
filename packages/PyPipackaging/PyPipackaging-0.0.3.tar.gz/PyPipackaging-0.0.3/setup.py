caf35d9a4fa28cadba75cdc1c60bfbeed730424e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:43 +0000

    Add package PyPipackaging/0.0.3/PyPipackaging-0.0.3.tar.gz

---
+++ packages/PyPipackaging/PyPipackaging-0.0.3.tar.gz/PyPipackaging-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup #setuptools is better than disutil
+
+with open ("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(   # lines of configuration
+      name='PyPipackaging',             #name is what you PIP install and what you will upload it as. Not the same as py_modules
+      version='0.0.3',
+      description='Say Hello',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      py_modules=["hellototi"],    # list of actual python code modules within src folder
+      package_dir={'':'src'},       # says that our code is under a src directory
+      url='https://github.com/Totilarson/CorePy',
+      author='Toti Larson',
+      author_email='totlarson@gmail.com',
+      license='LICENSE.txt',
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+      
+)
+
