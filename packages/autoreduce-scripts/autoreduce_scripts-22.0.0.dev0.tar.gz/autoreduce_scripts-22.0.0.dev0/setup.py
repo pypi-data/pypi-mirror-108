d1530de4bc8c025b9352dab1cecd509156918784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:31 +0000

    Add package autoreduce-scripts/22.0.0.dev0/autoreduce_scripts-22.0.0.dev0.tar.gz

---
+++ packages/autoreduce-scripts/autoreduce_scripts-22.0.0.dev0.tar.gz/autoreduce_scripts-22.0.0.dev0/setup.py
@@ -0,0 +1,29 @@
+# pylint:skip-file
+"""
+Wrapper for the functionality for various installation and project setup commands
+see:
+    `python setup.py help`
+for more details
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="autoreduce_scripts",
+    version="22.0.0.dev",
+    description="ISIS Autoreduce helper scripts",
+    author="ISIS Autoreduction Team",
+    url="https://github.com/ISISScientificComputing/autoreduce-scripts/",
+    install_requires=[
+        "autoreduce_qp",
+        "GitPython==3.1.14"  # for backup_reduction_scripts.py
+    ],
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "autoreduce-manual-submission = autoreduce_scripts.manual_operations.manual_submission:fire_entrypoint",
+            "autoreduce-manual-remove = autoreduce_scripts.manual_operations.manual_remove:fire_entrypoint"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+    ])
