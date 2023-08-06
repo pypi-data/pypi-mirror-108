44123cb64df02e2d33d2ea865d24b034405dc51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:39 +0000

    Add package neurobiba/0.4/neurobiba-0.4.tar.gz

---
+++ packages/neurobiba/neurobiba-0.4.tar.gz/neurobiba-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neurobiba',
+    version='0.4',
+    description='small collection of functions for neural networks',
+    url='https://github.com/displaceman/neurobiba',
+    author_email='cumnaamys@gmail.com',
+    author='displaceman',
+    license='GPL',
+    packages=find_packages(),
+    zip_safe=False)
