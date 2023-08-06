15fd6fcec2bc997cb9f5d0fd22c36182acca9a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:48 +0000

    Add package pybuilder-integration/42/pybuilder_integration-42-py3-none-any.whl

---
+++ packages/pybuilder-integration/pybuilder_integration-42-py3-none-any.whl/pybuilder_integration/__init__.py
@@ -0,0 +1,42 @@
+from pybuilder.core import task, Project, Logger, depends, after, init
+from pybuilder.reactor import Reactor
+
+import pybuilder_integration.tasks
+from pybuilder_integration.properties import *
+
+@init
+def init_plugin(project):
+    project.set_property("tavern_addition_args",[])
+    project.plugin_depends_on("pytest")
+    project.plugin_depends_on("tavern")
+
+@task(description="Runs integration tests against a CI/Prod environment."
+                  "\t1. Run current build integration tests found in ${dir_dist}\n"
+                  f"\t2. Run integration tests found in 'LATEST-{ENVIRONMENT}' managed by ${ARTIFACT_MANAGER}\n"
+                  f"\t3. Promote current build integration tests to 'LATEST-{ENVIRONMENT}' (disable with ${PROMOTE_ARTIFACT})\n"
+                  f"\t${INTEGRATION_TARGET_URL} - (required) Full URL target for tests\n"
+                  f"\t${ENVIRONMENT} - (required) Environment that is being tested (ci/prod)\n"
+                  f"\t${PROMOTE_ARTIFACT} - Promote integration tests to LATEST-${ENVIRONMENT} (default TRUE)\n"
+      )
+def verify_environment(project: Project, logger: Logger, reactor: Reactor):
+    tasks.verify_environment(project, logger, reactor)
+
+
+@task(description="Run integration tests using a cypress spec. Requires NPM installed.\n"
+                  f"\t{INTEGRATION_TARGET_URL} - (required) Full URL target for cypress tests\n"
+                  f"\t{CYPRESS_TEST_DIR} - directory for test specification (src/integrationtest/cypress)\n"
+      )
+def verify_cypress(project: Project, logger: Logger, reactor: Reactor):
+    tasks.verify_cypress(project, logger, reactor)
+
+
+@task(description="Run integration tests using tavern specifications.\n"
+                  f"\t{TAVERN_TEST_DIR} - directory containing tavern specifications ({DEFAULT_TAVERN_TEST_DIR})")
+def verify_tavern(project: Project, logger: Logger, reactor: Reactor):
+    tasks.verify_tavern(project, logger, reactor)
+
+
+@task(description="Run verify_tavern and verify_cypress")
+@depends("publish","verify_tavern","verify_cypress")
+def verify_package():
+    pass
