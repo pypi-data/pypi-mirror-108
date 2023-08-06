165b8dda229982de4552e6f1243bd6b0972f91e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:51 +0000

    Add package hankify-pw/1.1.0/hankify_pw-1.1.0.tar.gz

---
+++ packages/hankify-pw/hankify_pw-1.1.0.tar.gz/hankify_pw-1.1.0/README.md
@@ -0,0 +1,18 @@
+# Hank-tastic Password Generator
+
+This Python script generates a password that Hank Preston would be proud of.
+
+Install it with the following command:
+
+```sh
+pip install hankify-pw
+```
+
+All you really need to do is run it:
+
+```bash
+$ hankify-pw
+YoreAnywhySollyaBoatbill81
+```
+
+If you want to use an alternate word list, specify `--word-file PATH`.  If you want a longer or shorter password, specify `--num-words` (the default is 4).  The package ships with a default words file from FreeBSD.
---
+++ packages/hankify-pw/hankify_pw-1.1.0.tar.gz/hankify_pw-1.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(
+    name="hankify_pw",
+    version="1.1.0",
+    description="Generate Hank Preston-y passwords",
+    url="https://github.com/xorrkaz/hankify-pw",
+    entry_points={"console_scripts": ["hankify-pw=hankify_pw:hanky_pass"]},
+    package_data={"hankify_pw": ["words.txt"]},
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+    long_description=readme(),
+    author="Joe Clarke",
+    author_email="jclarke@cisco.com",
+    license="MIT",
+    setup_requires=["wheel"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    zip_safe=False,
+    keywords=["Elemental"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
