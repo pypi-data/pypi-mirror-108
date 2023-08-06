7504f0205a466fad437de7cdab006bb375bf8b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:33 +0000

    Add package oscsipm/0.6.3/oscsipm-0.6.3.tar.gz

---
+++ packages/oscsipm/oscsipm-0.6.3.tar.gz/oscsipm-0.6.3/oscsipm/__init__.py
@@ -0,0 +1,5 @@
+from .histogram import hist2Q, QStatisticsMaker
+from .oscillogram import PulsesHistMaker
+from .crosstalk import (find_pcrosstalk, compensate, distort,
+                        Q2total_pcrosstalk, total_pcrosstalk, single_pcrosstalk,
+                        ENF)
