ce850a561b12c6793bc5993842a08f6e54ede005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:11 +0000

    Add package pygwrapper/0.0.1/pygwrapper-0.0.1.tar.gz

---
+++ packages/pygwrapper/pygwrapper-0.0.1.tar.gz/pygwrapper-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygwrapper",
+    version="0.0.1",
+    author="SlitherCode",
+    description="explore endless creativity and developers with a combination of pygame and pymunk ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
