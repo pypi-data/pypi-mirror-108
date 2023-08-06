6f96e1b3a91b2051b0968321b9836ba6c316d782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:37 +0000

    Add package firstimpression/0.2.2/firstimpression-0.2.2-py3-none-any.whl

---
+++ packages/firstimpression/firstimpression-0.2.2-py3-none-any.whl/firstimpression/nu_nl.py
@@ -0,0 +1,45 @@
+import datetime
+import time
+import os
+from .scala import *
+from .file import download_media
+
+PICTURE_FORMATS_DICT = {
+    'fullscreen':   "http://narrowcasting.fimpweb.nl/imageresize.php?width=1920&url=",
+    'square':       "http://narrowcasting.fimpweb.nl/imageresize.php?width=512&url=",
+}
+
+
+def get_datetime_object(item):
+    unparsedpubdate = item.find('pubDate').text
+    # Wed, 23 Oct 2019 15:03:33 +0200
+
+    created_at = datetime.datetime.strptime(
+        unparsedpubdate, "%a, %d %b %Y %H:%M:%S {}".format(unparsedpubdate[-5:])).timetuple()
+    return datetime.datetime.fromtimestamp(time.mktime(created_at))
+
+
+def get_medialink(item):
+    return item.find('enclosure').attrib['url']
+
+
+def install_picture_content_wrap(picture_format, subdirectory, news_item):
+    # Installs content to LocalIntegratedContent folder and returns mediapath
+    if not picture_format in picture_formats:
+        return None
+
+    media_link = get_medialink(news_item).replace("_sqr256", "")
+
+    if picture_format == 'square':
+        media_link = media_link.replace('.jpg', '_sqr512.jpg')
+
+    media_path = download_media(
+        PICTURE_FORMATS_DICT[picture_format] + media_link, subdirectory)
+
+    # if not check_valid_jpeg(media_path):
+    #     return False
+    scala.install_content(media_path, subdirectory, True)
+
+    media_filename = media_path.split('\\').pop()
+
+    return os.path.join('Content:\\', subdirectory, media_filename)
