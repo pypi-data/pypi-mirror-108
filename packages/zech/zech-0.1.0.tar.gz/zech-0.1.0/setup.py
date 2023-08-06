2feece790f533dcbf48a98e8efc5c6df9de941d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:52 +0000

    Add package zech/0.1.0/zech-0.1.0.tar.gz

---
+++ packages/zech/zech-0.1.0.tar.gz/zech-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['zech']
+setup_kwargs = {
+    'name': 'zech',
+    'version': '0.1.0',
+    'description': 'Visit me at https://zech.codes',
+    'long_description': None,
+    'author': 'Zech Zimmerman',
+    'author_email': 'hi@zech.codes',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
