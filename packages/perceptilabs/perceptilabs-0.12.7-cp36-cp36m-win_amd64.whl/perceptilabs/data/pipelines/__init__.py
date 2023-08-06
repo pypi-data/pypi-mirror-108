802e48e9c5815a7fe5422ee59b50d3e99e9f5022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:42 +0000

    Add package perceptilabs/0.12.7/perceptilabs-0.12.7-cp36-cp36m-win_amd64.whl

---
+++ packages/perceptilabs/perceptilabs-0.12.7-cp36-cp36m-win_amd64.whl/perceptilabs/data/pipelines/__init__.py
@@ -0,0 +1,4 @@
+from perceptilabs.data.pipelines.numerical import NumericalPipelineBuilder
+from perceptilabs.data.pipelines.image import ImagePipelineBuilder
+from perceptilabs.data.pipelines.binary import BinaryPipelineBuilder
+from perceptilabs.data.pipelines.categorical import CategoricalPipelineBuilder
