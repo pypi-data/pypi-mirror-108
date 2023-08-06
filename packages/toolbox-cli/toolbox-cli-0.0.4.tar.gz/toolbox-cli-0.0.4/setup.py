94faa1aba8479bd5bd39b8aa740f0e90cc2de6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:10 +0000

    Add package toolbox-cli/0.0.4/toolbox-cli-0.0.4.tar.gz

---
+++ packages/toolbox-cli/toolbox-cli-0.0.4.tar.gz/toolbox-cli-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Pack command: python setup.py sdist bdist_wheel
+
+setup(
+    name="toolbox-cli",
+    version="0.0.4",
+    author="meowmeowcat",
+    author_email="",
+    description="A toolbox that runs in Command Line Interface.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["toolbox"],
+    entry_points='''
+        [console_scripts]
+        toolbox=toolbox.__main__:cli
+    ''',
+    python_requires=">=3.6",
+    install_requires=[
+        "colorama"
+    ],
+    
+)
