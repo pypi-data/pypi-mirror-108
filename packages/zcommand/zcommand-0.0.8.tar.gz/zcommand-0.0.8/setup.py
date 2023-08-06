88d2f29fc5cad3f7954b8659a1b059bd3c4cae10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:54 +0000

    Add package zcommand/0.0.8/zcommand-0.0.8.tar.gz

---
+++ packages/zcommand/zcommand-0.0.8.tar.gz/zcommand-0.0.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='zcommand',
+    version='0.0.8',
+    description='command line args',
+    packages=find_packages(),
+    url='https://github.com/izhengzhixian/zcommand',
+)
