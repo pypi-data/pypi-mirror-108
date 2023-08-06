cd1667115b66d6fd680fb1af46a30c6178e9f833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:01 +0000

    Add package iaqualink/0.3.90/iaqualink-0.3.90.tar.gz

---
+++ packages/iaqualink/iaqualink-0.3.90.tar.gz/iaqualink-0.3.90/src/iaqualink/const.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+
+AQUALINK_API_KEY = "EOOEMOW4YR6QNB07"
+
+AQUALINK_LOGIN_URL = "https://prod.zodiac-io.com/users/v1/login"
+AQUALINK_DEVICES_URL = "https://r-api.iaqualink.net/devices.json"
+AQUALINK_SESSION_URL = "https://p-api.iaqualink.net/v1/mobile/session.json"
+
+AQUALINK_TEMP_CELSIUS_LOW = 1
+AQUALINK_TEMP_CELSIUS_HIGH = 40
+AQUALINK_TEMP_FAHRENHEIT_LOW = 32
+AQUALINK_TEMP_FAHRENHEIT_HIGH = 104
