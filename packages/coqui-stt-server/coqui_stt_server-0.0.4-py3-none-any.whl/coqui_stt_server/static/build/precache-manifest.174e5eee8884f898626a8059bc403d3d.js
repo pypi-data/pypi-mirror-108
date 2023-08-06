0ed47e5000156ec4742b50d666466a4a24620cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:22 +0000

    Add package coqui-stt-server/0.0.4/coqui_stt_server-0.0.4-py3-none-any.whl

---
+++ packages/coqui-stt-server/coqui_stt_server-0.0.4-py3-none-any.whl/coqui_stt_server/static/build/precache-manifest.174e5eee8884f898626a8059bc403d3d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c77b55d79befd2813a3e285d651bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dcb11affb74ca4d362d",
+    "url": "/static/css/bundle.min.css"
+  },
+  {
+    "revision": "8dcb11affb74ca4d362d",
+    "url": "/static/js/bundle.min.js"
+  },
+  {
+    "revision": "799de9f25b80ded465f02f885354882e",
+    "url": "/static/js/bundle.min.js.LICENSE.txt"
+  }
+]);
