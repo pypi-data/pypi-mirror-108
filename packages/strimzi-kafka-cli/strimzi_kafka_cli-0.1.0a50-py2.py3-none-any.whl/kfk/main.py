86d01a479d43ba0721d6548bcdc011e9c7c5f2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:53 +0000

    Add package strimzi-kafka-cli/0.1.0a50/strimzi_kafka_cli-0.1.0a50-py2.py3-none-any.whl

---
+++ packages/strimzi-kafka-cli/strimzi_kafka_cli-0.1.0a50-py2.py3-none-any.whl/kfk/main.py
@@ -0,0 +1,14 @@
+from kfk.commands.main import kfk
+from kfk.commands.clusters import clusters
+from kfk.commands.topics import topics
+from kfk.commands.users import users
+from kfk.commands.console import console_consumer, console_producer
+from kfk.commands.configs import configs
+from kfk.commands.version import version
+from kfk.commands.acls import acls
+from kfk.commands.operator import operator
+from kfk.commands.connect import connect
+from kfk.commands.env import env
+from kfk.setup import setup
+
+setup()
