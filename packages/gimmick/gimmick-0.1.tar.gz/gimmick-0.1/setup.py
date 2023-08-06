68f7fa458699b9ff75296aff65d00e64091c8d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:23 +0000

    Add package gimmick/0.1/gimmick-0.1.tar.gz

---
+++ packages/gimmick/gimmick-0.1.tar.gz/gimmick-0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from glob import glob
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gimmick",
+    version="0.1",
+    author="Pankaj Rawat",
+    author_email="pankajr141@gmail.com",
+    description="Libraray contains algo to generate images by learning representation from data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pankajr141/gimmick",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+          'tensorflow',
+    ],
+)
