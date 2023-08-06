93c38d9ad76aa2291c7f1e205df4355f6a072044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:02 +0000

    Add package peekingduck/0.2.3/peekingduck-0.2.3.tar.gz

---
+++ packages/peekingduck/peekingduck-0.2.3.tar.gz/peekingduck-0.2.3/peekingduck/cli.py
@@ -0,0 +1,90 @@
+"""
+Copyright 2021 AI Singapore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import logging
+import yaml
+import click
+
+from peekingduck.runner import Runner
+from peekingduck.utils.logger import setup_logger
+
+setup_logger()
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+@click.group()
+def cli() -> None:
+    """
+    PeekingDuck is a python framework for dealing with Machine Learning model inferences.
+
+    Developed by Computer Vision Hub at AI Singapore.
+    """
+
+
+def _get_cwd() -> str:
+    return os.getcwd()
+
+
+def create_custom_folder() -> None:
+    """Make custom nodes folder to create custom nodes
+    """
+    curdir = _get_cwd()
+    custom_node_dir = os.path.join(curdir, "src/custom_nodes")
+
+    logger.info("Creating custom nodes folder in %s", custom_node_dir)
+    os.makedirs(custom_node_dir, exist_ok=True)
+
+
+def create_yml() -> None:
+    """Inits the declarative yaml"""
+    # Default yml to be discussed
+    default_yml = dict(
+        nodes=[
+            'input.live',
+            'model.yolo',
+            'draw.bbox',
+            'output.screen'
+        ]
+    )
+
+    with open('run_config.yml', 'w') as yml_file:
+        yaml.dump(default_yml, yml_file, default_flow_style=False)
+
+
+@cli.command()
+def init() -> None:
+    """Initialise a PeekingDuck project"""
+    print("Welcome to PeekingDuck!")
+    create_custom_folder()
+    create_yml()
+
+
+@cli.command()
+@click.option('--config_path', default=None, type=click.Path(),
+
+              help="List of nodes to run. None assumes \
+                   run_config.yml at current working directory")
+def run(config_path: str) -> None:
+    """Runs PeekingDuck"""
+    curdir = _get_cwd()
+    if not config_path:
+        config_path = os.path.join(curdir, "run_config.yml")
+
+    custom_node_path = os.path.join(curdir, 'src/custom_nodes')
+
+    runner = Runner(config_path, custom_node_path)
+    runner.run()
