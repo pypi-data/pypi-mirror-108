2d2c6d8df60f0c3a4ebfbd7e082afc5f1b1878c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:17 +0000

    Add package firstimpression/0.2.13/firstimpression-0.2.13.tar.gz

---
+++ packages/firstimpression/firstimpression-0.2.13.tar.gz/firstimpression-0.2.13/firstimpression/scala.py
@@ -0,0 +1,52 @@
+import warnings
+
+current_python_version = None
+
+PYTHON2 = "Python2"
+PYTHON3 = "Python3"
+PYTHON3_MAIN = "Python3_Main"
+
+try:
+    import scalalib
+    import scalatools
+
+    current_python_version = PYTHON2
+
+    warnings.warn("Python 2 is deprecated", DeprecationWarning)
+
+except ImportError:
+    try:
+        import scalascript
+
+        current_python_version = PYTHON3
+
+    except ImportError:
+
+        current_python_version = PYTHON3_MAIN
+
+
+def variables():
+    if current_python_version == PYTHON2:
+        return scalalib.sharedvars
+    elif current_python_version == PYTHON3:
+        return scalascript.variables()
+    elif current_python_version == PYTHON3_MAIN:
+        warnings.warn(
+            "Cannot grab variables from scala when running in Python 3 from main", RuntimeWarning)
+        return None
+    else:
+        raise RuntimeError("Wrong Python version used")
+
+
+def install_content(abspath, subfolder=None, autostart=True):
+    if current_python_version == PYTHON2:
+        if not subfolder:
+            subfolder = ''
+        scalalib.install_content(abspath, subfolder, autostart)
+    elif current_python_version == PYTHON3:
+        scalascript.install_content(abspath, subfolder)
+    elif current_python_version == PYTHON3_MAIN:
+        warnings.warn(
+            "Cannot install content in Python 3 when running from main", RuntimeWarning)
+    else:
+        raise RuntimeError("Wrong Python version used")
