31e3ca1f8987bcc3c2e2cf1d5cbca09b8dcc1c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:32 +0000

    Add package abksimplelib/0.0.1/abksimplelib-0.0.1.tar.gz

---
+++ packages/abksimplelib/abksimplelib-0.0.1.tar.gz/abksimplelib-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='abksimplelib',
+	version='0.0.1',
+	description='A simple calculator',
+	author='Andrew King',
+	author_email='andrewking1597@gmail.com',
+	url='',
+	packages=['abksimplelib'])
