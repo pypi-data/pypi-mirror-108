612d22484b7df53baa8c4586a50b3387bbb6ab01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:46 +0000

    Add package django-frozen-field/0.1b4/django_frozen_field-0.1b4-py3-none-any.whl

---
+++ packages/django-frozen-field/django_frozen_field-0.1b4-py3-none-any.whl/frozen_field/converters.py
@@ -0,0 +1,11 @@
+"""Field value conversion functions."""
+
+from datetime import datetime
+
+
+def to_datetime(value: str) -> datetime:
+    return
+
+
+def to_date(value):
+    return parser.parse(value).date()
