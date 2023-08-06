ace9b2b05e7258acf003e71686dd921b1fa4ba8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:18 +0000

    Add package envcon/1.1.0/envcon-1.1.0-py3-none-any.whl

---
+++ packages/envcon/envcon-1.1.0-py3-none-any.whl/envcon/__init__.py
@@ -0,0 +1,4 @@
+from .configuration import environment_configuration, configuration
+from .metaclasses import FrozenClassAttributesError
+
+__all__ = ["environment_configuration", "configuration", "FrozenClassAttributesError"]
