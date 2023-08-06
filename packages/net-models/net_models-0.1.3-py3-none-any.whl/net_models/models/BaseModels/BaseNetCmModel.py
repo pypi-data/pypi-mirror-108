3dff9448cb8fd49b3e45adcc3176ab1823df72aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:17 +0000

    Add package net-models/0.1.3/net_models-0.1.3-py3-none-any.whl

---
+++ packages/net-models/net_models-0.1.3-py3-none-any.whl/net_models/models/BaseModels/BaseNetCmModel.py
@@ -0,0 +1,30 @@
+# from diffsync import DiffSyncModel
+import yaml
+import json
+from pydantic import BaseModel, validate_model, Extra
+from net_models.utils.CustomYamlDumper import CustomYamlDumper
+
+
+class BaseNetCmModel(BaseModel):
+    """Base Network Config Model Class"""
+
+    class Config:
+        extra = Extra.forbid
+
+    def check(self):
+        *_, validation_error = validate_model(self.__class__, self.__dict__)
+        if validation_error:
+            raise validation_error
+
+    def yaml(self, indent: int = 2, **kwargs):
+        data_dict = self.dict(**kwargs)
+        return yaml.dump(data=data_dict, Dumper=CustomYamlDumper, indent=indent)
+
+    def serial_dict(self, **kwargs):
+        return json.loads(self.json(**kwargs))
+
+
+class VendorIndependentBaseModel(BaseNetCmModel):
+    """Vendor Independent Base Model Class"""
+
+    pass
---
+++ packages/net-models/net_models-0.1.3-py3-none-any.whl/net_models/models/services/vi/ServerModels.py
@@ -0,0 +1,93 @@
+import ipaddress
+from pydantic.typing import Optional, Union, List, Literal
+from pydantic import conint, root_validator
+from net_models.models.BaseModels import VendorIndependentBaseModel
+from net_models.models.BaseModels.SharedModels import KeyBase, AuthBase
+from net_models.models.Fields import GENERIC_OBJECT_NAME, VRF_NAME, BASE_INTERFACE_NAME
+
+
+class ServerBase(VendorIndependentBaseModel):
+
+    pass
+
+
+class ServerPropertiesBase(ServerBase):
+
+    server: Union[ipaddress.IPv4Address, ipaddress.IPv6Address]
+    src_interface: Optional[BASE_INTERFACE_NAME]
+    vrf: Optional[VRF_NAME]
+
+
+class NtpKey(KeyBase):
+
+    key_id: int
+    method: Literal["md5"]
+    trusted: Optional[bool]
+
+
+class NtpServer(ServerPropertiesBase):
+
+    key_id: Optional[int]
+    prefer: Optional[bool]
+
+
+class NtpConfig(VendorIndependentBaseModel):
+
+    authenticate: Optional[bool]
+    servers: Optional[List[NtpServer]]
+    peers: Optional[List[NtpServer]]
+    keys: Optional[List[NtpKey]]
+    src_interface: Optional[BASE_INTERFACE_NAME]
+
+
+class LoggingServer(ServerPropertiesBase):
+
+    protocol: Optional[Literal["tcp", "udp"]]
+    port: Optional[int]
+
+
+class AaaServer(ServerBase):
+
+    name: GENERIC_OBJECT_NAME
+    server: Union[ipaddress.IPv4Address, ipaddress.IPv6Address]
+    address_version: Optional[Literal["ipv4", "ipv6"]]
+    timeout: Optional[conint(ge=1)]
+    key: KeyBase
+    single_connection: Optional[bool]
+
+    @root_validator(allow_reuse=True)
+    def generate_address_version(cls, values):
+        if not values.get("address_version"):
+            if isinstance(values.get("server"), ipaddress.IPv4Address):
+                values["address_version"] = "ipv4"
+            elif isinstance(values.get("server"), ipaddress.IPv6Address):
+                values["address_version"] = "ipv6"
+        return values
+
+
+class RadiusServer(AaaServer):
+
+    retransmit: Optional[conint(ge=1)]
+
+
+class TacacsServer(AaaServer):
+
+    pass
+
+
+class AaaServerGroup(ServerBase):
+
+    name: GENERIC_OBJECT_NAME
+    src_interface: Optional[BASE_INTERFACE_NAME]
+    vrf: Optional[VRF_NAME]
+
+
+class RadiusServerGroup(AaaServerGroup):
+
+    servers: List[RadiusServer]
+
+
+class TacacsServerGroup(AaaServerGroup):
+
+    servers: List[TacacsServer]
+
