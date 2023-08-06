7fb200913c599b5010b788195f1bb663d6c0ec22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:02 +0000

    Add package cfwp/0.0.1/cfwp-0.0.1-py3-none-any.whl

---
+++ packages/cfwp/cfwp-0.0.1-py3-none-any.whl/codefast/__init__.py
@@ -0,0 +1,28 @@
+import builtins
+import sys
+
+import codefast.utils as utils
+from codefast.logger import Logger
+
+builtins._str = utils._str
+builtins.io = utils.FileIO
+builtins.say = io.say
+
+# Export methods and variables
+json = utils.JsonIO
+text = utils.FileIO
+file = utils.FileIO
+csv = utils.CSVIO
+net = utils.Network
+os = utils._os()
+
+format = utils.FormatPrint
+logger = Logger()
+info = logger.info
+error = logger.error
+warning = logger.warning
+
+shell = utils.shell
+
+sys.modules[__name__] = utils.wrap_mod(sys.modules[__name__],
+                                       deprecated=['text', 'file'])
