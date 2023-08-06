93b5f5e86e3bf47feb9ae01ad5ecd4cd30d9b2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:17 +0000

    Add package dnsuptools/0.0.23.post21/dnsuptools-0.0.23.post21.tar.gz

---
+++ packages/dnsuptools/dnsuptools-0.0.23.post21.tar.gz/dnsuptools-0.0.23.post21/dnsuptools/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- encoding: UTF8 -*-
+
+from simpleloggerplus import simpleloggerplus as log
+
+
+
+try:
+    import git
+    from git.exc import InvalidGitRepositoryError
+    repo = git.Repo(search_parent_directories=False)
+    __version__ = '-'.join(repo.git.describe('--tags').split('-')[:2])
+    with open('version', 'wt') as v:
+        v.write(__version__)
+#except ImportError as e:
+except Exception as e:
+    try:
+        with open('version', 'rt') as v:
+            __version__ = v.read()
+    except OSError as e:
+        log.warn("Can't open version file.")
+        __version__ = ''
+#except InvalidGitRepositoryError as e:
+
+
+
