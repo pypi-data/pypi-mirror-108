a535ae450eb883364e31d6ce43b3df7942f29c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:57 +0000

    Add package TS-distributions/1.1/TS_distributions-1.1.tar.gz

---
+++ packages/TS-distributions/TS_distributions-1.1.tar.gz/TS_distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='TS_distributions',
+      version='1.1',
+      description='Gaussian & Binomial distributions',
+      packages=['TS_distributions'],
+      author = 'Tarun Saxena',
+      author_email = 'tarunsaxena16@gmail.com',
+      zip_safe=False)
