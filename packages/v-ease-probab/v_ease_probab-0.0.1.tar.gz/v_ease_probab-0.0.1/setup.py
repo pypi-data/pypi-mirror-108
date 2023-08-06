cd39afb836e3403adfc9d5f200a196597384544e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:15 +0000

    Add package v-ease-probab/0.0.1/v_ease_probab-0.0.1.tar.gz

---
+++ packages/v-ease-probab/v_ease_probab-0.0.1.tar.gz/v_ease_probab-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='v_ease_probab',
+      version='0.0.1',
+      description='Gaussian distributions',
+      packages=['v_ease_probab'],
+      author='Soheb',
+      author_email='sksoheb54@gmail.com',
+    zip_safe=False)
