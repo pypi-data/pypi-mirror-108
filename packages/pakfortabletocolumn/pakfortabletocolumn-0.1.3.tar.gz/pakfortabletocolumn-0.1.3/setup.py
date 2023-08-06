561cf1a4737bf4584fffe3926e34356100122fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:01 +0000

    Add package pakfortabletocolumn/0.1.3/pakfortabletocolumn-0.1.3.tar.gz

---
+++ packages/pakfortabletocolumn/pakfortabletocolumn-0.1.3.tar.gz/pakfortabletocolumn-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pakfortabletocolumn',
+    version='0.1.3',    
+    description='table to column',
+    url='https://github.com/susaninsl/pakfortabletocolumn',
+    author='Sviat',
+    author_email='susaninsl@gmail.com',
+    license='BSD 2-clause',
+    packages=['pakfortabletocolumn'],
+    entry_points ={
+        'console_scripts': [
+            'pftc = pakfortabletocolumn.tabletocolumn:main'
+        ]
+    },
+    install_requires=['xlrd',
+                      'xlwt',
+                      'argparse',
+                      'xlutils',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',     
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
