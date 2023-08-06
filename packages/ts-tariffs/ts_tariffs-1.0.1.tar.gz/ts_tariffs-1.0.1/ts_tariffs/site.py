a6200a6b23150329c2fa747682a5138870bbf07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:07 +0000

    Add package ts-tariffs/1.0.1/ts_tariffs-1.0.1.tar.gz

---
+++ packages/ts-tariffs/ts_tariffs-1.0.1.tar.gz/ts_tariffs-1.0.1/ts_tariffs/site.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+import pandas as pd
+from time import time
+from typing import Type
+
+from tariffs import Charge, TariffRegime
+
+
+@dataclass
+class MeterData:
+    name: str
+    meter_ts: pd.DataFrame
+    units: str
+
+    def set_sample_rate(self, sample_rate):
+        self.meter_ts = self.meter_ts.resample(sample_rate).sum()
+
+
+@dataclass
+class Site:
+    name: str
+    tariffs: TariffRegime
+    meter_data: MeterData
+    bill_ledgers: dict[pd.DataFrame] = None
+    bill: dict[float] = None
+
+    def __post_init__(self):
+
+        self.meter_data.set_sample_rate(self.tariffs.metering_sample_rate)
+
+    def calculate_bill(self):
+        self.bill_ledgers = {}
+        self.bill = {}
+        for charge in self.tariffs.charges:
+            self.bill_ledgers[charge.name] = charge.apply_charge(
+                self.meter_data.meter_ts
+            )
+            self.bill[charge.name] = self.bill_ledgers[charge.name]['bill'].sum()
