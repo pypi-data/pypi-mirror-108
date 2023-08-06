55637a2779efb319de922bc0b29e76048aec3423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:38 +0000

    Add package mypy-boto3-finspace/1.17.88/mypy-boto3-finspace-1.17.88.tar.gz

---
+++ packages/mypy-boto3-finspace/mypy-boto3-finspace-1.17.88.tar.gz/mypy-boto3-finspace-1.17.88/mypy_boto3_finspace/literals.py
@@ -0,0 +1,37 @@
+"""
+Type annotations for finspace service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_finspace.literals import EnvironmentStatusType
+
+    data: EnvironmentStatusType = "CREATE_REQUESTED"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("EnvironmentStatusType", "FederationModeType")
+
+
+EnvironmentStatusType = Literal[
+    "CREATED",
+    "CREATE_REQUESTED",
+    "CREATING",
+    "DELETED",
+    "DELETE_REQUESTED",
+    "DELETING",
+    "FAILED_CREATION",
+    "FAILED_DELETION",
+    "RETRY_DELETION",
+    "SUSPENDED",
+]
+FederationModeType = Literal["FEDERATED", "LOCAL"]
