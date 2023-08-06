ea1486704d3f718aecd1024713c955a5505d299b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:13 +0000

    Add package simplematrixbotlib/1.0.1/simplematrixbotlib-1.0.1-py3-none-any.whl

---
+++ packages/simplematrixbotlib/simplematrixbotlib-1.0.1-py3-none-any.whl/simplematrixbotlib/__init__.py
@@ -0,0 +1,3 @@
+from simplematrixbotlib.api import API
+from simplematrixbotlib.auth import Creds
+from simplematrixbotlib.bot import Bot
---
+++ packages/simplematrixbotlib/simplematrixbotlib-1.0.1-py3-none-any.whl/simplematrixbotlib/api.py
@@ -0,0 +1,21 @@
+import asyncio
+from nio import (AsyncClient, SyncResponse, RoomMessageText)
+
+class API:
+    """
+    A class to interact with the matrix-nio library.
+    """
+    def __init__(self, creds):
+        self.creds = creds
+
+    async def login(self):
+        """
+        Login the client to the homeserver
+        """
+        self.async_client = AsyncClient(
+            self.creds.homeserver,
+            self.creds.username
+        )
+        
+        response = await self.async_client.login(self.creds.password)
+        print(response)
