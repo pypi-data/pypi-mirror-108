1945fa9d4f771600df2f6427290476858a90091e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:49 +0000

    Add package ZxSci/0.2.0/ZxSci-0.2.0.tar.gz

---
+++ packages/ZxSci/ZxSci-0.2.0.tar.gz/ZxSci-0.2.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ZxSci",
+    version="0.2.0",
+    author="Larry",
+    author_email="1140091006@qq.com",
+    description="一个帮助理工科本科学生进行简化计算的库",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    license="MIT Licence",
+    packages=setuptools.find_packages("自制库/ZxSci"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # include_package_data = True,
+    # platforms = "any",
+    # install_requires = ['chardet']
+
+)
