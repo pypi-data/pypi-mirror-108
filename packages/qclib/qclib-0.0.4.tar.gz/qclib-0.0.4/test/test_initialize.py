5249570706700736b50a0015b8483bd2e38df16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:45 +0000

    Add package qclib/0.0.4/qclib-0.0.4.tar.gz

---
+++ packages/qclib/qclib-0.0.4.tar.gz/qclib-0.0.4/test/test_initialize.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+import numpy as np
+from qclib.state_preparation import initialize
+from qclib.util import get_state
+
+
+class TestInitialize(TestCase):
+    def test_initialize(self):
+        a = np.random.rand(32) + np.random.rand(32) * 1j
+        a = a / np.linalg.norm(a)
+
+        circ = initialize(a)
+
+        state = get_state(circ)
+
+        self.assertTrue(np.allclose(a, state))
