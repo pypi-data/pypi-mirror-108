cf51cd63f829a3612bfd56ca30acfb7164aec92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:56 +0000

    Add package bntransformer/2.0.1/bntransformer-2.0.1-py3-none-any.whl

---
+++ packages/bntransformer/bntransformer-2.0.1-py3-none-any.whl/bntransformer/__init__.py
@@ -0,0 +1,9 @@
+__version__="2.0.1"
+
+from bntransformer.pipeline import (
+    BanglaQA,
+    BanglaNER,
+    BanglaMaskGeneration,
+    BanglaTranslation,
+)
+from bntransformer.tokenizer import BanglaTokenizer
