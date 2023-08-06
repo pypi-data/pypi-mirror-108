36250eb6fb86bd659b5cac98664909227bbdb469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:19 +0000

    Add package sciencewriter/0.0.1/sciencewriter-0.0.1-py2.py3-none-any.whl

---
+++ packages/sciencewriter/sciencewriter-0.0.1-py2.py3-none-any.whl/sciencewriter/sciencewriter.py
@@ -0,0 +1,24 @@
+"""Main module for the ScienceWrite application model."""
+
+from sciencewriter._api import *
+from sciencewriter.informationmodel import SciencePaper, AuthorInfo
+
+class ScienceWriterApp():
+    """This class represents the application as a whole that exposes the API for other derived works to access and to manipulate the internals of the writing project.
+    """
+    def __init__(self) -> None:
+        self.papers:list[SciencePaper] = None
+        self.location:str = None
+        self.author_infos:dict[str,AuthorInfo] = None
+        pass
+
+    def persist_information(self):
+        """This function takes the internal state of the application and persists it to a location from where it can be instantiated at a later time.
+        """
+        pass
+
+    def reload_information(self):
+        """This function takes an information state persisted at some location and it will instantiate the application based on that information.
+        """
+        pass
+
