d810f0b2373877ed1e788205956dda38179d94b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:33 +0000

    Add package saruca-uuv/0.0.9/saruca_uuv-0.0.9.tar.gz

---
+++ packages/saruca-uuv/saruca_uuv-0.0.9.tar.gz/saruca_uuv-0.0.9/setup.py
@@ -0,0 +1,23 @@
+
+from distutils.core import setup
+setup(
+  name = 'saruca_uuv',
+  packages = ['saruca_uuv'],
+  version = '0.0.9',
+  license='MIT',
+  description = 'Autonomous and manual control methods for UUVs',
+  author = 'SEMIH SAHIN',
+  author_email = 'esemihsahin@hotmail.com',
+  url = 'https://github.com/SARUCA-YazilimEkibi/saruca_uuv',
+  keywords = ['UUV', 'AUV', 'UNDERWATER'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
