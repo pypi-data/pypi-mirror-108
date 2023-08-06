c4ebaa74ec8b06e4ba36d19fcdf7d61f9b90c732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:03 +0000

    Add package pytest-timestamper/0.0.6/pytest-timestamper-0.0.6.tar.gz

---
+++ packages/pytest-timestamper/pytest-timestamper-0.0.6.tar.gz/pytest-timestamper-0.0.6/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="pytest-timestamper",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={"pytest11": ["name_of_plugin = pytest_timestamper.plugin"]},
+    classifiers=["Framework :: Pytest"],
+    use_scm_version={"write_to": "src/pytest_timestamper/_version.py"},
+)
