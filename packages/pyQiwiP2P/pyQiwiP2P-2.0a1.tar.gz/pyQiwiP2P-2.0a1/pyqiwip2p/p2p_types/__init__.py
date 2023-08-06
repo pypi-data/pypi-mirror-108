20ed82007c7da7479ffa16c53d919f85f79dcdba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:36 +0000

    Add package pyQiwiP2P/2.0a1/pyQiwiP2P-2.0a1.tar.gz

---
+++ packages/pyQiwiP2P/pyQiwiP2P-2.0a1.tar.gz/pyQiwiP2P-2.0a1/pyqiwip2p/p2p_types/__init__.py
@@ -0,0 +1,5 @@
+from pyqiwip2p.p2p_types.Customer import *
+from pyqiwip2p.p2p_types.QiwiDatetime import *
+from pyqiwip2p.p2p_types.Errors import *
+from pyqiwip2p.p2p_types.Responses import *
+from pyqiwip2p.p2p_types.AioResponses import *
