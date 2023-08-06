efd1684faed84536bdee69836dc08e3903baa6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:26 +0000

    Add package test-pypi-action/3.1.1/test-pypi-action-3.1.1.tar.gz

---
+++ packages/test-pypi-action/test-pypi-action-3.1.1.tar.gz/test-pypi-action-3.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt", "r") as req_fp:
+    required_packages = req_fp.readlines()
+
+# Use README for long description
+with open("README.md", "r") as readme_fp:
+    long_description = readme_fp.read()
+
+setup(
+    name="test-pypi-action",
+    author="Pablo Lecolinet",
+    author_email="pablolec@pm.me",
+    description="To be deleted.",
+    license="GNU GPLv3",
+    keywords="",
+    url="https://github.com/PabloLec/test-pypi-action",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests", "docs"]),
+    entry_points={},
+    install_requires=required_packages,
+    package_data={},
+    include_package_data=True,
+    classifiers=[],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+)
