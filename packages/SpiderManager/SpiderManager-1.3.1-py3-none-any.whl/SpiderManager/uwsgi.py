d2c1a28e04b488cce125f7d8e9ebfb1bdf5fe5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:41 +0000

    Add package SpiderManager/1.3.1/SpiderManager-1.3.1-py3-none-any.whl

---
+++ packages/SpiderManager/SpiderManager-1.3.1-py3-none-any.whl/SpiderManager/uwsgi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time      : 2017-09-24 14:53
+# @Author    : modm
+'''
+you can start the server by uwsgi
+like gunicorn -w 4 SpiderManager.uwsgi:app
+'''
+from SpiderManager.app import initialize
+
+initialize()
