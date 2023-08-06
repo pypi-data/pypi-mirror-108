8ea5f7ac1029f149707335e6051312f28e4f99a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:44 +0000

    Add package PyJ2L/1.0/PyJ2L-1.0.tar.gz

---
+++ packages/PyJ2L/PyJ2L-1.0.tar.gz/PyJ2L-1.0/PyJ2L/__init__.py
@@ -0,0 +1,6 @@
+from PyJ2L.PyJ2L import htm
+from PyJ2L.PyJ2L import HandProjects
+from PyJ2L.PyJ2L import Face
+from PyJ2L.PyJ2L import Posture
+from PyJ2L.PyJ2L import VideoExamples
+from PyJ2L.PyJ2L import Make
