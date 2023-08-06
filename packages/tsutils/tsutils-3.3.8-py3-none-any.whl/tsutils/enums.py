f98023659471b2e00949e36a14128affc4679a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:36 +0000

    Add package tsutils/3.3.8/tsutils-3.3.8-py3-none-any.whl

---
+++ packages/tsutils/tsutils-3.3.8-py3-none-any.whl/tsutils/enums.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class Server(Enum):
+    COMBINED = "COMBINED"
+    JP = "JP"
+    NA = "NA"
+    KR = "KR"
+
+
+class EvoToFocus(Enum):
+    newest = 0
+    naprio = 1
+
+
+class AltEvoSort(Enum):
+    dfs = 0
+    numerical = 1
+
+
+class ChildMenuSelector(Enum):
+    IdMenu = 0
+    NaDiffMenu = 1
+    AwakeningList = 2
