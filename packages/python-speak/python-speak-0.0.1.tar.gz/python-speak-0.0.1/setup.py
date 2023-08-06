32bbb5126ff6fa5976d45aa17cce8c8262942eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:16 +0000

    Add package python-speak/0.0.1/python-speak-0.0.1.tar.gz

---
+++ packages/python-speak/python-speak-0.0.1.tar.gz/python-speak-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import  setuptools
+
+setuptools.setup(
+    name="python-speak",
+    version="0.0.1",
+    author="轩轩",
+    author_email="3603695237@qq.com",
+    description="python speak,You can use it to convert text into speech",
+    url="",
+    py_modules=['speak\\readme','speak\\speak'],
+)
---
+++ packages/python-speak/python-speak-0.0.1.tar.gz/python-speak-0.0.1/speak/readme.py
@@ -0,0 +1,29 @@
+#--------------------------------------------------------------------------------------------------#
+#请将speak.py重命名改为speak.exe                                                                   #
+#请将软件加入注册表单后使用                                                                        #
+#打开cmd，输入：path=%path%;本文件路径 ，例如：path=%path%;C:\Python                               #
+#                                                                                                  #
+#                                                                                                  #
+#Please add the software to the registration form                                                  #
+#Open CMD and enter: path =% path%; The path of this file, for example: path = path%; C:\speak     #
+#                                                                                                  #
+#                                                                                                  #
+#                                                                                                  #
+#--------------------------------------------------------------------------------------------------#
+
+a="""
+#--------------------------------------------------------------------------------------------------#
+#请将speak.py重命名改为speak.exe                                                                   #
+#请将软件加入注册表单后使用                                                                        #
+#打开cmd，输入：path=%path%;本文件路径 ，例如：path=%path%;C:\Python                               #
+#                                                                                                  #
+#                                                                                                  #
+#Please add the software to the registration form                                                  #
+#Open CMD and enter: path =% path%; The path of this file, for example: path = path%; C:\speak     #
+#                                                                                                  #
+#                                                                                                  #
+#                                                                                                  #
+#--------------------------------------------------------------------------------------------------#
+"""
+
+print(a)
