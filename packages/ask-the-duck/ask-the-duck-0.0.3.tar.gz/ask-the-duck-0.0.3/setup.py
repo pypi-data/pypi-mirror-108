b424a11fe9541cdc278ef5634c997628601c79c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:53 +0000

    Add package ask-the-duck/0.0.3/ask-the-duck-0.0.3.tar.gz

---
+++ packages/ask-the-duck/ask-the-duck-0.0.3.tar.gz/ask-the-duck-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='ask-the-duck',
+    version='0.0.3',
+    packages=['ask_the_duck'],
+    url='https://github.com/HelloChatterbox/ask-the-duck',
+    license='apache-2.0',
+    author='jarbasAi',
+    install_requires=["requests",
+                      "requests_cache",
+                      "google_trans_new",
+                      "quebra_frases",
+                      "RAKEkeywords",
+                      "simplematch"],
+    author_email='jarbasai@mailfence.com',
+    description='duck duck go library'
+)
