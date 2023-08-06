27ef2f734ffbac777c8283fc50a6d73aa95dde19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package drf-nested-forms/1.0.0/drf_nested_forms-1.0.0.tar.gz

---
+++ packages/drf-nested-forms/drf_nested_forms-1.0.0.tar.gz/drf_nested_forms-1.0.0/drf_nested_forms/__init__.py
@@ -0,0 +1,12 @@
+__title__ = 'drf_nested_formdata'
+__version__ = '1.0.0'
+__author__ = 'Duke Effiom'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021'
+
+# Version synonym
+from .exceptions import ParseException
+from .parsers import NestedMultiPartParser, NestedJSONParser
+from .mixins import UtilityMixin
+from .utils import NestedForms
+VERSION = __version__
