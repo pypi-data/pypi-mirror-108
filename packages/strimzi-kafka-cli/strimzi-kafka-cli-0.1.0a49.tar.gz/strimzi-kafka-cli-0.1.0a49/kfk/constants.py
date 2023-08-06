70142e62c9506d72e1ca63cb3e31422945854c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:15 +0000

    Add package strimzi-kafka-cli/0.1.0a49/strimzi-kafka-cli-0.1.0a49.tar.gz

---
+++ packages/strimzi-kafka-cli/strimzi-kafka-cli-0.1.0a49.tar.gz/strimzi-kafka-cli-0.1.0a49/kfk/constants.py
@@ -0,0 +1,18 @@
+SPACE = " "
+COLON = ":"
+SEMICOLON = ";"
+AMPERSAND = "&"
+NEW_LINE = "\n"
+KAFKA_PORT = "9092"
+KAFKA_SECURE_PORT = "9093"
+KAFKA_SSL = "SSL"
+COMMON_NAME_PREFIX = "CN="
+BROKER_TMP_FOLDER_PATH = "/tmp"
+
+
+class SpecialTexts:
+    BROKER_CONFIG_FILE_USER_CONFIG_HEADER = "\n##########\n# User provided configuration\n##########\n"
+    CONNECT_BOOTSTRAP_SERVERS = "bootstrap.servers"
+    CONNECT_OUTPUT_IMAGE = "output.image"
+    CONNECT_PLUGIN_URL = "plugin.url"
+    CONNECT_PLUGIN_PATH = "plugin.path"
