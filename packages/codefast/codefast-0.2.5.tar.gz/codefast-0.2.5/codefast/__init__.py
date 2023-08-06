1131e2d46f6dfa0c2e232d315e5afe3b49cc1f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:20 +0000

    Add package codefast/0.2.5/codefast-0.2.5.tar.gz

---
+++ packages/codefast/codefast-0.2.5.tar.gz/codefast-0.2.5/codefast/__init__.py
@@ -0,0 +1,28 @@
+import builtins
+import sys
+
+import codefast.utils as utils
+from codefast.logger import Logger
+
+builtins._str = utils._str
+
+# Export methods and variables
+json = utils.JsonIO
+text = utils.FileIO
+file = utils.FileIO
+csv = utils.CSVIO
+net = utils.Network
+os = utils._os()
+
+p = utils.p
+pp = utils.pp
+say = utils.FileIO.say
+format = utils.FormatPrint
+logger = Logger()
+info = logger.info
+error = logger.error
+
+shell = utils.shell
+
+sys.modules[__name__] = utils.wrap_mod(sys.modules[__name__],
+                                       deprecated=['text'])
