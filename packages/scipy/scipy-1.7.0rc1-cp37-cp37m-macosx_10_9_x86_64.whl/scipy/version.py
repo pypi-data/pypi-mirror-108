7d81d4264750f63c0460f0ce948c1ec2e6d65706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:44 +0000

    Add package scipy/1.7.0rc1/scipy-1.7.0rc1-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/scipy/scipy-1.7.0rc1-cp37-cp37m-macosx_10_9_x86_64.whl/scipy/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.7.0rc1'
+version = '1.7.0rc1'
+full_version = '1.7.0rc1'
+git_revision = '9b04f4d'
+commit_count = '1209'
+release = True
+
+if not release:
+    version = full_version
