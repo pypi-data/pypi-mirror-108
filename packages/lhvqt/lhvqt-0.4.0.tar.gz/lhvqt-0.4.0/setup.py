742adc7661955a0a42e4d9d0f318bc708d8ecff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package lhvqt/0.4.0/lhvqt-0.4.0.tar.gz

---
+++ packages/lhvqt/lhvqt-0.4.0.tar.gz/lhvqt-0.4.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lhvqt',
+    url='https://github.com/cwitkowitz/LHVQT',
+    author='Frank Cwitkowitz',
+    author_email='fcwitkow@ur.rochester.edu',
+    packages=['lhvqt'],
+    install_requires=['numpy', 'librosa', 'torch', 'matplotlib', 'soundfile'],
+    version='0.4.0',
+    license='MIT',
+    description='Frontend filterbank learning module with HVQT initialization capabilities',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
