03faeb498acaeacfc256b0fc34a9cb4ae493a445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:28 +0000

    Add package panel/0.12.0a13/panel-0.12.0a13-py2.py3-none-any.whl

---
+++ packages/panel/panel-0.12.0a13-py2.py3-none-any.whl/panel/tests/template/fast/test_fast_list_template.py
@@ -0,0 +1,41 @@
+import panel as pn
+
+from panel.template.fast.list import FastListTemplate, FastListDarkTheme
+
+from panel.tests.template.fast.test_fast_grid_template import (
+    _create_hvplot,
+    _fast_button_card,
+    _sidebar_items,
+    INFO,
+)
+
+def test_template_theme_parameter():
+    template = FastListTemplate(title="Fast", theme="dark")
+    # Not '#3f3f3f' which is for the Vanilla theme
+
+    doc = template.server_doc()
+    assert doc.theme._json['attrs']['Figure']['background_fill_color']=="#181818"
+
+    assert isinstance(template._get_theme(), FastListDarkTheme)
+
+
+def test_app():
+    app = FastListTemplate(
+        title="FastListTemplate",
+        site="Panel",
+        site_url="https://panel.holoviz.org",
+    )
+    app.main[:] = [
+        pn.pane.Markdown(INFO, sizing_mode="stretch_both"),
+        pn.pane.HoloViews(_create_hvplot(), sizing_mode="stretch_both"),
+        _fast_button_card(),
+        pn.pane.HoloViews(_create_hvplot(), sizing_mode="stretch_both"),
+    ]
+    app.sidebar.extend(_sidebar_items())
+
+    return app
+
+
+if __name__.startswith("bokeh"):
+    pn.extension(sizing_mode="stretch_width")
+    test_app().servable()
