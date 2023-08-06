889fea4627e9237637bc797c8edda094ea4ad0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:01 +0000

    Add package randomstuff.py/1.4.2/randomstuff.py-1.4.2-py3-none-any.whl

---
+++ packages/randomstuff.py/randomstuff.py-1.4.2-py3-none-any.whl/randomstuff/constants.py
@@ -0,0 +1,10 @@
+BASE_URL = 'https://api.pgamerx.com'
+IMAGE_TYPES = ['aww', 'duck', 
+	'dog', 'cat', 
+	'memes', 'dankmemes', 
+	'holup', 'art', 
+	'harrypottermemes','facepalm']
+JOKE_TYPES = ['any', 'dev', 'spooky', 'pun']
+PLANS = ['', 'pro', 'ultra', 'biz', 'mega']
+VERSIONS = ['3', '4']
+SERVERS = ['primary', 'backup', 'unstable']
