8debf1fb36e40cebe80ae97dc906a9f964c4efda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:17 +0000

    Add package cowin-app-services/0.0.3/cowin-app-services-0.0.3.tar.gz

---
+++ packages/cowin-app-services/cowin-app-services-0.0.3.tar.gz/cowin-app-services-0.0.3/cowin_api/base_api.py
@@ -0,0 +1,42 @@
+from typing import Union
+
+import requests
+from fake_useragent import UserAgent
+from requests.exceptions import HTTPError
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+import os
+from cowin_api.constants import Constants
+
+
+class BaseApi:
+
+    def _call_api(self, url) -> Union[HTTPError, dict]:
+        user_agent = UserAgent()
+        # parameters for retry
+        retry_strategy = Retry(
+            total=Constants.total_retries,
+            backoff_factor=Constants.backoff_factor, # default is 0 and must be non zero
+            status_forcelist=[429, 500, 502, 503, 504],
+            method_whitelist=["GET"]
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        http = requests.Session()
+        http.mount("https://", adapter)
+        http.mount("http://", adapter)
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36'}
+
+
+        response = http.get(url, headers=headers)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            # return an empty dictionary for consistency downstream
+            print(f'[ERROR] {e}')
+            return {}
+        except e:
+            print(f'[ERROR] {e}')
+            return {}
+
+        return response.json()
