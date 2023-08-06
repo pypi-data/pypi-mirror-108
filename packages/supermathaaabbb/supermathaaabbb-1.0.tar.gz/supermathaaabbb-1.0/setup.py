a3acfd982f41fe88710c5f300554b2dde4285607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:01 +0000

    Add package supermathaaabbb/1.0/supermathaaabbb-1.0.tar.gz

---
+++ packages/supermathaaabbb/supermathaaabbb-1.0.tar.gz/supermathaaabbb-1.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+# coding: UTF-8
+from distutils.core import setup
+setup(name='supermathaaabbb',
+      version='1.0',
+      description='这是本公司对外发布的测试模块',
+      author='zwl123456789',
+      author_email='zhaowanli@mails.ccnu.edu.cn ',
+      py_modules=['supermath.a1model','supermath.a2model']
+      )
