fbb46c9b53a4cfd41fae9a94d1928e379e27b613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:12 +0000

    Add package dnsuptools/0.0.23.post25/dnsuptools-0.0.23.post25.tar.gz

---
+++ packages/dnsuptools/dnsuptools-0.0.23.post25.tar.gz/dnsuptools-0.0.23.post25/dnsuptools/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- encoding: UTF8 -*-
+
+import os
+
+# VERSION
+__version__ = "0.0.23-25"
+
+try:
+    import git
+    #from git.exc import InvalidGitRepositoryError
+    repo = git.Repo(search_parent_directories=False)
+    __version__ = '-'.join(repo.git.describe('--tags').split('-')[:2])
+    print(__file__)
+    print(os.path.realpath(__file__))
+    with open(os.path.realpath(__file__), 'rt') as v:
+        print(__file__)
+        initfile = v.read()
+        lines = initfile.splitlines()
+        bg = [i for i, e in enumerate(lines) if 'VERSION' in e][0]
+        print(bg)
+        lines[bg+1] = "__version__ = \"{}\"".format(__version__)
+        initfile = '\n'.join(lines)
+    with open(os.path.realpath(__file__), 'wt') as v:
+        v.write(initfile)
+#except ImportError as e:
+except Exception as e:
+    print(e)
