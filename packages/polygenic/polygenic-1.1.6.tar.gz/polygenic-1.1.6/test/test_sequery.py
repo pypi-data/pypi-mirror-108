08f05972baa7813dac3bfa7e097efabe86d70ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:40 +0000

    Add package polygenic/1.1.6/polygenic-1.1.6.tar.gz

---
+++ packages/polygenic/polygenic-1.1.6.tar.gz/polygenic-1.1.6/test/test_sequery.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from polygenic import polygenic
+
+#class TestSequery(TestCase):
+#    def testCommandLine(self):
+#        polygenic.main(["--vcf", "/home/marpiech/data/clustered_204800980122_R01C02.vcf.gz", "--af", "/home/marpiech/data/af.vcf.gz", "--log_file", "/dev/null", "--models_and_descriptions_path", "/tmp/polygenic/models", "--population", "eas", "--out_dir", "/tmp/polygenic"])
+#        self.assertEqual('1', '1')
+
+class TestSequeryShort(TestCase):
+    def testCommandLine(self):
+        polygenic.main(["--version"])
+        self.assertEqual('1', '1')
+
+if __name__ == "__main__":
+    unittest.main()
