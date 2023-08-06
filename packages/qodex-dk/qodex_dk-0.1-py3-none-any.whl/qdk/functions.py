c1a9e9c4bd9e27b003bab4a3bb7d5a36695774e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:22 +0000

    Add package qodex-dk/0.1/qodex_dk-0.1-py3-none-any.whl

---
+++ packages/qodex-dk/qodex_dk-0.1-py3-none-any.whl/qdk/functions.py
@@ -0,0 +1,25 @@
+def extract_supported_methods(sdk_cliend):
+    """ Извлечь все методы из самого API """
+    command = {'get_methods': None}
+    sdk_cliend.send_data(command)
+    response = sdk_cliend.get_data()
+    return response
+
+
+def execute_method(sdk_client, methods_dict, method_name, *args, **kwargs):
+    """ Сформировать и отправить запрос """
+    method = get_method(methods_dict, method_name, *args, **kwargs)
+    sdk_client.send_data(method)
+
+
+def get_method(methods_dict, method_name, *args, **kwargs):
+    """ Сделать шаблонный запрос  """
+    try:
+        method_from_dict = methods_dict[method_name]
+    except KeyError:
+        return {'status': 'failed', 'info': 'Метод {} не поддерживается в QDK. Что бы получить список поддреживаемых '
+                                            'методов выполните get_qdk_methods.'.format(method_name)}
+    all_arguments = method_from_dict['kwargs']
+    all_arguments.update(kwargs)
+    method = {'method': method_from_dict['command'],  'data': all_arguments}
+    return method
