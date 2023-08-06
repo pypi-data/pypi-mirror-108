2d5592eb3cc4c2e8fe975a78bf02ba8196e33cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:41 +0000

    Add package pymakeapps/0.5/pymakeapps-0.5.tar.gz

---
+++ packages/pymakeapps/pymakeapps-0.5.tar.gz/pymakeapps-0.5/pymakeapps/__init__.py
@@ -0,0 +1,5 @@
+from pymakeapps.pymakeapps import htm
+from pymakeapps.pymakeapps import HandProjects
+from pymakeapps.pymakeapps import Face
+from pymakeapps.pymakeapps import Posture
+from pymakeapps.pymakeapps import VideoExamples
