9b9b5e504f1c93773d11241fa26e4594fa44ebb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:07 +0000

    Add package roa-ai-model/1.0.0/roa_ai_model-1.0.0-py2-none-any.whl

---
+++ packages/roa-ai-model/roa_ai_model-1.0.0-py2-none-any.whl/src/version_check.py
@@ -0,0 +1,21 @@
+def main () : 
+    
+    print("")
+    print("Hello This Library is ROA AI TEAM")
+    print("")
+
+    print("----------------------------------------------------------------")
+    print("")
+    print("Made By...")
+    print("")
+    print("Kim Kyoung Hoon    CTO               sanghee@roa.ai")
+    print("Park Sang Hee      ML Engineer       sanghee@roa.ai")
+    print("Lee Jae Seung      Data Scientist    sanghee@roa.ai")
+    print("Park Sleuki        Data Scientist    sanghee@roa.ai")
+    print("Choi Moon Seok     Data Manager      sanghee@roa.ai")
+    print("Kim Hyun Sun       Data Scientist    sanghee@roa.ai")
+    print("Kim Joon Han       Data Scientist    sanghee@roa.ai")
+    print("")
+
+if __name__ == '__main__':
+    main()
