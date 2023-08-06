3e5ce8dc4162fda9f48fe9798f036ad6ceab7cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:49 +0000

    Add package dialogy/0.6.4/dialogy-0.6.4.tar.gz

---
+++ packages/dialogy/dialogy-0.6.4.tar.gz/dialogy-0.6.4/dialogy/parser/text/entity/duckling_parser.py
@@ -0,0 +1,6 @@
+"""
+.. deprecated:: DucklingParser will be deprecated, please use DucklingPlugin instead. Please use DucklingPlug
+"""
+from dialogy.preprocess.text.duckling_plugin import DucklingPlugin
+
+DucklingParser = DucklingPlugin
