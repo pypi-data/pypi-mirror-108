be562da4d35a4d22f32e825df5ceb44cbcc5871c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:50 +0000

    Add package MeetJoin/0.2/MeetJoin-0.2.tar.gz

---
+++ packages/MeetJoin/MeetJoin-0.2.tar.gz/MeetJoin-0.2/README.txt
@@ -0,0 +1,7 @@
+This is a simple google meet bot that can attend your lectures. 
+It is an open source library that can be used by anyone. 
+Anyone can give me suggestion on my mail id so that I can improve my package.
+
+This is package is only for education purposes please dont misuse it.
+
+Technology is the weapon for the future it is in your hand you use it or misuse it. -APJ Abdul Kalam
---
+++ packages/MeetJoin/MeetJoin-0.2.tar.gz/MeetJoin-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.2'
+DESCRIPTION = 'A Sinple Google Meet Bot'
+LONG_DESCRIPTION = 'A package that can attend your google meet classes at different time with different links.'
+
+# Setting up
+setup(
+    name="MeetJoin",
+    version=VERSION,
+    author="IronMan (Hridhan Dwivedi)",
+    author_email="<dwivedihridhan@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['MeetJoin','selenium', 'datetime', 'pynput', 'pause'],
+    keywords=['python', 'bot', 'google meet', 'online classes', 'selenium'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
