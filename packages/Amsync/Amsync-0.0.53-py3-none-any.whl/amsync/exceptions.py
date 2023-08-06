35b60ed9a2e22139ce7f08157fdaca6295005d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:13 +0000

    Add package Amsync/0.0.53/Amsync-0.0.53-py3-none-any.whl

---
+++ packages/Amsync/Amsync-0.0.53-py3-none-any.whl/amsync/exceptions.py
@@ -0,0 +1,14 @@
+class SmallReasonForBan(Exception): pass
+class AccountNotFoundInDotenv(Exception): pass
+class InvalidDotenvKeys(Exception): pass
+class CommandIsntAsync(Exception): pass
+class EventIsntAsync(Exception): pass
+class InvalidChatChoice(Exception): pass
+class InvalidEvent(Exception): pass
+class InvalidFileType(Exception): pass
+class InvalidReturn(Exception): pass
+class AminoSays(Exception): pass
+class EmptyCom(Exception): pass
+class InvalidRole(Exception): pass
+class InvalidPythonVersion(Exception): pass
+class FontNotFound(Exception): pass
