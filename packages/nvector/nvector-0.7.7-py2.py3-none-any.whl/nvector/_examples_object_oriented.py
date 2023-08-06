21f2094ea23da9c87fb78e946d1ad23a3dba7f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:49 +0000

    Add package nvector/0.7.7/nvector-0.7.7-py2.py3-none-any.whl

---
+++ packages/nvector/nvector-0.7.7-py2.py3-none-any.whl/nvector/_examples_object_oriented.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from nvector._examples import GETTING_STARTED
+__doc__ = GETTING_STARTED  # @ReservedAssignment
+
+
+if __name__ == '__main__':
+    from nvector._common import write_readme, test_docstrings
+    test_docstrings(__file__)
+    write_readme(__doc__)
