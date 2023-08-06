7b11a2872d6452377b7aa4f9e35492f56630b23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:10 +0000

    Add package TEItransformer/0.0.1/TEItransformer-0.0.1.tar.gz

---
+++ packages/TEItransformer/TEItransformer-0.0.1.tar.gz/TEItransformer-0.0.1/TEItransformer/__init__.py
@@ -0,0 +1,13 @@
+import .tei_auxiliary import *
+import .setup import *
+# import .path_validator
+# import .xml_validators
+# import .docx_constructor
+# import .css_builder
+# import .docx_builder
+# import .tei_xml
+# import .odd_processor
+# import .html_builder
+# import .json_builder
+# import .director
+# import .tei_transformer
