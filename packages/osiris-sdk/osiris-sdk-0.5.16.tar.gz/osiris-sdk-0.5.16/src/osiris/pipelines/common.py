c691b38ed2a6c45680d02a5befd5c0d8bb6bb639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:10 +0000

    Add package osiris-sdk/0.5.16/osiris-sdk-0.5.16.tar.gz

---
+++ packages/osiris-sdk/osiris-sdk-0.5.16.tar.gz/osiris-sdk-0.5.16/src/osiris/pipelines/common.py
@@ -0,0 +1,16 @@
+"""
+Contains common functions for pipelines modules.
+"""
+
+from ..core.azure_client_authorization import ClientAuthorization
+
+
+def initialize_client_auth(func):
+    """
+    This function is used to initialize the ClientAuthorization in a lazy way because of Beam.
+    """
+    def wrapper(self, *args, **kwargs):
+        self.client_auth = ClientAuthorization(self.tenant_id, self.client_id, self.client_secret)
+        return func(self, *args, **kwargs)
+
+    return wrapper
