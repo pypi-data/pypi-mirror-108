4c21d312a3d92ce6d00ec6cc2fec402a94360869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package pydwf/0.7.2/pydwf-0.7.2-py3-none-any.whl

---
+++ packages/pydwf/pydwf-0.7.2-py3-none-any.whl/pydwf/utilities/__init__.py
@@ -0,0 +1,4 @@
+"""This package provides utility functions for managing Digilent test and measurement devices."""
+
+# Make the open_dwf_device function directly available from the pydwf.utilities package.
+from pydwf.utilities.open_dwf_device import open_dwf_device
