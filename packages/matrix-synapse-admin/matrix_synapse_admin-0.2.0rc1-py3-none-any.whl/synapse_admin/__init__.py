9d3492fba939677eecdc3f78c00a297add218015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:12 +0000

    Add package matrix-synapse-admin/0.2.0rc1/matrix_synapse_admin-0.2.0rc1-py3-none-any.whl

---
+++ packages/matrix-synapse-admin/matrix_synapse_admin-0.2.0rc1-py3-none-any.whl/synapse_admin/__init__.py
@@ -0,0 +1,12 @@
+import sys
+if sys.version_info < (3, 7):
+    print("Synapse Admin API requires Python 3.7 or above.")
+    sys.exit(1)
+
+__version__ = "0.2.0rc1"
+
+from synapse_admin.user import User
+from synapse_admin.management import Management
+from synapse_admin.media import Media
+from synapse_admin.room import Room
+import synapse_admin.base as base
