f666c4fb02dc0ee3c7884b93097bb13759bae6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:44 +0000

    Add package hajkr-facebook-scraper/0.1.3/hajkr-facebook-scraper-0.1.3.tar.gz

---
+++ packages/hajkr-facebook-scraper/hajkr-facebook-scraper-0.1.3.tar.gz/hajkr-facebook-scraper-0.1.3/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hajkr_facebook_scraper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['facebook-scraper>=0.2.41,<0.3.0']
+
+entry_points = \
+{'console_scripts': ['hajkr-facebook-scraper = '
+                     'hajkr_facebook_scraper.__main__:run']}
+
+setup_kwargs = {
+    'name': 'hajkr-facebook-scraper',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Tadej Hribar',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
