042cfcfb598ee470a1dd03fcf2adf7412f751714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:41 +0000

    Add package coqui-stt-server/0.0.5/coqui_stt_server-0.0.5.tar.gz

---
+++ packages/coqui-stt-server/coqui_stt_server-0.0.5.tar.gz/coqui_stt_server-0.0.5/coqui_stt_server/static/build/precache-manifest.9412db0192acaf0411272825e7be6be9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c77b55d79befd2813a3e285d651bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a497eccb4ff1c8842caf",
+    "url": "/static/css/bundle.min.css"
+  },
+  {
+    "revision": "a497eccb4ff1c8842caf",
+    "url": "/static/js/bundle.min.js"
+  },
+  {
+    "revision": "799de9f25b80ded465f02f885354882e",
+    "url": "/static/js/bundle.min.js.LICENSE.txt"
+  }
+]);
