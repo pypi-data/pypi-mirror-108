c5ae7506346b9616520d3ed3bf0e6ed821fcaf0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:15 +0000

    Add package modelark/0.3.4/modelark-0.3.4.tar.gz

---
+++ packages/modelark/modelark-0.3.4.tar.gz/modelark-0.3.4/modelark/__init__.py
@@ -0,0 +1,9 @@
+from .common import (
+    Entity, Domain, DataDict, RecordList)
+from .repository import (
+    Repository, MemoryRepository,
+    JsonRepository, SqlRepository, RestRepository)
+
+__author__ = 'Knowark'
+__email__ = 'info@knowark.com'
+__version__ = '0.3.4'
