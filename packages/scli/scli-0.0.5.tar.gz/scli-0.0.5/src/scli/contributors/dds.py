1b74aa7e304535c746bdf281107ce34b3ebd7567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:41 +0000

    Add package scli/0.0.5/scli-0.0.5.tar.gz

---
+++ packages/scli/scli-0.0.5.tar.gz/scli-0.0.5/src/scli/contributors/dds.py
@@ -0,0 +1,41 @@
+
+def author():
+    """
+    Author Name
+    """
+    return "Dilip Debsingha"
+
+def email():
+    """
+    Author Email ID
+    """
+    return "ddsiha.cob@gmail.com"
+
+def experience():
+    """
+    Author Working Experience
+    """
+    return 10
+
+def skill():
+    """
+    Author Skills
+    """
+    return "SDET, Python"
+
+def company():
+    """
+    Author Currently Working Company
+    """
+    return "Vmware, Inc."
+
+
+def resume():
+    """
+    Author Resume
+    """
+    print(author())
+    print(email())
+    print(experience())
+    print(skill())
+    print(company())
