7cfabb6abc7f0077968150b80de291d40a1e49d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:06 +0000

    Add package fastformers/0.0.78/fastformers-0.0.78.tar.gz

---
+++ packages/fastformers/fastformers-0.0.78.tar.gz/fastformers-0.0.78/src/fastformers/modules/__init__.py
@@ -0,0 +1,4 @@
+from .modules import TanhHead
+from .embeddings import Embeddings, MaskEmbeddings, EmbeddingsWithTokenTypes
+from .transformer_encoder import TransformerEncoder
+from .transformer_decoder import TransformerDecoder
