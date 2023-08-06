098235cc6e700752f5e48fba19ac6bcfe7ef7e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:04 +0000

    Add package Chibchas/0.2.5b0/Chibchas-0.2.5b0.tar.gz

---
+++ packages/Chibchas/Chibchas-0.2.5b0.tar.gz/Chibchas-0.2.5b0/setup.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Copyright (c) Colav.
+# Distributed under the terms of the Modified BSD License.
+
+# -----------------------------------------------------------------------------
+# Minimal Python version sanity check (from IPython)
+# -----------------------------------------------------------------------------
+
+# See https://stackoverflow.com/a/26737258/2268280
+# sudo pip3 install twine
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+# For test purposes
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+import os
+import sys
+import codecs
+
+
+v = sys.version_info
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+shell = False
+if os.name in ('nt', 'dos'):
+    shell = True
+    warning = "WARNING: Windows is not officially supported"
+    print(warning, file=sys.stderr)
+
+
+def main():
+    setup(
+        # Application name:
+        name="Chibchas",
+
+        # Version number (initial):
+        version="0.2.5-beta",
+
+        # Application author details:
+        author="Colav",
+        author_email="colav@udea.edu.co",
+
+        # Packages
+        packages=find_packages(exclude=['tests']),
+
+        # Include additional files into the package
+        include_package_data=True,
+
+        # Details
+        url="https://github.com/colav/Chibchas",
+        scripts=['bin/chibchas_server','bin/chibchas_institulac'],
+        #
+        license="BSD",
+
+        description="InstituLAC Automation Tools",
+
+        long_description=open("README.md").read(),
+
+        long_description_content_type="text/markdown",
+
+        # Dependent packages (distributions)
+        install_requires=[
+            'flask-wtf',
+            'wtforms==2.3.3',
+            'Flask-Bootstrap',
+            'html5lib',
+            'pandas',
+            'numpy',
+            'helium',
+            'xlsxwriter',
+            'lxml',
+            'bs4',
+            'xlrd',
+            'openpyxl'
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
