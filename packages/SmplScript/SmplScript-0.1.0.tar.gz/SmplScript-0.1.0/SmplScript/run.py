e0c1e3fc2197ecc9f5b8efa51161b07c228ee8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:52 +0000

    Add package SmplScript/0.1.0/SmplScript-0.1.0.tar.gz

---
+++ packages/SmplScript/SmplScript-0.1.0.tar.gz/SmplScript-0.1.0/SmplScript/run.py
@@ -0,0 +1,19 @@
+from SmplScript.Interpret_and_run.interpret_and_run import run
+from termcolor import cprint as c
+
+def run_language():
+	while True:
+		text = input('Smpl > ')
+		if text.strip() == "": continue
+		result, error = run('<stdin>', text)
+
+		if error:
+			c(error.as_string(), 'red')
+		elif result:
+			if len(result.elements) == 1:
+				print(repr(result.elements[0]))
+			else:
+				print(repr(result))
+
+if __name__ == '__main__':
+	run_language()
