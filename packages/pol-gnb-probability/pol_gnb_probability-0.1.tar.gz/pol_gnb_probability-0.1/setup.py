d9056651f49a66aa74d7e149d834c2540f2fc1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:35 +0000

    Add package pol-gnb-probability/0.1/pol_gnb_probability-0.1.tar.gz

---
+++ packages/pol-gnb-probability/pol_gnb_probability-0.1.tar.gz/pol_gnb_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pol_gnb_probability',
+      version='0.1',
+      description='Gaussian distributions & Binomial distribution',
+      packages=['pol_gnb_probability'],
+      author = 'Panjapol Ampornratana',
+      author_email = 'panjapol.pol@gmail.com',
+      zip_safe=False)
