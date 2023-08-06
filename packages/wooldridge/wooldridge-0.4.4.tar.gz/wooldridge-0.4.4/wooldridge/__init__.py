0780f3ff317f6e66ae1c57430408ac0cf09db624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:22 +0000

    Add package wooldridge/0.4.4/wooldridge-0.4.4.tar.gz

---
+++ packages/wooldridge/wooldridge-0.4.4.tar.gz/wooldridge-0.4.4/wooldridge/__init__.py
@@ -0,0 +1,11 @@
+"""
+Github page: https://github.com/spring-haru/wooldridge
+"""
+
+from .load_data import *
+
+__all__ = ['data', 'dataWoo']
+
+__author__ = 'Tetsu Haruyama'
+__version__ = '0.4.4'
+__copyright__ = 'Copyright (c) 2021 Tetsu Haruyama'
