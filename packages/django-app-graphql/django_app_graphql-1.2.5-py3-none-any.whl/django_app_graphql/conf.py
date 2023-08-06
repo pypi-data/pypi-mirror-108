9d1a3bf9e74027b1a54eea2b98dfd8c83f4fdeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:19 +0000

    Add package django-app-graphql/1.2.5/django_app_graphql-1.2.5-py3-none-any.whl

---
+++ packages/django-app-graphql/django_app_graphql-1.2.5-py3-none-any.whl/django_app_graphql/conf.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from appconf import AppConf
+
+
+class DjangoAppGraphQLAppConf(AppConf):
+    class Meta:
+        prefix = "DJANGO_APP_GRAPHQL"
+
+    EXPOSE_GRAPHIQL = True
+    """
+    If set, we will expose the graphiql UI
+    """
+    GRAPHQL_SERVER_URL = "/graphql"
+    """
+    the endpoint where the graphql server is located
+    """
+
