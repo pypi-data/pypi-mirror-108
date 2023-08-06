70b00cdfff603d0e53e64bf4031d758e415d50c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:53 +0000

    Add package minecraft-seed-generator/0.1.0/minecraft_seed_generator-0.1.0-py3-none-any.whl

---
+++ packages/minecraft-seed-generator/minecraft_seed_generator-0.1.0-py3-none-any.whl/minecraft_seed_generator.py
@@ -0,0 +1,10 @@
+import random
+
+class GenerateSeed():
+    def __init__(self, seeds):
+        self.range = seeds
+        for x in range(self.range):
+            seed = f"{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}"
+            print(seed)
+
+GenerateSeed(5)
