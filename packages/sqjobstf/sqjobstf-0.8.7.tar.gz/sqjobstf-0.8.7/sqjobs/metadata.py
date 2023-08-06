58c45730a4a440dbc7b41b1b444cac0f259771b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:05 +0000

    Add package sqjobstf/0.8.7/sqjobstf-0.8.7.tar.gz

---
+++ packages/sqjobstf/sqjobstf-0.8.7.tar.gz/sqjobstf-0.8.7/sqjobs/metadata.py
@@ -0,0 +1,7 @@
+__uname__ = 'sqjobstf'
+__long_name__ = 'Simple Queue Jobs TF Fork'
+__version__ = '0.8.7'
+__author__ = 'Thinkful'
+__email__ = 'engineering@thinkful.com'
+__url__ = 'http://github.com/Thinkful/sqjobs'
+__license__ = 'BSD'
