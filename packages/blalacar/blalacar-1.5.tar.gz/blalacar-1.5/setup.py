4ab5ef8f31fe69ecfbf8c6e6d8f5b24ac59973c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:32 +0000

    Add package blalacar/1.5/blalacar-1.5.tar.gz

---
+++ packages/blalacar/blalacar-1.5.tar.gz/blalacar-1.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='blalacar',
+    packages=find_packages(),
+    version='1.5',
+    description='blalacar',
+    author='k.e.p',
+    author_email='k.e.p@gmail.com',
+    url='https://github.com/EPkravec/blalacar1',
+    download_url='https://github.com/EPkravec/blalacar1/archive/1.5.tar.gz',
+    keywords=['blalacar1'],
+    install_requires=['simple_draw']
+)
