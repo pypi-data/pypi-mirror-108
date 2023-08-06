2321cef60cbb3f9ee483461a18303b39323d8776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:56 +0000

    Add package pytest-split/0.1.6/pytest-split-0.1.6.tar.gz

---
+++ packages/pytest-split/pytest-split-0.1.6.tar.gz/pytest-split-0.1.6/src/pytest_split/plugin.py
@@ -0,0 +1,205 @@
+import json
+import pytest
+import os
+from collections import defaultdict, OrderedDict, namedtuple
+from typing import TYPE_CHECKING
+
+from _pytest.config import create_terminal_writer
+
+if TYPE_CHECKING:
+    from typing import List, Tuple
+    from _pytest.config.argparsing import Parser
+    from _pytest.main import Session
+
+    from _pytest import nodes
+    from _pytest.config import Config
+
+# Ugly hacks for freezegun compatibility: https://github.com/spulec/freezegun/issues/286
+STORE_DURATIONS_SETUP_AND_TEARDOWN_THRESHOLD = 60 * 10  # seconds
+
+TestGroup = namedtuple("TestGroup", "index, num_tests")
+TestSuite = namedtuple("TestSuite", "splits, num_tests")
+
+
+def pytest_addoption(parser: "Parser") -> None:
+    group = parser.getgroup(
+        "Split tests into groups which execution time is about the same. "
+        "Run first the whole suite with --store-durations to save information "
+        "about test execution times"
+    )
+    group.addoption(
+        "--store-durations",
+        dest="store_durations",
+        action="store_true",
+        help="Store durations into '--durations-path'",
+    )
+    group.addoption(
+        "--durations-path",
+        dest="durations_path",
+        help=(
+            "Path to the file in which durations are (to be) stored, "
+            "default is .test_durations in the current working directory"
+        ),
+        default=os.path.join(os.getcwd(), ".test_durations"),
+    )
+    group.addoption(
+        "--splits",
+        dest="splits",
+        type=int,
+        help="The number of groups to split the tests into",
+    )
+    group.addoption(
+        "--group",
+        dest="group",
+        type=int,
+        help="The group of tests that should be executed (first one is 1)",
+    )
+
+
+@pytest.mark.tryfirst
+def pytest_cmdline_main(config: "Config") -> None:
+    group = config.getoption("group")
+    splits = config.getoption("splits")
+
+    if splits is None and group is None:
+        return
+
+    if splits and group is None:
+        raise pytest.UsageError("argument `--group` is required")
+
+    if group and splits is None:
+        raise pytest.UsageError("argument `--splits` is required")
+
+    if splits < 1:
+        raise pytest.UsageError("argument `--splits` must be >= 1")
+
+    if group < 1 or group > splits:
+        raise pytest.UsageError(f"argument `--group` must be >= 1 and <= {splits}")
+
+
+class SplitPlugin:
+    def __init__(self):
+        self._suite: TestSuite
+        self._group: TestGroup
+        self._messages: "List[str]" = []
+
+    def pytest_report_collectionfinish(self, config: "Config") -> "List[str]":
+        lines = []
+        if self._messages:
+            lines += self._messages
+
+        if hasattr(self, "_suite"):
+            lines.append(
+                f"Running group {self._group.index}/{self._suite.splits}"
+                f" ({self._group.num_tests}/{self._suite.num_tests}) tests"
+            )
+
+        prefix = "[pytest-split]"
+        lines = [f"{prefix} {m}" for m in lines]
+
+        return lines
+
+    def pytest_collection_modifyitems(
+        self, config: "Config", items: "List[nodes.Item]"
+    ) -> None:
+        splits = config.option.splits
+        group = config.option.group
+        store_durations = config.option.store_durations
+        durations_report_path = config.option.durations_path
+
+        if store_durations:
+            if any((group, splits)):
+                self._messages.append(
+                    "Not splitting tests because we are storing durations"
+                )
+            return None
+
+        if not group and not splits:
+            # don't split unless explicitly requested
+            return None
+
+        if not os.path.isfile(durations_report_path):
+            self._messages.append(
+                "Not splitting tests because the durations_report is missing"
+            )
+            return None
+
+        with open(durations_report_path) as f:
+            stored_durations = OrderedDict(json.load(f))
+
+        start_idx, end_idx = _calculate_suite_start_and_end_idx(
+            splits, group, items, stored_durations
+        )
+
+        self._suite = TestSuite(splits, len(items))
+        self._group = TestGroup(group, end_idx - start_idx)
+        items[:] = items[start_idx:end_idx]
+
+
+def pytest_configure(config: "Config") -> None:
+    config.pluginmanager.register(SplitPlugin())
+
+
+def pytest_sessionfinish(session: "Session") -> None:
+    if session.config.option.store_durations:
+        report_path = session.config.option.durations_path
+        terminal_reporter = session.config.pluginmanager.get_plugin("terminalreporter")
+        durations: dict = defaultdict(float)
+        for test_reports in terminal_reporter.stats.values():
+            for test_report in test_reports:
+                if hasattr(test_report, "duration"):
+                    stage = getattr(test_report, "when", "")
+                    duration = test_report.duration
+                    # These ifs be removed after this is solved:
+                    # https://github.com/spulec/freezegun/issues/286
+                    if duration < 0:
+                        continue
+                    if (
+                        stage in ("teardown", "setup")
+                        and duration > STORE_DURATIONS_SETUP_AND_TEARDOWN_THRESHOLD
+                    ):
+                        # Ignore not legit teardown durations
+                        continue
+                    durations[test_report.nodeid] += test_report.duration
+
+        with open(report_path, "w") as f:
+            f.write(json.dumps(list(durations.items()), indent=2))
+
+        terminal_writer = create_terminal_writer(session.config)
+        message = terminal_writer.markup(
+            " Stored test durations in {}\n".format(report_path)
+        )
+        terminal_reporter.write(message)
+
+
+def _calculate_suite_start_and_end_idx(
+    splits: int, group: int, items: "List[nodes.Item]", stored_durations: OrderedDict
+) -> "Tuple[int, int]":
+    item_node_ids = [item.nodeid for item in items]
+    stored_durations = OrderedDict(
+        {k: v for k, v in stored_durations.items() if k in item_node_ids}
+    )
+    avg_duration_per_test = sum(stored_durations.values()) / len(stored_durations)
+
+    durations = OrderedDict()
+    for node_id in item_node_ids:
+        durations[node_id] = stored_durations.get(node_id, avg_duration_per_test)
+
+    time_per_group = sum(durations.values()) / splits
+    start_time = time_per_group * (group - 1)
+    end_time = time_per_group + start_time
+    start_idx = end_idx = duration_rolling_sum = 0
+
+    for idx, duration in enumerate(durations.values()):
+        duration_rolling_sum += duration
+        if group != 1 and not start_idx and duration_rolling_sum > start_time:
+            start_idx = idx
+            if group == splits:
+                break
+        elif group != splits and not end_idx and duration_rolling_sum > end_time:
+            end_idx = idx
+            break
+    if not end_idx:
+        end_idx = len(items)
+
+    return start_idx, end_idx
---
+++ packages/pytest-split/pytest-split-0.1.6.tar.gz/pytest-split-0.1.6/tests/test_plugin.py
@@ -0,0 +1,307 @@
+import itertools
+import json
+import os
+
+import pytest
+
+
+pytest_plugins = ["pytester"]
+
+
+@pytest.fixture
+def example_suite(testdir):
+    testdir.makepyfile(
+        """
+    def test_1(): pass
+    def test_2(): pass
+    def test_3(): pass
+    def test_4(): pass
+    def test_5(): pass
+    def test_6(): pass
+    def test_7(): pass
+    def test_8(): pass
+    def test_9(): pass
+    def test_10(): pass
+    """
+    )
+    yield testdir
+
+
+@pytest.fixture
+def durations_path(tmpdir):
+    return str(tmpdir.join(".durations"))
+
+
+class TestStoreDurations:
+    def test_it_stores(self, example_suite, durations_path):
+        example_suite.runpytest("--store-durations", "--durations-path", durations_path)
+        with open(durations_path) as f:
+            durations = json.load(f)
+
+        stored_tests = [d[0] for d in durations]
+        assert stored_tests == [
+            "test_it_stores.py::test_1",
+            "test_it_stores.py::test_2",
+            "test_it_stores.py::test_3",
+            "test_it_stores.py::test_4",
+            "test_it_stores.py::test_5",
+            "test_it_stores.py::test_6",
+            "test_it_stores.py::test_7",
+            "test_it_stores.py::test_8",
+            "test_it_stores.py::test_9",
+            "test_it_stores.py::test_10",
+        ]
+
+        durations_per_test = [d[1] for d in durations]
+        for duration in durations_per_test:
+            assert isinstance(duration, float)
+
+    def test_it_does_not_store_without_flag(self, example_suite, durations_path):
+        example_suite.runpytest("--durations-path", durations_path)
+        assert not os.path.exists(durations_path)
+
+
+class TestSplitToSuites:
+    @pytest.mark.parametrize(
+        "param_idx, splits, expected_tests_per_group",
+        [
+            (
+                0,
+                1,
+                [
+                    [
+                        "test_1",
+                        "test_2",
+                        "test_3",
+                        "test_4",
+                        "test_5",
+                        "test_6",
+                        "test_7",
+                        "test_8",
+                        "test_9",
+                        "test_10",
+                    ]
+                ],
+            ),
+            (
+                1,
+                2,
+                [
+                    ["test_1", "test_2", "test_3", "test_4", "test_5", "test_6"],
+                    ["test_7", "test_8", "test_9", "test_10"],
+                ],
+            ),
+            (
+                2,
+                3,
+                [
+                    ["test_1", "test_2", "test_3", "test_4", "test_5"],
+                    ["test_6", "test_7"],
+                    ["test_8", "test_9", "test_10"],
+                ],
+            ),
+            (
+                3,
+                4,
+                [
+                    ["test_1", "test_2", "test_3"],
+                    ["test_4", "test_5", "test_6"],
+                    ["test_7", "test_8"],
+                    ["test_9", "test_10"],
+                ],
+            ),
+        ],
+    )
+    def test_it_splits(
+        self, param_idx, splits, expected_tests_per_group, example_suite, durations_path
+    ):
+        assert len(list(itertools.chain(*expected_tests_per_group))) == 10
+
+        durations = [
+            ["test_it_splits{}/test_it_splits.py::test_1".format(param_idx), 1],
+            ["test_it_splits{}/test_it_splits.py::test_2".format(param_idx), 1],
+            ["test_it_splits{}/test_it_splits.py::test_3".format(param_idx), 1],
+            ["test_it_splits{}/test_it_splits.py::test_4".format(param_idx), 1],
+            ["test_it_splits{}/test_it_splits.py::test_5".format(param_idx), 1],
+            ["test_it_splits{}/test_it_splits.py::test_6".format(param_idx), 2],
+            ["test_it_splits{}/test_it_splits.py::test_7".format(param_idx), 2],
+            ["test_it_splits{}/test_it_splits.py::test_8".format(param_idx), 2],
+            ["test_it_splits{}/test_it_splits.py::test_9".format(param_idx), 2],
+            ["test_it_splits{}/test_it_splits.py::test_10".format(param_idx), 2],
+        ]
+
+        with open(durations_path, "w") as f:
+            json.dump(durations, f)
+
+        results = []
+        for group in range(splits):
+            results.append(
+                example_suite.inline_run(
+                    "--splits",
+                    str(splits),
+                    "--group",
+                    str(group + 1),
+                    "--durations-path",
+                    durations_path,
+                )
+            )
+
+        for result, expected_tests in zip(results, expected_tests_per_group):
+            result.assertoutcome(passed=len(expected_tests))
+            assert _passed_test_names(result) == expected_tests
+
+    def test_it_does_not_split_with_invalid_args(self, example_suite, durations_path):
+        durations = [["test_it_does_not_split_with_invalid_args.py::test_1", 1]]
+        with open(durations_path, "w") as f:
+            json.dump(durations, f)
+
+        result = example_suite.inline_run(
+            "--splits", "2", "--group", "1"
+        )  # no durations report in default location
+        result.assertoutcome(passed=10)
+
+    def test_it_adapts_splits_based_on_new_and_deleted_tests(
+        self, example_suite, durations_path
+    ):
+        # Only 4/10 tests listed here, avg duration 1 sec
+        test_path = (
+            "test_it_adapts_splits_based_on_new_and_deleted_tests0/"
+            "test_it_adapts_splits_based_on_new_and_deleted_tests.py::{}"
+        )
+        durations = [
+            [test_path.format("test_1"), 1],
+            [test_path.format("test_5"), 2.6],
+            [test_path.format("test_6"), 0.2],
+            [test_path.format("test_10"), 0.2],
+            [test_path.format("test_THIS_IS_NOT_IN_THE_SUITE"), 1000],
+        ]
+
+        with open(durations_path, "w") as f:
+            json.dump(durations, f)
+
+        result = example_suite.inline_run(
+            "--splits", "3", "--group", "1", "--durations-path", durations_path
+        )  # no --group
+        result.assertoutcome(passed=3)
+        assert _passed_test_names(result) == ["test_1", "test_2", "test_3"]
+
+        result = example_suite.inline_run(
+            "--splits", "3", "--group", "2", "--durations-path", durations_path
+        )  # no --group
+        result.assertoutcome(passed=2)
+        assert _passed_test_names(result) == ["test_4", "test_5"]
+
+        result = example_suite.inline_run(
+            "--splits", "3", "--group", "3", "--durations-path", durations_path
+        )  # no --group
+        result.assertoutcome(passed=5)
+        assert _passed_test_names(result) == [
+            "test_6",
+            "test_7",
+            "test_8",
+            "test_9",
+            "test_10",
+        ]
+
+
+class TestRaisesUsageErrors:
+    def test_returns_nonzero_when_group_but_not_splits(self, example_suite, capsys):
+        result = example_suite.inline_run("--group", "1")
+        assert result.ret == 4
+
+        outerr = capsys.readouterr()
+        assert "argument `--splits` is required" in outerr.err
+
+    def test_returns_nonzero_when_splits_but_not_group(self, example_suite, capsys):
+        result = example_suite.inline_run("--splits", "1")
+        assert result.ret == 4
+
+        outerr = capsys.readouterr()
+        assert "argument `--group` is required" in outerr.err
+
+    def test_returns_nonzero_when_group_below_one(self, example_suite, capsys):
+        result = example_suite.inline_run("--splits", "3", "--group", "0")
+        assert result.ret == 4
+
+        outerr = capsys.readouterr()
+        assert "argument `--group` must be >= 1 and <= 3" in outerr.err
+
+    def test_returns_nonzero_when_group_larger_than_splits(self, example_suite, capsys):
+        result = example_suite.inline_run("--splits", "3", "--group", "4")
+        assert result.ret == 4
+
+        outerr = capsys.readouterr()
+        assert "argument `--group` must be >= 1 and <= 3" in outerr.err
+
+    def test_returns_nonzero_when_splits_below_one(self, example_suite, capsys):
+        result = example_suite.inline_run("--splits", "0", "--group", "1")
+        assert result.ret == 4
+
+        outerr = capsys.readouterr()
+        assert "argument `--splits` must be >= 1" in outerr.err
+
+
+class TestHasExpectedOutput:
+    def test_does_not_print_splitting_summary_when_durations_missing(
+        self, example_suite, capsys
+    ):
+        result = example_suite.inline_run("--splits", "1", "--group", "1")
+        assert result.ret == 0
+
+        outerr = capsys.readouterr()
+        assert (
+            "[pytest-split] Not splitting tests because the durations_report is missing"
+            in outerr.out
+        )
+        assert "[pytest-split] Running group" not in outerr.out
+
+    def test_prints_splitting_summary_when_durations_present(
+        self, example_suite, capsys, durations_path
+    ):
+        test_name = "test_prints_splitting_summary_when_durations_present"
+        with open(durations_path, "w") as f:
+            json.dump([[f"{test_name}0/{test_name}.py::test_1", 0.5]], f)
+        result = example_suite.inline_run(
+            "--splits", "1", "--group", "1", "--durations-path", durations_path
+        )
+        assert result.ret == 0
+
+        outerr = capsys.readouterr()
+        assert "[pytest-split] Running group 1/1 (10/10) tests" in outerr.out
+
+    def test_prints_splitting_summary_when_storing_durations(
+        self, example_suite, capsys, durations_path
+    ):
+        test_name = "test_prints_splitting_summary_when_storing_durations"
+        with open(durations_path, "w") as f:
+            json.dump([[f"{test_name}0/{test_name}.py::test_1", 0.5]], f)
+
+        result = example_suite.inline_run(
+            "--splits",
+            "1",
+            "--group",
+            "1",
+            "--durations-path",
+            durations_path,
+            "--store-durations",
+        )
+        assert result.ret == 0
+
+        outerr = capsys.readouterr()
+        assert (
+            "[pytest-split] Not splitting tests because we are storing durations"
+            in outerr.out
+        )
+
+    def test_does_not_print_splitting_summary_when_no_pytest_split_arguments(
+        self, example_suite, capsys
+    ):
+        result = example_suite.inline_run()
+        assert result.ret == 0
+
+        outerr = capsys.readouterr()
+        assert "[pytest-split]" not in outerr.out
+
+
+def _passed_test_names(result):
+    return [passed.nodeid.split("::")[-1] for passed in result.listoutcomes()[0]]
