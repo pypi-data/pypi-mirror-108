e59ab0b9e84dec0175b7bb4ddbdc49623bb88fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:28 +0000

    Add package pyvdr/0.3.1/pyvdr-0.3.1.tar.gz

---
+++ packages/pyvdr/pyvdr-0.3.1.tar.gz/pyvdr-0.3.1/test/test_Real.py
@@ -0,0 +1,18 @@
+import unittest
+from pyvdr import PYVDR
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+
+        # pyvdr = PYVDR(hostname='easyvdr.fritz.box')
+        # channel = pyvdr.get_channel()
+        # epg_info = pyvdr.get_channel_epg_info()
+        # diskstat = pyvdr.stat()
+        # list_of_timers = pyvdr.get_timers()
+        # is_recording = pyvdr.is_recording()
+        # list_of_recordings = pyvdr.list_recordings()
+
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
