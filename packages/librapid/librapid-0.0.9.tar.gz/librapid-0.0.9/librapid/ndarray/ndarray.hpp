d9cd7f5c1932ffbdcf9cfe6958cc545353ca226d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:41 +0000

    Add package librapid/0.0.9/librapid-0.0.9.tar.gz

---
+++ packages/librapid/librapid-0.0.9.tar.gz/librapid-0.0.9/librapid/ndarray/ndarray.hpp
@@ -0,0 +1,20 @@
+#ifndef NDARRAY_CORE
+#define NDARRAY_CORE
+
+#include <librapid/ndarray/config.hpp>
+
+#include <librapid/ndarray/errors.hpp>
+#include <librapid/ndarray/core_math.hpp>
+#include <librapid/ndarray/basic_stride.hpp>
+#include <librapid/ndarray/basic_extent.hpp>
+#include <librapid/ndarray/strided_loops.hpp>
+#include <librapid/ndarray/to_string.hpp>
+#include <librapid/ndarray/utils.hpp>
+#include <librapid/ndarray/broadcast_arrays.hpp>
+#include <librapid/ndarray/array_array_arithmetic.hpp>
+#include <librapid/ndarray/array_scalar_arithmetic.hpp>
+#include <librapid/ndarray/scalar_array_arithmetic.hpp>
+#include <librapid/ndarray/array_arithmetic.hpp>
+#include <librapid/ndarray/basic_ndarray.hpp>
+
+#endif // NDARRAY_CORE
