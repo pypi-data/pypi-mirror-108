9b6f08cb62a2ab120d65c25175fbfd507075277c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:10 +0000

    Add package gracefml/0.0.1/gracefml-0.0.1.tar.gz

---
+++ packages/gracefml/gracefml-0.0.1.tar.gz/gracefml-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gracefml']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Sphinx>=4.0.2,<5.0.0', 'pandas>=1.2.4,<2.0.0', 'pyro-ppl>=1.6.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'gracefml',
+    'version': '0.0.1',
+    'description': 'Framework for weakly-supervised regression',
+    'long_description': None,
+    'author': 'chrisaddy',
+    'author_email': 'chris.william.addy@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
