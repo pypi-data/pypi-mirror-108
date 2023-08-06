5481f9d811f627b6a10dc73514f7e441bbd88b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:53 +0000

    Add package mqttwarn/0.22.0/mqttwarn-0.22.0.tar.gz

---
+++ packages/mqttwarn/mqttwarn-0.22.0.tar.gz/mqttwarn-0.22.0/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# (c) 2018 The mqttwarn developers
+
+# Configuration- and function files used by the test harness
+configfile = 'tests/selftest.ini'
+configfile_v2 = 'tests/selftest_v2.ini'
+funcfile = 'tests/selftest.py'
+bad_funcfile = 'tests/bad_funcs.py'
