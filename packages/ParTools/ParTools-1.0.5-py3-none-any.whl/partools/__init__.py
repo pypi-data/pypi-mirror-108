523ed169a1d657fb8bd3a20433607cdfd92787c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:10 +0000

    Add package ParTools/1.0.5/ParTools-1.0.5-py3-none-any.whl

---
+++ packages/ParTools/ParTools-1.0.5-py3-none-any.whl/partools/__init__.py
@@ -0,0 +1,23 @@
+import importlib.util as u
+from os.path import exists
+
+
+def load_module(name, path):
+
+    spec = u.spec_from_file_location(name, path)
+    mod = u.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    return mod
+
+
+if exists('PTconf_perso.py'):
+    cfg = load_module('cfg', 'PTconf_perso.py')
+elif exists('PTconf.py'):
+    cfg = load_module('cfg', 'PTconf.py')
+else:
+    import partools.conf as cfg
+
+__version__ = '1.0.5'
+GITHUB_LINK = 'https://github.com/paularnaud2/ParTools'
+
+import partools.utils
