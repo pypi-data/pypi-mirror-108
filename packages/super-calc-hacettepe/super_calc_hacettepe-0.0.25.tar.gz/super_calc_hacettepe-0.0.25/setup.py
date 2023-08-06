4615d417f36fd64415f436853f9f958cbd5ba1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:21 +0000

    Add package super-calc-hacettepe/0.0.25/super_calc_hacettepe-0.0.25.tar.gz

---
+++ packages/super-calc-hacettepe/super_calc_hacettepe-0.0.25.tar.gz/super_calc_hacettepe-0.0.25/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import setup, find_packages
+import os
+
+from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="super_calc_hacettepe",                     # This is the name of the package
+    version="0.0.25",                        # The initial release version
+    author="Atahan Çelebi",                     # Full name of the author
+    description="Quicksample HACETTEPE",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src"),    # List of all python modules to be installed
+    
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+                 # Name of the python package
+    package_dir={'':'src'},     # Directory of the source code of the package
+    install_requires=['pysal >= 2.0.0'],                     # Install other dependencies if any
+    package_data={'': ['yazilar.txt'],'super_calc_hacettepe':['test/*']}
+)
