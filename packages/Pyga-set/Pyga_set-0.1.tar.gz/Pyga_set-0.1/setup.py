76a9ad4f1c24ccbbb033572ec426dbd50bbf8daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:40 +0000

    Add package Pyga-set/0.1/Pyga_set-0.1.tar.gz

---
+++ packages/Pyga-set/Pyga_set-0.1.tar.gz/Pyga_set-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Pyga_set',
+      version='0.1',
+      description='Pyga_set',
+      packages=['Pyga_set'],
+      author_email='den.gavrin@mail.ru',
+      zip_safe=False)
