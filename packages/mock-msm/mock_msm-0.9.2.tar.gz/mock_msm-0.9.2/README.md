4adf7ebe11398447a6429a36bffe21bbeec8c112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:18 +0000

    Add package mock-msm/0.9.2/mock_msm-0.9.2.tar.gz

---
+++ packages/mock-msm/mock_msm-0.9.2.tar.gz/mock_msm-0.9.2/README.md
@@ -0,0 +1,7 @@
+## Mock Mycroft Skills Manager
+
+The sole purpose of this pkg is to mock msm imports
+
+It allows to cleanly disable msm in mycroft-lib
+
+the only implemented functionality is scanning of skills folder
---
+++ packages/mock-msm/mock_msm-0.9.2.tar.gz/mock_msm-0.9.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='mock_msm',
+    version='0.9.2',
+    packages=['mock_msm'],
+    url='https://github.com/MycroftAI/mycroft-skills-manager',
+    license='Apache-2.0',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='Mock Mycroft Skills Manager',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
