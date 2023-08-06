6df26273f9dacf81c40032cbfb2ac69abef34967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:23 +0000

    Add package geoai-gdal/3.2.3/geoai_GDAL-3.2.3-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/geoai-gdal/geoai_GDAL-3.2.3-cp38-cp38-manylinux2010_x86_64.whl/geoai_GDAL-3.2.3.data/scripts/gdal_auth.py
@@ -0,0 +1,11 @@
+#!python
+
+import sys
+# import osgeo.utils.gdal_auth as a convenience to use as a script
+from osgeo.utils.gdal_auth import *  # noqa
+from osgeo.utils.gdal_auth import main
+from osgeo.gdal import deprecation_warn
+
+
+deprecation_warn('gdal_auth', 'utils')
+sys.exit(main(sys.argv))
