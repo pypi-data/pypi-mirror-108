7235c135829241eaf06b233992e507b51297976d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:34 +0000

    Add package ascend-deployer/0.0.6/ascend_deployer-0.0.6-py3-none-any.whl

---
+++ packages/ascend-deployer/ascend_deployer-0.0.6-py3-none-any.whl/ascend_deployer/downloader/logger_config.py
@@ -0,0 +1,87 @@
+# coding: utf-8
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===========================================================================
+
+import logging
+import os
+import platform
+import stat
+
+from logging.handlers import RotatingFileHandler
+
+
+DOCUMENTION=r"""
+Basic Log Configuration
+"""
+
+
+class BasicLogConfig(object):
+    """basic logger configuration"""
+    DEBUG = False
+
+    LOG_DIR = os.path.dirname(os.path.realpath(__file__))
+    LOG_DIR = os.path.dirname(LOG_DIR)
+    if platform.system() == 'Linux':
+        if 'site-packages' in LOG_DIR or 'dist-packages' in LOG_DIR:
+            ad_home = os.getenv('ASCEND_DEPLOYER_HOME')
+            if ad_home is None:
+                ad_home = os.getenv('HOME')
+            LOG_DIR = os.path.join(ad_home, 'ascend-deployer')
+
+        if not os.path.exists(LOG_DIR):
+            LOG_DIR = os.getcwd()
+    else:
+        LOG_DIR = os.getcwd()
+
+    LOG_FILE = os.path.join(LOG_DIR, 'downloader.log')
+    if not os.path.exists(LOG_FILE):
+        os.close(
+            os.open(
+                LOG_FILE, os.O_CREAT,
+                stat.S_IRUSR | stat.S_IWUSR
+            )
+        )
+    else:
+        os.chmod(LOG_FILE, stat.S_IRUSR | stat.S_IWUSR)
+    LOG_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+    LOG_FORMAT_STRING = \
+            "[%(asctime)s] downloader %(levelname)s [pid:%(process)d] " \
+            "[%(threadName)s] [%(filename)s:%(lineno)d %(funcName)s] %(message)s"
+    LOG_LEVEL = logging.INFO
+
+
+LOG_CONF = BasicLogConfig()
+
+logging.basicConfig(filename=LOG_CONF.LOG_FILE,
+             level=LOG_CONF.LOG_LEVEL,
+             format=LOG_CONF.LOG_FORMAT_STRING,
+             datefmt=LOG_CONF.LOG_DATE_FORMAT)
+
+
+ROTATING_FILE_LOG_CONF = dict(
+    filename=LOG_CONF.LOG_FILE,
+    mode='a',
+    maxBytes=20 * 1024 * 1024,
+    backupCount=5,
+    encoding="UTF-8"
+)
+
+
+def get_logger(name):
+    """get_logger"""
+    logger = logging.getLogger(name)
+    rotating_log_handler = RotatingFileHandler(**ROTATING_FILE_LOG_CONF)
+    logger.addHandler(rotating_log_handler)
+    return logger
---
+++ packages/ascend-deployer/ascend_deployer-0.0.6-py3-none-any.whl/ascend_deployer/start_download.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+readonly TRUE=1
+readonly FALSE=0
+readonly BASE_DIR=$(cd "$(dirname $0)" > /dev/null 2>&1; pwd -P)
+readonly CUR_DIR=$(cd $(dirname "$0"); pwd)
+OS_LIST=""
+PKG_LIST=""
+
+
+
+
+function get_python3()
+{
+    # try this first
+    py36="/usr/bin/python3.6"
+    if [ -f ${py36} ];then
+        echo ${py36}
+        return 0
+    fi
+
+    # centos 8.2
+    platform_python="/usr/libexec/platform-python3.6"
+    if [ -f ${platform_python} ];then
+        echo ${platform_python}
+        return 0
+    fi
+
+    # this python3 mybe replace by user and have no lzma or other module
+    have_python3=`command -v python3 | wc -l`
+    if [ ${have_python3} -eq 1 ];then
+        echo python3
+        return 0
+    fi
+
+    return 1
+}
+
+function install_python3()
+{
+    if [ $(id -u) -ne 0 ];then
+        echo "ERROR" "you are not root user and python3 is not available, please install python3 first by yourselt"
+        exit 1
+    fi
+
+    if [[ ! -e ${BASE_DIR}/install_python3.log ]];then
+        touch ${BASE_DIR}/install_python3.log
+    fi
+    chmod 600 ${BASE_DIR}/install_python3.log
+
+    have_yum=`command -v yum | wc -l`
+    if [ ${have_yum} -eq 1 ];then
+        yum install -y python3 >> ${BASE_DIR}/install_python3.log 2>&1
+        if [[ $? != 0 ]];then
+            echo 'ERROR: python3 is not available and "yum install -y python3" failed, please check network or yum'
+            exit 1
+        fi
+    fi
+
+    have_apt=`command -v apt | wc -l`
+    if [ ${have_apt} -eq 1 ];then
+        apt install -y python3 >> ${BASE_DIR}/install_python3.log 2>&1
+        if [[ $? != 0 ]];then
+            echo 'ERROR: python3 is not available and "apt install -y python3" failed, please check network or apt'
+            exit 1
+        fi
+    fi
+}
+
+function print_usage()
+{
+    echo "Usage: ./start_download.sh [-h] [--os-list=OS_LIST] [--download=PACKAGES]]"
+    echo ""
+    echo " optional arguments:"
+    echo "--help  -h               show this help message and exit"
+    echo "--os-list=<OS1>,<OS2>    Specific OS list to download, supported os are:"
+    for os in $(find ${BASE_DIR}/downloader/config -mindepth 1 -type d  | sort)
+    do
+        os_name=$(basename ${os})
+        echo "                         ${os_name}"
+    done
+    echo "--download=<PK1>,<PK2>==<Version>"
+    echo "                         Specific package list to download, supported packages are:"
+    for package_json in $(find ${BASE_DIR}/downloader/software -mindepth 1 -type f -name "*.json" | sort)
+    do
+        package=$(basename ${package_json} .json | sed "s/_/==/g")
+        echo "                         ${package}"
+    done
+    echo -e "\n  notes: When <Version> is missing, <PK> is the latest.\n"
+    exit 0
+}
+
+function parse_script_args() {
+    if [ $# = 0 ];then
+        print_usage
+    fi
+    while true; do
+        case "$1" in
+        --help | -h)
+            print_usage
+            ;;
+        --os-list=*)
+            OS_LIST=$(echo $1 | cut -d"=" -f2)
+            shift
+            ;;
+        --download=*)
+            PKG_LIST=$(echo $1| cut -d"=" -f2-)
+            shift
+            ;;
+        *)
+            if [ "x$1" != "x" ]; then
+                echo "ERROR" "Unsupported parameters: $1"
+                print_usage
+            fi
+            break
+            ;;
+        esac
+    done
+}
+
+function check_script_args()
+{
+    if [ -z "${OS_LIST}" ] && [ -z "${PKG_LIST}" ];then
+        echo "ERROR" "--os-list or --download expected one argument at least"
+        print_usage
+    fi
+
+    # --os-list
+    if $(echo "${OS_LIST}" | grep -Evq '^[a-zA-Z0-9._,]*$');then
+        echo "ERROR" "--os-list ${OS_LIST} is invalid"
+        print_usage
+    fi
+    local unsupport=${FALSE}
+    IFS=','
+    for os in ${OS_LIST}
+    do
+        if [ "${os}" = "." ] || [ ! -d ${BASE_DIR}/downloader/config/${os} ];then
+            echo "Error: not support download for ${os}"
+            unsupport=${TRUE}
+        fi
+    done
+    unset IFS
+    if [ ${unsupport} == ${TRUE} ];then
+        print_usage
+    fi
+
+    # --download
+    if $(echo "${PKG_LIST}" | grep -Evq '^[a-zA-Z0-9._,=]*$');then
+        echo "ERROR" "--download ${PKG_LIST} is invalid"
+        print_usage
+    fi
+    local unsupport=${FALSE}
+    IFS=','
+    for package in ${PKG_LIST}
+    do
+        case "${package}" in
+        CANN|MindStudio)
+            continue
+            ;;
+        *)
+        local name_version=$(echo ${package} | awk -F== '{print $1"_"$2}')
+            if [ ! -f ${BASE_DIR}/downloader/software/${name_version}.json ];then
+                echo "Error: not support download for ${package}"
+                unsupport=${TRUE}
+            fi
+            ;;
+        esac
+    done
+    unset IFS
+    if [ ${unsupport} == ${TRUE} ];then
+        print_usage
+    fi
+}
+
+function main()
+{
+    parse_script_args $*
+    check_script_args
+    get_python3 >/dev/null 2>&1
+    if [[ $? == 0 ]];then
+        local pycmd=$(get_python3)
+    else
+        install_python3
+        local pycmd="python3"
+    fi
+    local os_cmd=""
+    if [ ! -z "${OS_LIST}" ];then
+        os_cmd="--os-list ${OS_LIST}"
+    fi
+    local download_cmd=""
+    if [ ! -z "${PKG_LIST}" ];then
+        download_cmd="--download ${PKG_LIST}"
+    fi
+    ${pycmd} ${CUR_DIR}/downloader/downloader.py ${os_cmd} ${download_cmd}
+}
+
+main $*
