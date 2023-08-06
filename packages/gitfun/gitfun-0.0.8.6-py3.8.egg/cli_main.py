97f24aa96d1705c3b15794000baedc8449f259e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:31 +0000

    Add package gitfun/0.0.8.6/gitfun-0.0.8.6-py3.8.egg

---
+++ packages/gitfun/gitfun-0.0.8.6-py3.8.egg/cli_main.py
@@ -0,0 +1,34 @@
+import subprocess
+from github import Github
+import click
+import argparse
+from gitfun import GitFun as gf
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.group(context_settings=CONTEXT_SETTINGS)
+def cli():
+    pass
+
+
+
+
+@cli.command()
+def status():
+    click.echo(gf.status())
+
+@cli.command()
+@click.option('-url', type=str, help='Paste your link of the repo')
+@click.option('-m',type=str,help = "Commit message ")
+@click.option('-b',type=str, help ="Branch name for checkout")
+def pushbranch(url,m,b):
+    click.echo(gf.push_branch(url,m,b))
+
+@cli.command()
+def remote():
+    click.echo(gf.remote())
+
+
+@cli.command()
+@click.option('-b',type=str, help ="Branch name for checkout")
+def pullbranch(b):
+    click.echo(gf.pull_branch(b,b))
---
+++ packages/gitfun/gitfun-0.0.8.6-py3.8.egg/gitfun/GitFun.py
@@ -0,0 +1,62 @@
+import subprocess
+
+
+def constants(self,url:str,m:str,b:str) -> str:
+    cmd_remote = "git remote add origin {0}".format(url)
+    cmd_commit = "git commit -m {0}".format(m)
+    cmd_checkout_branch = "git checkout {0}".format(b)
+    cmd_branch = b
+    cmd_push = "git push origin {0}".format(b)
+    return cmd_remote,cmd_commit,cmd_branch,cmd_push,cmd_checkout_branch
+
+# def init_push(cmd_remote,cmd_commit) -> str:
+#     """
+#
+#     :param url:str, required
+#     The url is passed to add a remote repo link for the git
+#     :return:
+#     str  subprocess.run("git push --set-upstream origin master ")
+#     which pushes all the changes to repo
+#     """
+#
+#     print(subprocess.getoutput("git init"))
+#     subprocess.getoutput(cmd_remote)
+#     print(subprocess.getoutput("git add ."))
+#     try:
+#         if m == type(str):
+#             subprocess.getoutput(cmd_commit)
+#         elif m != type(str):
+#             subprocess.getoutput("git commit -m initial commit")
+#     except Exception as ex:
+#         print(ex)
+#     return subprocess.getoutput("git push --set-upstream origin master ")
+
+
+def status():
+    return subprocess.getoutput("git status")
+
+
+def push_branch(cmd_remote: str,cmd_commit: str,cmd_branch: str) -> str:
+    try:
+        if cmd_commit == str and cmd_branch == str:
+            subprocess.getoutput(cmd_branch)
+            subprocess.getoutput(cmd_remote)
+            subprocess.getoutput("git add .")
+            subprocess.getoutput(cmd_commit)
+            result = subprocess.getoutput("git push origin {0}").fomart(push_branch)
+        else:
+            subprocess.getoutput("git add .")
+            subprocess.getoutput("git commit -m initial_commit")
+            result = subprocess.getoutput("git push origin main")
+        return  result
+
+    except Exception as ex:
+        print(ex)
+
+
+def remote():
+    return subprocess.getoutput("git remote -v")
+
+def pull_branch(cmd_checkout_branch:str,cmd_branch:str) -> str:
+    subprocess.getoutput(cmd_checkout_branch)
+    return subprocess.getoutput("git pull origin {0}").format(cmd_branch)
