2e93c8d76d8b2a59004b880ee50c09dd0771a633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:14 +0000

    Add package testmodule3091/0.2.2/testmodule3091-0.2.2-py3-none-any.whl

---
+++ packages/testmodule3091/testmodule3091-0.2.2-py3-none-any.whl/testmodule3091/loadsetup.py
@@ -0,0 +1,36 @@
+from .teiauxiliary import read_yaml, load_directories, create_file_list
+import pkg_resources
+import os
+
+
+# TT_CFG = read_yaml("data/config/tei_transformer.yaml")
+# HTMLT_CFG = read_yaml("data/config/html_transformer.yaml")
+
+filenames = create_file_list()
+
+TT_CFG = read_yaml(filenames['config']['tei_transformer.yaml'])
+HTMLT_CFG = read_yaml(filenames['config']['html_transformer.yaml'])
+
+
+# PROJECT_DIR = os.path.dirname(__file__)
+# DATA_DIR = os.path.join(PROJECT_DIR, 'data')
+# RESOURCE = 'testmodule3091'
+
+
+
+		
+
+# for dir_ in os.listdir(DATA_DIR):
+# 	if not dir_.startswith('.'):
+# 		dir_path = os.path.join(DATA_DIR, dir_)) 
+# 		for file in os.listdir(dir_path):
+# 			if not dir_.startswith('.')
+		
+
+
+# res = ['regs.txt', 'diacrit.json', 'cons.json', 'vows.json', 'ftable.json', 'index_column.json', 'rows.json']
+# paths = ['/'.join(('data', i)) for i in res]
+# filenames = [pkg_resources.resource_filename(resource, path) for path in paths]
+
+# SCHEMA_PATH = TT_CFG["PATHS"]["schema_dir"]
+# SCENARIOS = load_directories(SCHEMA_PATH)
