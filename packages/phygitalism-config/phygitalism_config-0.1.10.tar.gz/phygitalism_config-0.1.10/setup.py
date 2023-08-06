faadcb243b57be8df646cb5edf513f406d365ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:56 +0000

    Add package phygitalism-config/0.1.10/phygitalism_config-0.1.10.tar.gz

---
+++ packages/phygitalism-config/phygitalism_config-0.1.10.tar.gz/phygitalism_config-0.1.10/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['phygitalism_config']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.8.2,<2.0.0',
+ 'toml>=0.10.2,<0.11.0',
+ 'typing-inspect>=0.6.0,<0.7.0']
+
+setup_kwargs = {
+    'name': 'phygitalism-config',
+    'version': '0.1.10',
+    'description': '',
+    'long_description': None,
+    'author': 'Phygitalism',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
