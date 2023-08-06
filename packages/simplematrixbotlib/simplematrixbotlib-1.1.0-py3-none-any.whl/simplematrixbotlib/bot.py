edac3d0d1c2fa423aac6598b7b4481f65cd0d392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:12 +0000

    Add package simplematrixbotlib/1.1.0/simplematrixbotlib-1.1.0-py3-none-any.whl

---
+++ packages/simplematrixbotlib/simplematrixbotlib-1.1.0-py3-none-any.whl/simplematrixbotlib/bot.py
@@ -0,0 +1,33 @@
+import asyncio
+import simplematrixbotlib as botlib
+
+class Bot:
+    """
+    A class for the bot developer to primarily interact with
+    Example Usage:
+
+    import simplematrixbotlib as bl
+    
+    creds = bl.Creds("home.server", "user", "pass")
+    bot = bl.Bot(creds)
+    bot.run()
+    """
+    
+    def __init__(self, creds):
+        self.creds = creds
+        self.api = botlib.API(self.creds)
+
+    async def main(self):
+        await self.api.login()
+        self.async_client = self.api.async_client
+
+        self.callbacks = botlib.Callbacks(self.async_client)
+        await self.callbacks.setup_callbacks()
+
+        await self.async_client.sync_forever(timeout=3000, full_state=True)
+
+    def run(self):
+        asyncio.get_event_loop().run_until_complete(self.main())
+
+
+
