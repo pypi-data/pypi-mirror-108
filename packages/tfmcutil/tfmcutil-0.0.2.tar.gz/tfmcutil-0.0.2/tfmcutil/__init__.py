56807a9a57206a688ab0fde224b77b1047b2b80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:29 +0000

    Add package tfmcutil/0.0.2/tfmcutil-0.0.2.tar.gz

---
+++ packages/tfmcutil/tfmcutil-0.0.2.tar.gz/tfmcutil-0.0.2/tfmcutil/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from .tfmcutil import TextFiles
+
+AUTHOR = "hrpzcf"
+EMAIL = "hrpzcf@foxmail.com"
+NAME = "tfmcutil"
+VERSION = "0.0.2"
+WEBSITE = "https://gitee.com/hrpzcf/tfmcutil"
+
+__all__ = ["TextFiles", "AUTHOR", "EMAIL", "NAME", "VERSION", "WEBSITE"]
