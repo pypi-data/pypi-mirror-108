2ad313198f9cefcb4a628635c64cef41a528aea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:22 +0000

    Add package bytesviewapi/0.1.6/bytesviewapi-0.1.6-py3-none-any.whl

---
+++ packages/bytesviewapi/bytesviewapi-0.1.6-py3-none-any.whl/bytesviewapi/bytesviewapi_exception.py
@@ -0,0 +1,7 @@
+# Custom exception class BytesviewException
+
+class BytesviewException(Exception):
+    """Base class for all other exceptions"""
+
+    def __init__(self, Error):
+        self.Error = Error
