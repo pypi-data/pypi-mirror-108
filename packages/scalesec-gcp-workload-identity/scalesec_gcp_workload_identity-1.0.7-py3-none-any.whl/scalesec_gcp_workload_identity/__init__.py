41380fb7971c43aab35c2908de35c84ab98c9bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:54 +0000

    Add package scalesec-gcp-workload-identity/1.0.7/scalesec_gcp_workload_identity-1.0.7-py3-none-any.whl

---
+++ packages/scalesec-gcp-workload-identity/scalesec_gcp_workload_identity-1.0.7-py3-none-any.whl/scalesec_gcp_workload_identity/__init__.py
@@ -0,0 +1,15 @@
+"""
+Logging handler.
+"""
+import logging
+
+# Set up logging to ``/dev/null`` like a library is supposed to.
+# http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
+class NullHandler(logging.Handler):
+    """
+    Set up logging to `/dev/null`.
+    """
+    def emit(self, record):
+        pass
+
+logging.getLogger('gcp_token_federation').addHandler(NullHandler())
