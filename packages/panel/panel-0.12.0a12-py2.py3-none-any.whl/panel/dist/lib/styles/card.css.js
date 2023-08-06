a32e7bb72c4710b9589bb402f53088b3f092ea38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:19 +0000

    Add package panel/0.12.0a12/panel-0.12.0a12-py2.py3-none-any.whl

---
+++ packages/panel/panel-0.12.0a12-py2.py3-none-any.whl/panel/dist/lib/styles/card.css.js
@@ -0,0 +1,41 @@
+const css = `
+.bk.card {
+  border: 1px solid rgba(0,0,0,.125);
+  border-radius: 0.25rem;
+}
+.bk.accordion {
+  border: 1px solid rgba(0,0,0,.125);
+}
+.bk.card-header {
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.03);
+  border-radius: 0.25rem;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+.bk.accordion-header {
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.03);
+  border-radius: 0;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+.bk.card-button {
+  background-color: transparent;
+  position: absolute;
+  left: 0.5em;
+}
+.bk.card-header-row {
+  position: relative !important;
+}
+.bk.card-title {
+  align-items: center;
+  display: flex !important;
+  font-size: 1.4em;
+  font-weight: bold;
+  position: relative !important;
+}
+`;
+export default css;
