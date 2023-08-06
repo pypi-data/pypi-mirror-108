f41e7239781300e43eb7c78b90748166bccf4450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:24 +0000

    Add package whois-parser/0.1.0/whois-parser-0.1.0.tar.gz

---
+++ packages/whois-parser/whois-parser-0.1.0.tar.gz/whois-parser-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['whois_parser', 'whois_parser.parsers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dataclasses-json>=0.5.3,<0.6.0',
+ 'dateparser>=1.0.0,<2.0.0',
+ 'poetry-version>=0.1.5,<0.2.0',
+ 'pyparsing>=2.4.7,<3.0.0']
+
+setup_kwargs = {
+    'name': 'whois-parser',
+    'version': '0.1.0',
+    'description': 'Yet another whois parser for Python',
+    'long_description': '# whois-parser\n\nYet another whois parser for Python. 🐍\n\n- Parse a whois record by using [PyParsing](https://github.com/pyparsing/pyparsing/) not Regex.\n- Return a parsed record as [dataclass](https://docs.python.org/3/library/dataclasses.html) not dict.\n\n## Installation\n\n```bash\npip install whois-parser\n```\n\n## Usage\n\n```python\nimport sh\nfrom whois_parser import WhoisParser\n\n# get whois record\nhostname = "google.co.jp"\nwhois = sh.Command("whois")\nraw_text = whois(hostname)\n\n# parse whois record\nparser = WhoisParser()\nrecord = parser.parse(raw_text, hostname=hostname)\nprint(record)\n# => WhoisRecord(raw_text="...", registrant=Registrant(organization=\'グーグル合同会社\', email=None, name=None, telephone=None), admin=Admin(organization=None, email=None, name=None, telephone=None), tech=Tech(organization=None, email=None, name=None, telephone=None), abuse=Abuse(email=None, telephone=None), statuses=[\'Connected (2022/03/31)\'], name_servers=[\'ns1.google.com\', \'ns2.google.com\', \'ns3.google.com\', \'ns4.google.com\'], domain=\'google.co.jp\', registrar=None, expires_at=None, registered_at=datetime.datetime(2001, 3, 22, 0, 0), updated_at=datetime.datetime(2021, 4, 1, 1, 5, 22, tzinfo=<StaticTzInfo \'JST\'>))\n```\n\n## Customize / Contribution\n\nWhois\'s responses will follow a semi-free text format. Thus, unfortunately, this library does not support all the formats in the wild.\n\nYou can create customized parsers to suit your needs. References are placed in `whois-parser/parsers/`.\n\nAny contribution is welcome.\n',
+    'author': 'Manabu Niseki',
+    'author_email': 'manabu.niseki@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ninoseki/whois-parser',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
