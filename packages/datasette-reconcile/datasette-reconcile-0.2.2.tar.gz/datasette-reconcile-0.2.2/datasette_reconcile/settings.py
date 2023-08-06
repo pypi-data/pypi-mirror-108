d96b6246ad5f15bf4bd0bf2c54bc88d7a1084962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:15 +0000

    Add package datasette-reconcile/0.2.2/datasette-reconcile-0.2.2.tar.gz

---
+++ packages/datasette-reconcile/datasette-reconcile-0.2.2.tar.gz/datasette-reconcile-0.2.2/datasette_reconcile/settings.py
@@ -0,0 +1,7 @@
+DEFAULT_LIMIT = 5
+DEFAULT_TYPE = {
+    "name": "Object",
+    "id": "object",
+}
+DEFAULT_IDENTIFER_SPACE = "http://rdf.freebase.com/ns/type.object.id"
+DEFAULT_SCHEMA_SPACE = "http://rdf.freebase.com/ns/type.object.id"
