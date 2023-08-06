8e326170c69489801a1b13f0b1b419352fefba2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:00 +0000

    Add package cityweather/1.0.0/cityweather-1.0.0.tar.gz

---
+++ packages/cityweather/cityweather-1.0.0.tar.gz/cityweather-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name = 'cityweather',
+    packages = ['cityweather'],
+    version = '1.0.0',
+    license = 'MIT',
+    description = 'Weather forecast and details',
+    author = 'Sidd',
+    keywords = ['weather','forecast','openweather','temperature'],
+    install_requires = ['requests',],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
