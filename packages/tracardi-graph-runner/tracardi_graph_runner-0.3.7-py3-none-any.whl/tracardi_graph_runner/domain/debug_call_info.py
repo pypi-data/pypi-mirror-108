9e37b4c78cde2872c6695fdfeabcea8f6f119a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:21 +0000

    Add package tracardi-graph-runner/0.3.7/tracardi_graph_runner-0.3.7-py3-none-any.whl

---
+++ packages/tracardi-graph-runner/tracardi_graph_runner-0.3.7-py3-none-any.whl/tracardi_graph_runner/domain/debug_call_info.py
@@ -0,0 +1,16 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from tracardi_graph_runner.domain.entity import Entity
+from tracardi_graph_runner.domain.input_params import InputParams
+from tracardi_graph_runner.domain.action_result import ActionResult
+
+
+class DebugCallInfo(BaseModel):
+    node: Entity
+    edge: Optional[Entity] = None
+    init: Optional[dict] = None
+    input: Optional[InputParams] = None
+    output: Optional[List[ActionResult]] = None
+    error: Optional[str] = None
