c44917ecc60aeadd41568388b526cd729fa3617b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:47 +0000

    Add package psychonaughtcrawler/1.0.1/psychonaughtcrawler-1.0.1-py3-none-any.whl

---
+++ packages/psychonaughtcrawler/psychonaughtcrawler-1.0.1-py3-none-any.whl/psychonaughtcrawler-1.0.1.data/scripts/run.py
@@ -0,0 +1,9 @@
+from psychonaughtcrawler.crawler import Crawler
+
+def main():
+    url = 'https://psychonautwiki.org/wiki/LSD'
+    c = Crawler(url)
+    c.print_doses()
+
+if __name__ == '__main__':
+    main()
