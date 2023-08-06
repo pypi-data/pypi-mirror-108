500bb28dfa12f9273bb6f3388e9a56db8d7f381c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:38 +0000

    Add package scli/0.0.4/scli-0.0.4-py3-none-any.whl

---
+++ packages/scli/scli-0.0.4-py3-none-any.whl/scli/contributors/dds.py
@@ -0,0 +1,19 @@
+
+def author():
+    return "Dilip Debsingha"
+def email():
+    return "ddsiha.cob@gmail.com"
+def experience():
+    return 10
+def skill():
+    return "SDET, Python"
+def company():
+    return "Vmware, Inc."
+
+
+def resume():
+    print(author())
+    print(email())
+    print(experience())
+    print(skill())
+    print(company())
