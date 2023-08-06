9cb40f616aac97a95c6cc6f1bb5c306803ced742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:39 +0000

    Add package inspy-logger/2.1a14/inspy_logger-2.1a14-py3-none-any.whl

---
+++ packages/inspy-logger/inspy_logger-2.1a14-py3-none-any.whl/inspy_logger/manifest/__init__.py
@@ -0,0 +1,41 @@
+from time import time
+from logging import  getLogger
+from box import Box
+
+
+class Manifest(object):
+    def __init__(self, root_name, root_device, calling_file,line_num):
+        self.name = root_name
+        
+        self.contents = None
+        self.add(root_name, root_device, calling_file, line_num)
+
+    def check(self, name):
+        if name in self.contents.keys():
+            return True
+        else:
+            return False
+
+    def add(self, name, logger_device, calling_file, line_num):
+        """
+        
+        Add the provided logger to the manifest.
+
+        Parameters:
+          name (str): The logger name.
+          logger_device ()
+        
+        """
+        
+        entry = { 
+                name: {
+                    'created-ts': time(),
+                    'device': logger_device,
+                    'caller_file': calling_file,
+                }
+            }
+
+        if self.contents is None:
+            self.contents = Box(entry)
+        else:
+            self.contents.update(entry)
