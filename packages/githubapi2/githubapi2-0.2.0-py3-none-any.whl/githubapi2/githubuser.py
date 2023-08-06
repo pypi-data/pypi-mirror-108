e26a55d9b6261d3bd614a0c6c80a68188254ac75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:21 +0000

    Add package githubapi2/0.2.0/githubapi2-0.2.0-py3-none-any.whl

---
+++ packages/githubapi2/githubapi2-0.2.0-py3-none-any.whl/githubapi2/githubuser.py
@@ -0,0 +1,45 @@
+import sys
+
+import requests
+
+GITHUB_API = "https://api.github.com"
+
+
+def get_username():
+    return str(input("Username: "))
+
+
+def get_user_details(username):
+    return requests.get(f"{GITHUB_API}/users/{username}").json()
+
+
+def get_important_data(user_details):
+    return {
+        "username": user_details["login"],
+        "id": user_details["id"],
+        "name": user_details["name"],
+        "company": user_details["company"],
+        "webpage": user_details["blog"],
+        "location": user_details["location"],
+        "bio": user_details["bio"],
+        "twitter_username": user_details["twitter_username"],
+        "total_public_repos": user_details["public_repos"],
+        "total_public_gists": user_details["public_gists"],
+        "total_followers": user_details["followers"],
+        "total_following": user_details["following"],
+        "created_date": user_details["created_at"],
+    }
+
+
+def print_user_data(data):
+    print(f"{data['name']}(@{data['username']}): {data['bio']}")
+
+
+def run():
+    user_details = get_user_details(get_username())
+    important_data = get_important_data(user_details)
+    print_user_data(important_data)
+
+
+if __name__ == "__main__":
+    run()
