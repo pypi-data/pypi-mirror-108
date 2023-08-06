34a25d42b8215569bf20f36ddc0548a3330e2881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:09 +0000

    Add package termitext/0.1.2/TermiText-0.1.2.tar.gz

---
+++ packages/termitext/TermiText-0.1.2.tar.gz/TermiText-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['termitext']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'termitext',
+    'version': '0.1.2',
+    'description': 'Color and format text in the console',
+    'long_description': None,
+    'author': 'Yek',
+    'author_email': 'gwojtysiak34@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
