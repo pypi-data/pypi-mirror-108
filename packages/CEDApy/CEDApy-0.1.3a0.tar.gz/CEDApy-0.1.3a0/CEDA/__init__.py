db6e6b54febf8d08ee4313dba31fa42717944ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:54 +0000

    Add package CEDApy/0.1.3a0/CEDApy-0.1.3a0.tar.gz

---
+++ packages/CEDApy/CEDApy-0.1.3a0.tar.gz/CEDApy-0.1.3a0/CEDA/__init__.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+__version__ = "0.1.3a"
+__author__ = "Terence Lau"
+
+
+if sys.version_info < (3, 6):
+    print(f"CEDA {__version__} requires Python 3.6+")
+    sys.exit(1)
+del sys
+
+from CEDA import * 
