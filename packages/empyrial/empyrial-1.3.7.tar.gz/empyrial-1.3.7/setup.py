1705146717aca7746be8449b1baefbd888ad7861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:13 +0000

    Add package empyrial/1.3.7/empyrial-1.3.7.tar.gz

---
+++ packages/empyrial/empyrial-1.3.7.tar.gz/empyrial-1.3.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='empyrial',
+    version='1.3.7',
+    description='AI and data-driven quantitative portfolio management for risk and performance analytics',
+    py_modules=['empyrial'],
+    package_dir={'': 'src'},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ssantoshp/Empyrial',
+    author="Santosh Passoubady",
+    author_email="santoshpassoubady@gmail.com",
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'pandas_datareader',
+        'datetime',
+        'empyrical',
+        'quantstats',
+        'yfinance',
+        'darts',
+        'yahoo-fin',
+        'yahoofinancials',
+        'prompt-toolkit==3.0.18',
+        'ipython==7.22.0',
+        'pyportfolioopt'
+
+    ],
+)
