99f00288a7171a9e393353a1a81182ce865b0e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package django-minio-backend/2.7.0/django_minio_backend-2.7.0-py3-none-any.whl

---
+++ packages/django-minio-backend/django_minio_backend-2.7.0-py3-none-any.whl/django_minio_backend/apps.py
@@ -0,0 +1,22 @@
+from django.apps import AppConfig
+from .utils import get_setting, ConfigurationError
+
+
+__all__ = ['DjangoMinioBackendConfig', ]
+
+
+class DjangoMinioBackendConfig(AppConfig):
+    name = 'django_minio_backend'
+
+    def ready(self):
+        consistency_check_on_start = get_setting('MINIO_CONSISTENCY_CHECK_ON_START', False)
+        if consistency_check_on_start:
+            from django.core.management import call_command
+            print("Executing consistency checks...")
+            call_command('initialize_buckets', silenced=True)
+
+        # Validate configuration combinations for EXTERNAL ENDPOINT
+        external_address = bool(get_setting('MINIO_EXTERNAL_ENDPOINT'))
+        external_use_https = get_setting('MINIO_EXTERNAL_ENDPOINT_USE_HTTPS')
+        if (external_address and external_use_https is None) or (not external_address and external_use_https):
+            raise ConfigurationError('MINIO_EXTERNAL_ENDPOINT must be configured together with MINIO_EXTERNAL_ENDPOINT_USE_HTTPS')
