95a4a27a46a6322993a8dbe7ae99926747464102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:40 +0000

    Add package torch-pruning/0.2.5/torch-pruning-0.2.5.tar.gz

---
+++ packages/torch-pruning/torch-pruning-0.2.5.tar.gz/torch-pruning-0.2.5/torch_pruning/prune/strategy.py
@@ -0,0 +1,50 @@
+import torch
+from abc import abstractclassmethod, ABC
+from typing import Sequence
+import random
+
+class BaseStrategy(ABC):
+    def __call__(self, weights, amount=0.0):
+        return self.apply(weights, amount=amount)
+
+    @abstractclassmethod
+    def apply(self, weights, amount=0.0)->  Sequence[int]:  # return index
+        """ Apply the strategy on weights with user specified pruning percentage.
+
+        Parameters:
+            weights (torch.Parameter): weights to be pruned.
+            amount (Callable): the percentage of weights to be pruned.
+        """
+        raise NotImplementedError
+
+class RandomStrategy(BaseStrategy):
+
+    def apply(self, weights, amount=0.0)->  Sequence[int]:  # return index
+        if amount<=0: return []
+        n = len(weights)
+        n_to_prune = int(amount*n)
+        indices = random.sample( list( range(n) ), k=n_to_prune )
+        return indices
+
+class LNStrategy(BaseStrategy):
+    def __init__(self, p):
+        self.p = p
+
+    def apply(self, weights, amount=0.0)->  Sequence[int]:  # return index
+        if amount<=0: return []
+        n = len(weights)
+        l1_norm = torch.norm( weights.view(n, -1), p=self.p, dim=1 )
+        n_to_prune = int(amount*n)
+        if n_to_prune == 0:
+            return []
+        threshold = torch.kthvalue(l1_norm, k=n_to_prune).values 
+        indices = torch.nonzero(l1_norm <= threshold).view(-1).tolist()
+        return indices
+
+class L1Strategy(LNStrategy):
+    def __init__(self):
+        super(L1Strategy, self).__init__(p=1)
+
+class L2Strategy(LNStrategy):
+    def __init__(self):
+        super(L2Strategy, self).__init__(p=2)
