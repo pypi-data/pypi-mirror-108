e6d128b59a31c108ab107a2702e63ed012643590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:26 +0000

    Add package dframeio/0.1.0/dframeio-0.1.0-py3-none-any.whl

---
+++ packages/dframeio/dframeio-0.1.0-py3-none-any.whl/dframeio/abstract.py
@@ -0,0 +1,52 @@
+"""Abstract interfaces for all storage backends"""
+from abc import abstractmethod
+from typing import Dict, List, Union
+
+try:
+    import pandas as pd
+except ImportError:
+    pd = None
+
+
+class AbstractDataFrameReader:
+    """Interface for reading dataframes from different storage drivers"""
+
+    @abstractmethod
+    def read_to_pandas(
+        self,
+        source: str,
+        columns: List[str] = None,
+        row_filter: str = None,
+        drop_duplicates: bool = False,
+        limit: int = -1,
+        sample: int = -1,
+    ) -> pd.DataFrame:
+        """Read data into a pandas.DataFrame"""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def read_to_dict(
+        self,
+        source: str,
+        columns: List[str] = None,
+        row_filter: str = None,
+        drop_duplicates: bool = False,
+        limit: int = -1,
+        sample: int = -1,
+    ) -> Dict[str, List]:
+        """Read data into a dict of named columns"""
+        raise NotImplementedError()
+
+
+class AbstractDataFrameWriter:
+    """Interface for writing dataframes to different storage drivers"""
+
+    @abstractmethod
+    def write_replace(self, target: str, dataframe: Union[pd.DataFrame, Dict[str, List]]):
+        """Write data with full replacement of an existing dataset"""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def write_append(self, target: str, dataframe: Union[pd.DataFrame, Dict[str, List]]):
+        """Write data in append-mode"""
+        raise NotImplementedError()
