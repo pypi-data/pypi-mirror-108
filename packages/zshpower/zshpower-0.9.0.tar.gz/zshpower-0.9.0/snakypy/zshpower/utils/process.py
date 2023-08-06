aa3e0eaa22163c803cff2336936f53146966d9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:54 +0000

    Add package zshpower/0.9.0/zshpower-0.9.0.tar.gz

---
+++ packages/zshpower/zshpower-0.9.0.tar.gz/zshpower-0.9.0/snakypy/zshpower/utils/process.py
@@ -0,0 +1,18 @@
+from subprocess import call as subprocess_call
+
+from snakypy.helpers import FG, printer
+from snakypy.helpers.catches import shell, whoami
+
+
+def reload_zsh() -> None:
+    subprocess_call("exec zsh", shell=True)
+
+
+def change_shell() -> bool:
+    if shell() != "zsh":
+        try:
+            subprocess_call(f"chsh -s $(which zsh) {whoami()}", shell=True)
+            return True
+        except KeyboardInterrupt:
+            printer("Canceled by user.", foreground=FG().WARNING)
+    return False
