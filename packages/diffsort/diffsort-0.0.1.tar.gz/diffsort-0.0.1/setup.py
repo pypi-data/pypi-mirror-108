9594638fda8b7262750ebbd96f976fb148d7e23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:26 +0000

    Add package diffsort/0.0.1/diffsort-0.0.1.tar.gz

---
+++ packages/diffsort/diffsort-0.0.1.tar.gz/diffsort-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='diffsort',
+    version='0.0.1',
+    author='Felix Petersen',
+    description='Differentiable Sorting Networks.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Felix-Petersen/diffsort',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'diffsort': 'diffsort'},
+    packages=['diffsort'],
+    python_requires='>=3.7',
+    install_requires=[
+        'torch==1.4.0',
+        'numpy',
+    ],
+)
