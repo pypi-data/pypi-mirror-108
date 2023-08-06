d16b8f8b60226b0655eba5d81e8d34699ae7a649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:08 +0000

    Add package Client-API-VN/2.8.1/Client_API_VN-2.8.1.tar.gz

---
+++ packages/Client-API-VN/Client_API_VN-2.8.1.tar.gz/Client_API_VN-2.8.1/tests/test_evnconf.py
@@ -0,0 +1,456 @@
+"""
+Test each property of envconf module.
+"""
+from datetime import datetime
+import logging
+import shutil
+from pathlib import Path
+
+import pytest
+
+from export_vn import __version__
+from export_vn.evnconf import EvnConf
+from export_vn.evnconf import MissingConfigurationFile
+from strictyaml import YAMLValidationError
+
+# Testing constants
+CRTL = "observations"
+
+
+# Create test site configuration file
+@pytest.fixture(
+    scope="session",
+    params=[
+        {
+            "file": "evn_tst1.yaml",
+            "site": "tst1",
+            "site_enabled": True,
+            "file_enabled": True,
+            "db_enabled": True,
+            "start_date": datetime(2019, 8, 1),
+            "end_date": datetime(2019, 9, 1),
+        },
+        {
+            "file": "evn_tst1.yaml",
+            "site": "tst2",
+            "site_enabled": False,
+            "file_enabled": True,
+            "db_enabled": True,
+            "start_date": datetime(2019, 8, 1),
+            "end_date": datetime(2019, 9, 1),
+        },
+        {
+            "file": "evn_tst2.yaml",
+            "site": "tst3",
+            "site_enabled": True,
+            "file_enabled": False,
+            "db_enabled": False,
+            "start_date": None,
+            "end_date": None,
+        },
+        {
+            "file": "evn_tst3.yaml",
+            "site": "tst4",
+            "site_enabled": True,
+            "file_enabled": False,
+            "db_enabled": False,
+            "start_date": None,
+            "end_date": None,
+        },
+    ],
+)
+def create_file(request):
+    cfg_file = "." + request.param["file"]
+    # Copy test file to HOME
+    in_file = (
+        Path.home()
+        / "Code"
+        / "Client_API_VN"
+        / "tests"
+        / "data"
+        / request.param["file"]
+    )
+    out_file = Path.home() / cfg_file
+    if (not out_file.is_file()) or (in_file.stat().st_mtime > out_file.stat().st_mtime):
+        shutil.copy(in_file, out_file)
+    # Instantiate configuration classes
+    if request.param["file"] == "evn_tst3.yaml":
+        try:
+            cfg = EvnConf(cfg_file)
+        except YAMLValidationError:
+            pytest.skip("Found YAML error as expected")
+            return
+    else:
+        cfg = EvnConf(cfg_file)
+    c_cfg = cfg.ctrl_list[CRTL]
+    s_cfg = cfg.site_list[request.param["site"]]
+    return (cfg, c_cfg, s_cfg, cfg_file, request.param)
+
+
+def test_no_file():
+    """Check for exception is no configuration file."""
+    cfg_file = ".QfB7F0jvnC7V.yaml"
+    try:
+        EvnConf(cfg_file)
+    except MissingConfigurationFile:
+        pytest.skip("Expected evnconf MissingConfigurationFile error")
+
+
+def test_version(create_file):
+    """Check if version is defined."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    pkg_version = cfg.version
+    logging.debug("package version: %s", pkg_version)
+    assert pkg_version == __version__
+
+
+def test_ctrl_list(create_file):
+    """Check if list of controlers is complete."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    ctrl_list = cfg.ctrl_list
+    for ctrl in {
+        "entities",
+        "families",
+        "fields",
+        "local_admin_units",
+        "observations",
+        "observers",
+        "places",
+        "species",
+        "taxo_groups",
+        "territorial_units",
+        "validations",
+    }:
+        assert ctrl in ctrl_list
+
+
+def test_site(create_file):
+    """Check if configuration file exists."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    cfg_file = Path.home() / cfg_file
+    assert cfg_file.is_file()
+
+
+def test_site_name(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.site == params["site"]
+
+
+def test_site_enabled(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.enabled == params["site_enabled"]
+
+
+def test_ctrl_enabled(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert c_cfg.enabled
+
+
+def test_ctrl_schedule_year(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert c_cfg.schedule_year == "*"
+    else:
+        assert c_cfg.schedule_year is None
+
+
+def test_ctrl_schedule_month(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert c_cfg.schedule_month == "*"
+    else:
+        assert c_cfg.schedule_month is None
+
+
+def test_ctrl_schedule_day(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert c_cfg.schedule_day == "*"
+    else:
+        assert c_cfg.schedule_day is None
+
+
+def test_ctrl_schedule_week(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert c_cfg.schedule_week == "*"
+    else:
+        assert c_cfg.schedule_week is None
+
+
+def test_ctrl_schedule_day_of_week(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert c_cfg.schedule_day_of_week == "*"
+    else:
+        assert c_cfg.schedule_day_of_week == "2"
+
+
+def test_ctrl_schedule_hour(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert c_cfg.schedule_hour == "*"
+    else:
+        assert c_cfg.schedule_hour is None
+
+
+def test_ctrl_schedule_minute(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert c_cfg.schedule_minute == "0"
+    else:
+        assert c_cfg.schedule_minute is None
+
+
+def test_ctrl_schedule_second(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert c_cfg.schedule_second == "0"
+    else:
+        assert c_cfg.schedule_second is None
+
+
+def test_taxo_exclude(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.taxo_exclude == ["TAXO_GROUP_ALIEN_PLANTS"]
+
+
+def test_territorial_unit_ids(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.territorial_unit_ids == ["7"]
+
+
+def test_json_format(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.json_format == "short"
+
+
+def test_start_date(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.start_date == params["start_date"]
+
+
+def test_end_date(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.end_date == params["end_date"]
+
+
+def test_type_date(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if s_cfg.type_date is not None:
+        assert s_cfg.type_date == "sighting" or s_cfg.type_date == "entry"
+
+
+def test_base_url(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.base_url == "https://www.faune-xxx.org/"
+
+
+def test_user_email(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.user_email == "nom.prenom@example.net"
+
+
+def test_user_pw(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.user_pw == "user_pw"
+
+
+def test_client_key(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.client_key == "client_key"
+
+
+def test_client_secret(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.client_secret == "client_secret"
+
+
+def test_file_enabled(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.file_enabled == params["file_enabled"]
+
+
+def test_file_store(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if s_cfg.file_enabled:
+        assert s_cfg.file_store == "test_files" + "/" + params["site"] + "/"
+    else:
+        assert s_cfg.file_store == ""
+
+
+def test_db_enabled(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_enabled == params["db_enabled"]
+
+
+def test_db_host(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_host == "localhost"
+
+
+def test_db_port(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_port == "5432"
+
+
+def test_db_name(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_name == "faune_xxx"
+
+
+def test_db_schema_import(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_schema_import == "import"
+
+
+def test_db_schema_vn(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_schema_vn == "src_vn"
+
+
+def test_db_group(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_group == "lpo_xxx"
+
+
+def test_db_user(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_user == "xferxx"
+
+
+def test_db_pw(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_pw == "db_pw"
+
+
+def test_db_secret_key(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.db_secret_key == "mySecretKey"
+
+
+def test_tuning_max_list_length(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_max_list_length == 100
+
+
+def test_tuning_max_chunks(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_max_chunks == 10
+
+
+def test_tuning_max_retry(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_max_retry == 5
+
+
+def test_tuning_retry_delay(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_retry_delay == 5
+
+
+def test_tuning_unavailable_delay(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_unavailable_delay == 600
+
+
+def test_tuning_max_requests(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_max_requests == 0
+
+
+def test_tuning_lru_maxsize(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_lru_maxsize == 32
+
+
+def test_tuning_pid_kp(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_pid_kp == 0.0
+
+
+def test_tuning_pid_ki(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_pid_ki == 0.003
+
+
+def test_tuning_pid_kd(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_pid_kd == 0.0
+
+
+def test_tuning_pid_setpoint(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_pid_setpoint == 10000
+
+
+def test_tuning_pid_limit_min(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_pid_limit_min == 1
+
+
+def test_tuning_pid_limit_max(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_pid_limit_max == 2000
+
+
+def test_tuning_pid_delta_days(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    assert s_cfg.tuning_pid_delta_days == 15
+
+
+def test_tuning_sched_executors(create_file):
+    """ Test property."""
+    cfg, c_cfg, s_cfg, cfg_file, params = create_file
+    if params["site"] in ["tst1", "tst2"]:
+        assert s_cfg.tuning_sched_executors == 2
+    else:
+        assert s_cfg.tuning_sched_executors == 1
---
+++ packages/Client-API-VN/Client_API_VN-2.8.1.tar.gz/Client_API_VN-2.8.1/tests/test_store_postgresql.py
@@ -0,0 +1,190 @@
+"""
+Test each method of store_postgresql module.
+Also test them as backend to download_vn.
+"""
+import logging
+from datetime import datetime
+
+import pytest
+
+from export_vn.download_vn import (
+    Entities,
+    Families,
+    Fields,
+    LocalAdminUnits,
+    Observations,
+    Observers,
+    Places,
+    Species,
+    TaxoGroup,
+    TerritorialUnits,
+)
+from export_vn.evnconf import EvnConf
+from export_vn.store_postgresql import PostgresqlUtils, ReadPostgresql, StorePostgresql
+
+# Using faune-ardeche or faune-isere site, that needs to be created first
+SITE = "t07"
+# SITE = 't38'
+FILE = ".evn_test.yaml"
+
+# Get configuration for test site
+CFG = EvnConf(FILE).site_list[SITE]
+MANAGE_PG = PostgresqlUtils(CFG)
+# Skip exception if json tables are not yet created
+# Need to run test_create_json_tables first
+try:
+    STORE_PG = StorePostgresql(CFG)
+    READ_PG = ReadPostgresql(CFG)
+    ENTITIES = Entities(CFG, STORE_PG)
+    FAMILIES = Families(CFG, STORE_PG)
+    FIELDS = Fields(CFG, STORE_PG)
+    LOCAL_ADMIN_UNITS = LocalAdminUnits(CFG, STORE_PG)
+    OBSERVATIONS = Observations(CFG, STORE_PG)
+    OBSERVERS = Observers(CFG, STORE_PG)
+    PLACES = Places(CFG, STORE_PG)
+    SPECIES = Species(CFG, STORE_PG)
+    TAXO_GROUP = TaxoGroup(CFG, STORE_PG)
+    TERRITORIAL_UNIT = TerritorialUnits(CFG, STORE_PG)
+except Exception:
+    raise
+
+
+def test_version():
+    """Check if version is defined."""
+    logging.debug("package version: %s", STORE_PG.version)
+
+
+# --------
+# Database
+# --------
+def test_create_json_tables():
+    """Create the tables, if not exists."""
+    MANAGE_PG.create_json_tables()
+
+
+# ----
+# Logs
+# ----
+def test_log_pg_store():
+    """Create a log entry."""
+    STORE_PG.log(SITE, "Test", 0, 0, comment="Test")
+
+
+def test_increment_log_pg_store():
+    """Create an increment entry."""
+    STORE_PG.increment_log(SITE, 1, datetime.now())
+
+
+def test_increment_get_pg_store():
+    """Get an increment entry."""
+    last_ts = STORE_PG.increment_get(SITE, 1)
+    if last_ts is not None:
+        assert last_ts < datetime.now()
+
+
+# --------
+# Entities
+# --------
+def test_entities_api_pg_store():
+    """Store entities to database."""
+    ENTITIES.store()
+
+
+# --------
+# Families
+# --------
+@pytest.mark.slow
+def test_families_api_pg_store():
+    """Store families to database."""
+    FAMILIES.store()
+
+
+# ------
+# Fields
+# ------
+@pytest.mark.slow
+def test_fields_api_pg_store():
+    """Store fields to database."""
+    FIELDS.store()
+
+
+# -----------------
+# Local_admin_units
+# -----------------
+@pytest.mark.slow
+def test_local_adm_u_api_pg_store():
+    """Store local_admin_units to database."""
+    LOCAL_ADMIN_UNITS.store()
+
+
+def test_local_adm_u_api_pg_read():
+    """Read local admin units from database."""
+    l_a_u = READ_PG.read("local_admin_units")
+    assert len(l_a_u) > 339
+
+
+# -------------
+#  Observations
+# -------------
+@pytest.mark.slow
+def test_observations_api_pg_store(capsys):
+    """Store observations of a taxo_group to database."""
+    with capsys.disabled():
+        OBSERVATIONS.store(18, method="search")
+
+
+# ---------
+# Observers
+# ---------
+@pytest.mark.slow
+def test_observers_api_pg_store():
+    """Store observers to database."""
+    OBSERVERS.store()
+
+
+# -------
+#  Places
+# -------
+@pytest.mark.slow
+def test_places_api_pg_store():
+    """Store places to database."""
+    PLACES.store()
+
+
+# --------
+#  Species
+# --------
+@pytest.mark.slow
+def test_species_api_pg_store():
+    """Store species to database."""
+    SPECIES.store()
+
+
+# ------------
+#  Taxo_groups
+# ------------
+def test_taxo_groups_api_pg_store():
+    """Store taxonomic groups to database."""
+    TAXO_GROUP.store()
+
+
+# ------------------
+#  Territorial units
+# ------------------
+def test_terr_u_api_pg_store():
+    """Store territorial units to database."""
+    TERRITORIAL_UNIT.store()
+
+
+def test_terr_u_api_pg_read():
+    """Read territorial units from database."""
+    t_u = READ_PG.read("territorial_units")
+    assert len(t_u) == 1
+
+
+# -------------
+#  Finalization
+# -------------
+def test_finish_pg_store():
+    """Finalize tasks."""
+    STORE_PG.__exit__(0, 1, 2)
