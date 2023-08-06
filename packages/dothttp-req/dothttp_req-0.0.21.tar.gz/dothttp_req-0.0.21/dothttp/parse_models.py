4c0e21931953232d84ac9cbe7a8910e7bdf50d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:22 +0000

    Add package dothttp-req/0.0.21/dothttp_req-0.0.21.tar.gz

---
+++ packages/dothttp-req/dothttp_req-0.0.21.tar.gz/dothttp_req-0.0.21/dothttp/parse_models.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+
+@dataclass
+class Allhttp:
+    pass
+
+
+@dataclass
+class NameWrap:
+    name: str
+
+
+@dataclass
+class UrlWrap:
+    method: str
+    url: str
+
+
+@dataclass
+class BasicAuth:
+    username: str
+    password: str
+
+
+@dataclass
+class Query:
+    key: str
+    value: str
+
+
+@dataclass
+class Header:
+    key: str
+    value: str
+
+
+@dataclass
+class Line:
+    header: Optional[Header]
+    query: Optional[Query]
+
+
+@dataclass
+class MultiPartFile:
+    name: str
+    path: str
+    type: Optional[str] = None
+
+
+@dataclass
+class FilesWrap:
+    files: List[MultiPartFile]
+
+
+@dataclass
+class TripleOrDouble:
+    triple: Optional[str] = None
+    str: Optional[str] = None
+
+
+@dataclass
+class Payload:
+    data: Optional[List[TripleOrDouble]]
+    datajson: Optional
+    file: Optional[str]
+    json: Optional
+    fileswrap: Optional[FilesWrap]
+    type: Optional[str]
+    multi: Optional[str] = None
+
+
+@dataclass
+class ToFile:
+    output: str
+
+
+@dataclass
+class TestScript:
+    script: Optional[str]
+
+
+@dataclass
+class Http:
+    namewrap: Optional[NameWrap]
+    urlwrap: UrlWrap
+    basic_auth_wrap: Optional[BasicAuth]
+    lines: Optional[List[Line]]
+    payload: Optional[Payload]
+    output: Optional[ToFile]
+    script_wrap: Optional[TestScript] = field(default_factory=lambda: TestScript(''))
+
+
+@dataclass
+class Allhttp:
+    allhttps: List[Http]
