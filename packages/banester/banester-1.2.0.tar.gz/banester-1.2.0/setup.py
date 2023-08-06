1cfbda0298c5d1acaf1d4801e06794389f73709f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:26 +0000

    Add package banester/1.2.0/banester-1.2.0.tar.gz

---
+++ packages/banester/banester-1.2.0.tar.gz/banester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'banester',
+version = '1.2.0',
+py_modules = ['banester'],
+author = 'Aliaksandr.Berdnik',
+author_email = 'berdnikora@gmail.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple printer of nested lists',
+)
