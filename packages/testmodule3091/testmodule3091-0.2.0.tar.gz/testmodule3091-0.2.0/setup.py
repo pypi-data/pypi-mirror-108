4058e168bc503b3c8b14105785417cd1c5412aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:09 +0000

    Add package testmodule3091/0.2.0/testmodule3091-0.2.0.tar.gz

---
+++ packages/testmodule3091/testmodule3091-0.2.0.tar.gz/testmodule3091-0.2.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.0' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="testmodule3091", 
+        version=VERSION,
+        author="AKS",
+        author_email="",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+        'PyYAML==5.4.1', 
+        ], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer',
+        
+        include_package_data=True,
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
