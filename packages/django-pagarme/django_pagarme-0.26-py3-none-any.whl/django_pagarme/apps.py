8761cf7fa82e9db396b63422eff6a1b7a1fd820f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:36 +0000

    Add package django-pagarme/0.26/django_pagarme-0.26-py3-none-any.whl

---
+++ packages/django-pagarme/django_pagarme-0.26-py3-none-any.whl/django_pagarme/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoPagarmeConfig(AppConfig):
+    name = 'django_pagarme'
+    verbose_name = "Dados do Pagarme"
+    default_auto_field = 'django.db.models.BigAutoField'
