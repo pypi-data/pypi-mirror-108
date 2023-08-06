a0bac25d318c14aa365f9c1a79a79c144bb8f0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:19 +0000

    Add package gracefml/0.0.2/gracefml-0.0.2-py3-none-any.whl

---
+++ packages/gracefml/gracefml-0.0.2-py3-none-any.whl/gracefml/bounding/bf.py
@@ -0,0 +1,12 @@
+class BoundingFunction:
+    """Base class for all bounding functions.
+
+    Bounding functions return the expected domain of a uniform random variable, or can return None in order to not contribute to drawing bounds.
+
+    Parameters
+    ----------
+
+    """
+
+    def __Init__(self) -> None:
+        pass
