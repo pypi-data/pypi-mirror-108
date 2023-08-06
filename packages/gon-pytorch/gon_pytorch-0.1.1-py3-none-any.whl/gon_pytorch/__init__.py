b7d29f4e445e5dd813da35586ea0fc0c3d320f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:34 +0000

    Add package gon-pytorch/0.1.1/gon_pytorch-0.1.1-py3-none-any.whl

---
+++ packages/gon-pytorch/gon_pytorch-0.1.1-py3-none-any.whl/gon_pytorch/__init__.py
@@ -0,0 +1,14 @@
+from .modules import (
+    CoordinateEncoding,
+    IdentityPositionalEncoding,
+    GaussianFourierFeatureTransform,
+    NeRFPositionalEncoding,
+    LinearBlock,
+    SirenLinear,
+    LinearBlockFactory,
+    SirenBlockFactory,
+    Swish,
+    Sine,
+    ImplicitDecoder,
+    GON
+)
