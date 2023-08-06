cd4db0d0f750039f9e0dc87efde7b02ca75c45b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:29 +0000

    Add package sinatra/0.0.1/sinatra-0.0.1.tar.gz

---
+++ packages/sinatra/sinatra-0.0.1.tar.gz/sinatra-0.0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup()
+# import cemake
+
+# setuptools.setup(
+#     ext_modules = [cemake.CMakeExtension("ganondorf.core")],
+#     cmdclass = {'build_ext': cemake.cmake_build_ext}
+#     )
