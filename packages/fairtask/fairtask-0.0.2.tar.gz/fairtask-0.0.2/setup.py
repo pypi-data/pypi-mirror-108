ef247402ddcfd473f34b8df21f47cff5b49f7ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:54 +0000

    Add package fairtask/0.0.2/fairtask-0.0.2.tar.gz

---
+++ packages/fairtask/fairtask-0.0.2.tar.gz/fairtask-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'fairtask',
+    version = '0.0.2',
+    description = 'This is only a placeholder package.',
+)
+
+raise RuntimeError("This is a placeholder package that was never meant to be actually installed from public PyPI. Please double check your installation instructions.")
