e11a442c1457f2d4896afb56842255c4f5825ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:24 +0000

    Add package onnxruntime-extensions/0.3.1/onnxruntime_extensions-0.3.1-cp39-cp39-macosx_10_14_x86_64.whl

---
+++ packages/onnxruntime-extensions/onnxruntime_extensions-0.3.1-cp39-cp39-macosx_10_14_x86_64.whl/onnxruntime_extensions/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+###############################################################################
+
+"""
+The entry point to onnxruntime custom op library
+"""
+
+__version__ = "0.3.1"
+__author__ = "Microsoft"
+
+
+from ._ocos import get_library_path  # noqa
+from ._ocos import Opdef, PyCustomOpDef, hash_64,  enable_custom_op  # noqa
+from ._ocos import expand_onnx_inputs  # noqa
+from ._ocos import hook_model_op  # noqa
+from ._ocos import default_opset_domain  # noqa
+from ._cuops import *  # noqa
+from .eager_op import EagerOp as PyOrtFunction
+
+
+onnx_op = Opdef.declare
+PyOp = PyCustomOpDef
