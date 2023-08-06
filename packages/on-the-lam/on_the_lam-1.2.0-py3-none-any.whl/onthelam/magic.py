b9ff8ecccd264dcef88750ba33bd3170716dcccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:52 +0000

    Add package on-the-lam/1.2.0/on_the_lam-1.2.0-py3-none-any.whl

---
+++ packages/on-the-lam/on_the_lam-1.2.0-py3-none-any.whl/onthelam/magic.py
@@ -0,0 +1,39 @@
+"""
+Magically create new LambdaBuilder instances for any object imported from
+this module.
+
+Examples:
+
+>>> from onthelam.magic import foo, bar
+>>> foo + bar
+"bar, foo -> foo + bar"
+
+>>> import onthelam.magic as _
+>>> _.b[_.a]
+"a, b -> b[a]"
+"""
+
+import sys
+from types import ModuleType
+from typing import Any
+
+from .lambdabuilder import LambdaBuilder
+
+
+_ = LambdaBuilder("_")
+
+
+class DynamicModule(ModuleType):
+    """Module type that generates a new LambdaBuilder on any module access"""
+
+    def __getattr__(self, name: str) -> Any:
+        if name.startswith("__") and name.endswith("__"):
+            return self.__getattribute__(name)
+        return LambdaBuilder(name)
+
+    @property
+    def __all__(self) -> list[str]:
+        return [*map("_" + _, map(str, range(10)))]
+
+
+sys.modules[__name__].__class__ = DynamicModule
