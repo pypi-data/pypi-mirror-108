8182d4b69cb0d8bf0825a5b28976a925496dcb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:59 +0000

    Add package asciiplot/0.1.1/asciiplot-0.1.1.tar.gz

---
+++ packages/asciiplot/asciiplot-0.1.1.tar.gz/asciiplot-0.1.1/asciiplot/__init__.py
@@ -0,0 +1,5 @@
+from asciiplot._asciiization import asciiize
+from asciiplot._coloring import Color
+
+
+__version__ = '0.1.1'
---
+++ packages/asciiplot/asciiplot-0.1.1.tar.gz/asciiplot-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+from asciiplot import __version__
+
+
+with open(os.path.join(os.getcwd(), 'README.md'), 'r', encoding='utf-8') as f:
+    readme_content = f.read()
+setup(
+    name='asciiplot',
+    packages=find_packages(exclude=(["*.tests", "*.tests.*", "tests.*", "tests"])),
+    version=__version__,
+    # long_description=readme_content,
+    # long_description_content_type='text/markdown',
+    license='MIT',
+    keywords=['plotting', 'terminal', 'console', 'ascii', 'gui', 'data', 'visualization', 'statistics'],
+    url='https://github.com/w2sv/asciiplot',
+    python_requires='>=3.6',
+    install_requires=[
+        'colored==1.4.2',
+        'more-itertools',
+        'dataclasses'
+    ],
+    author='w2sv',
+    author_email='zangenbergjanek@googlemail.com',
+    platforms=['Linux', 'Windows', 'MacOS'],
+    description='Platform-agnostic, highly customizable sequence plotting in terminal for GUIs'
+)
+
+
+
