838e1d536f482edb918eb869c459aa44ee1e2ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:32 +0000

    Add package sample-list-read/1.0.0/sample_list_read-1.0.0.tar.gz

---
+++ packages/sample-list-read/sample_list_read-1.0.0.tar.gz/sample_list_read-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from distutils.core import setup
+setup(
+	name="sample_list_read",
+	version="1.0.0",
+	py_modules=['installing_python_example'],
+	author='ganeshperi',
+	author_email='ganeshperi14@gmail.com',
+	url='https://www.donotexist.com',
+	description='testing the installtion process learned'
+)
