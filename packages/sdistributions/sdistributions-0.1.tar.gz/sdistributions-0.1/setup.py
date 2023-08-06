d3cca4d941a4e8b745497a7fcf09e0ffee9ec890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:33 +0000

    Add package sdistributions/0.1/sdistributions-0.1.tar.gz

---
+++ packages/sdistributions/sdistributions-0.1.tar.gz/sdistributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sdistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sdistributions'],
+      author = 'Udacity AWS course',
+      zip_safe=False)
