d0636af37ca6ba35ae154e381ec887e7e8f30f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:35 +0000

    Add package determined/0.15.6/determined-0.15.6-py3-none-any.whl

---
+++ packages/determined/determined-0.15.6-py3-none-any.whl/determined/common/experimental/determined.py
@@ -0,0 +1,187 @@
+import pathlib
+from typing import Any, Dict, List, Optional, Union
+
+from determined._swagger.client.api.experiments_api import ExperimentsApi
+from determined._swagger.client.api.internal_api import InternalApi
+from determined._swagger.client.api.trials_api import TrialsApi
+from determined._swagger.client.api_client import ApiClient
+from determined._swagger.client.configuration import Configuration
+from determined._swagger.client.models.v1_create_experiment_request import V1CreateExperimentRequest
+from determined._swagger.client.models.v1_file import V1File
+from determined.common import api, check, context, util, yaml
+from determined.common.experimental import checkpoint, experiment, model, session, trial
+
+
+def _path_to_files(path: pathlib.Path) -> List[V1File]:
+    files = []
+    for item in context.read_context(path)[0]:
+        content = item["content"].decode("utf-8")
+        file = V1File(
+            path=item["path"],
+            type=item["type"],
+            content=content,
+            mtime=item["mtime"],
+            uid=item["uid"],
+            gid=item["gid"],
+            mode=item["mode"],
+        )
+        files.append(file)
+    return files
+
+
+class Determined:
+    """
+    Determined gives access to Determined API objects.
+
+    Arguments:
+        master (string, optional): The URL of the Determined master. If
+            this argument is not specified, the environment variables ``DET_MASTER`` and
+            ``DET_MASTER_ADDR`` will be checked for the master URL in that order.
+        user (string, optional): The Determined username used for
+            authentication. (default: ``determined``)
+    """
+
+    def __init__(
+        self,
+        master: Optional[str] = None,
+        user: Optional[str] = None,
+    ):
+        self._session = session.Session(master, user)
+
+        userauth = api.authentication.Authentication.instance()
+
+        configuration = Configuration()
+        configuration.host = self._session._master.rstrip("/")
+        configuration.username = userauth.token_store.get_active_user()
+        configuration.api_key_prefix["Authorization"] = "Bearer"
+        configuration.api_key["Authorization"] = userauth.get_session_token()
+
+        self._experiments = ExperimentsApi(ApiClient(configuration))
+        self._internal = InternalApi(ApiClient(configuration))
+        self._trials = TrialsApi(ApiClient(configuration))
+
+    def create_experiment(
+        self,
+        config: Union[str, pathlib.Path, Dict],
+        model_dir: str,
+    ) -> experiment.ExperimentReference:
+        """
+        Create an experiment with config parameters and model direcotry. The function
+        returns :class:`~determined.experimental.ExperimentReference` of the experiment.
+
+        Arguments:
+            config(string, pathlib.Path, dictionary): experiment config filename (.yaml)
+                or a dict.
+            model_dir(string): directory containing model definition.
+        """
+        check.is_instance(
+            config, (str, pathlib.Path, dict), "config parameter must be dictionary or path"
+        )
+        if isinstance(config, str):
+            with open(config) as f:
+                experiment_config = util.safe_load_yaml_with_exceptions(f)
+        elif isinstance(config, pathlib.Path):
+            with config.open() as f:
+                experiment_config = util.safe_load_yaml_with_exceptions(f)
+        elif isinstance(config, Dict):
+            experiment_config = config
+
+        model_context = _path_to_files(pathlib.Path(model_dir))
+
+        experiment_request = V1CreateExperimentRequest(
+            model_definition=model_context,
+            config=yaml.safe_dump(experiment_config),
+        )
+        experiment_response = self._internal.determined_create_experiment(experiment_request)
+        return experiment.ExperimentReference(
+            experiment_response.experiment.id,
+            self._session._master,
+            self._experiments,
+        )
+
+    def get_experiment(self, experiment_id: int) -> experiment.ExperimentReference:
+        """
+        Get the :class:`~determined.experimental.ExperimentReference` representing the
+        experiment with the provided experiment ID.
+        """
+        return experiment.ExperimentReference(
+            experiment_id,
+            self._session._master,
+            self._experiments,
+        )
+
+    def get_trial(self, trial_id: int) -> trial.TrialReference:
+        """
+        Get the :class:`~determined.experimental.TrialReference` representing the
+        trial with the provided trial ID.
+        """
+        return trial.TrialReference(trial_id, self._session._master, self._trials)
+
+    def get_checkpoint(self, uuid: str) -> checkpoint.Checkpoint:
+        """
+        Get the :class:`~determined.experimental.Checkpoint` representing the
+        checkpoint with the provided UUID.
+        """
+        r = api.get(self._session._master, "/api/v1/checkpoints/{}".format(uuid)).json()
+        return checkpoint.Checkpoint.from_json(r["checkpoint"], master=self._session._master)
+
+    def create_model(
+        self, name: str, description: Optional[str] = "", metadata: Optional[Dict[str, Any]] = None
+    ) -> model.Model:
+        """
+        Add a model to the model registry.
+
+        Arguments:
+            name (string): The name of the model. This name must be unique.
+            description (string, optional): A description of the model.
+            metadata (dict, optional): Dictionary of metadata to add to the model.
+        """
+        r = api.post(
+            self._session._master,
+            "/api/v1/models/{}".format(name),
+            body={"description": description, "metadata": metadata},
+        )
+
+        return model.Model.from_json(r.json().get("model"), self._session._master)
+
+    def get_model(self, name: str) -> model.Model:
+        """
+        Get the :class:`~determined.experimental.Model` from the model registry
+        with the provided name. If no model with that name is found in the registry,
+        an exception is raised.
+        """
+        r = api.get(self._session._master, "/api/v1/models/{}".format(name))
+        return model.Model.from_json(r.json().get("model"), self._session._master)
+
+    def get_models(
+        self,
+        sort_by: model.ModelSortBy = model.ModelSortBy.NAME,
+        order_by: model.ModelOrderBy = model.ModelOrderBy.ASCENDING,
+        name: str = "",
+        description: str = "",
+    ) -> List[model.Model]:
+        """
+        Get a list of all models in the model registry.
+
+        Arguments:
+            sort_by: Which field to sort by. See :class:`~determined.experimental.ModelSortBy`.
+            order_by: Whether to sort in ascending or descending order. See
+                :class:`~determined.experimental.ModelOrderBy`.
+            name: If this parameter is set, models will be filtered to only
+                include models with names matching this parameter.
+            description: If this parameter is set, models will be filtered to
+                only include models with descriptions matching this parameter.
+        """
+        r = api.get(
+            self._session._master,
+            "/api/v1/models/",
+            params={
+                "sort_by": sort_by.value,
+                "order_by": order_by.value,
+                "name": name,
+                "description": description,
+            },
+        )
+
+        models = r.json().get("models")
+        return [model.Model.from_json(m, self._session._master) for m in models]
---
+++ packages/determined/determined-0.15.6-py3-none-any.whl/determined/common/experimental/experiment.py
@@ -0,0 +1,176 @@
+import time
+from typing import List, Optional
+
+from determined._swagger.client.api.experiments_api import ExperimentsApi
+from determined._swagger.client.models.determinedexperimentv1_state import (
+    Determinedexperimentv1State,
+)
+from determined.common import api
+from determined.common.experimental import checkpoint
+
+
+class ExperimentReference:
+    """
+    Helper class that supports querying the set of checkpoints associated with an
+    experiment.
+    """
+
+    def __init__(
+        self,
+        experiment_id: int,
+        master: str,
+        api_ref: ExperimentsApi,
+    ):
+        self.id = experiment_id
+        self._master = master
+        self._experiments = api_ref
+
+    def activate(self) -> None:
+        self._experiments.determined_activate_experiment(id=self.id)
+
+    def archive(self) -> None:
+        self._experiments.determined_archive_experiment(id=self.id)
+
+    def cancel(self) -> None:
+        self._experiments.determined_cancel_experiment(id=self.id)
+
+    def delete(self) -> None:
+        self._experiments.determined_delete_experiment(id=self.id)
+
+    def get_config(self) -> object:
+        exp_resp = self._experiments.determined_get_experiment(experiment_id=self.id)
+        return exp_resp.config
+
+    def kill(self) -> None:
+        self._experiments.determined_kill_experiment(id=self.id)
+
+    def pause(self) -> None:
+        self._experiments.determined_pause_experiment(id=self.id)
+
+    def unarchive(self) -> None:
+        self._experiments.determined_unarchive_experiment(id=self.id)
+
+    def wait(self, interval: int = 5) -> None:
+        """
+        Wait for the experiment to reach a complete or terminal state.
+
+        Arguments:
+            interval (int, optional): An interval time in seconds before checking
+                next experiement state.
+        """
+        elapsed_time = 0
+        while True:
+            exp_resp = self._experiments.determined_get_experiment(experiment_id=self.id)
+            if exp_resp.experiment.state in (
+                Determinedexperimentv1State.COMPLETED,
+                Determinedexperimentv1State.CANCELED,
+                Determinedexperimentv1State.DELETED,
+                Determinedexperimentv1State.ERROR,
+            ):
+                break
+            elif exp_resp.experiment.state == Determinedexperimentv1State.PAUSED:
+                raise ValueError(
+                    "Experiment {} is in paused state. Make sure the experiment is active.".format(
+                        self.id
+                    )
+                )
+            else:
+                # ACTIVE, STOPPING_COMPLETED, etc.
+                time.sleep(interval)
+                elapsed_time += interval
+                if elapsed_time % 60 == 0:
+                    print(
+                        "Waiting for Experiment {} to complete. Elapsed {} minutes".format(
+                            self.id, elapsed_time / 60
+                        )
+                    )
+
+    def top_checkpoint(
+        self,
+        sort_by: Optional[str] = None,
+        smaller_is_better: Optional[bool] = None,
+    ) -> checkpoint.Checkpoint:
+        """
+        Return the :class:`~determined.experimental.Checkpoint` for this experiment that
+        has the best validation metric, as defined by the ``sort_by`` and ``smaller_is_better``
+        arguments.
+
+        Arguments:
+            sort_by (string, optional): The name of the validation metric to
+                order checkpoints by. If this parameter is not specified, the metric
+                defined in the experiment configuration ``searcher`` field will be used.
+
+            smaller_is_better (bool, optional): Specifies whether to sort the
+                metric above in ascending or descending order. If ``sort_by`` is unset,
+                this parameter is ignored. By default, the value of ``smaller_is_better``
+                from the experiment's configuration is used.
+        """
+        checkpoints = self.top_n_checkpoints(
+            1, sort_by=sort_by, smaller_is_better=smaller_is_better
+        )
+
+        if not checkpoints:
+            raise AssertionError("No checkpoints found for experiment {}".format(self.id))
+
+        return checkpoints[0]
+
+    def top_n_checkpoints(
+        self,
+        limit: int,
+        sort_by: Optional[str] = None,
+        smaller_is_better: Optional[bool] = None,
+    ) -> List[checkpoint.Checkpoint]:
+        """
+        Return the N :class:`~determined.experimental.Checkpoint` instances with the best
+        validation metrics, as defined by the ``sort_by`` and ``smaller_is_better``
+        arguments. This method will return the best checkpoint from the
+        top N best-performing distinct trials of the experiment. Only checkpoints in
+        a ``COMPLETED`` state with a matching ``COMPLETED`` validation are considered.
+
+        Arguments:
+            limit (int): The maximum number of checkpoints to return.
+
+            sort_by (string, optional): The name of the validation metric to use for
+                sorting checkpoints. If this parameter is unset, the metric defined
+                in the experiment configuration searcher field will be
+                used.
+
+            smaller_is_better (bool, optional): Specifies whether to sort the
+                metric above in ascending or descending order. If ``sort_by`` is unset,
+                this parameter is ignored. By default, the value of ``smaller_is_better``
+                from the experiment's configuration is used.
+        """
+        r = api.get(
+            self._master,
+            "/api/v1/experiments/{}/checkpoints".format(self.id),
+            params={
+                "states": checkpoint.CheckpointState.COMPLETED.value,
+                "validation_states": checkpoint.CheckpointState.COMPLETED.value,
+            },
+        )
+        checkpoints = r.json()["checkpoints"]
+
+        if not checkpoints:
+            raise AssertionError("No checkpoint found for experiment {}".format(self.id))
+
+        if not sort_by:
+            sort_by = checkpoints[0]["experimentConfig"]["searcher"]["metric"]
+            smaller_is_better = checkpoints[0]["experimentConfig"]["searcher"]["smaller_is_better"]
+
+        checkpoints.sort(
+            reverse=not smaller_is_better,
+            key=lambda x: (x["metrics"]["validationMetrics"][sort_by], x["trialId"]),
+        )
+
+        # Ensure returned checkpoints are from distinct trials.
+        t_ids = set()
+        checkpoint_refs = []
+        for ckpt in checkpoints:
+            if ckpt["trialId"] not in t_ids:
+                checkpoint_refs.append(checkpoint.Checkpoint.from_json(ckpt, self._master))
+                t_ids.add(ckpt["trialId"])
+
+        return checkpoint_refs[:limit]
+
+    def __repr__(self) -> str:
+        return "Experiment(id={})".format(self.id)
