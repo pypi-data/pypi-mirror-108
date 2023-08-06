30e57d24ba04b1ef7e73161c7331301650ad791a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:00 +0000

    Add package HashDL/3.0.0/HashDL-3.0.0.tar.gz

---
+++ packages/HashDL/HashDL-3.0.0.tar.gz/HashDL-3.0.0/HashDL/__init__.py
@@ -0,0 +1,6 @@
+from .hashdl import (SGD, Adam,
+                     WTA, DWTA,
+                     ConstantFrequency, ExponentialDecay,
+                     Linear, ReLU, Sigmoid,
+                     ConstantInitializer, GaussInitializer,
+                     Network)
