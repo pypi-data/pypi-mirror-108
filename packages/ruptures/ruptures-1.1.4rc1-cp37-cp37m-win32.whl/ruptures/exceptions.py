5152401884751718dece64c16070da2e61de98e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:19 +0000

    Add package ruptures/1.1.4rc1/ruptures-1.1.4rc1-cp37-cp37m-win32.whl

---
+++ packages/ruptures/ruptures-1.1.4rc1-cp37-cp37m-win32.whl/ruptures/exceptions.py
@@ -0,0 +1,17 @@
+"""The :mod:`ruptures.exceptions` module includes all custom warnings and error
+classes used across ruptures."""
+
+
+class NotEnoughPoints(Exception):
+
+    """Raise this exception when there is not enough point to calculate a cost
+    function."""
+
+    pass
+
+
+class BadSegmentationParameters(Exception):
+    """Raise this exception when a segmentation is not possible given the
+    parameters."""
+
+    pass
