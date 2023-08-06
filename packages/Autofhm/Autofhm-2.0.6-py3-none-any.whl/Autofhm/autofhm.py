133960dd2809e6c32a8b1a442f238590494a80a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:28 +0000

    Add package Autofhm/2.0.6/Autofhm-2.0.6-py3-none-any.whl

---
+++ packages/Autofhm/Autofhm-2.0.6-py3-none-any.whl/Autofhm/autofhm.py
@@ -0,0 +1,253 @@
+import os
+import warnings
+import random
+import pickle
+
+
+import pandas as pd
+import numpy as np
+
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+
+from Autofhm.utils.metrics import r_metrics, c_metrics
+from Autofhm.utils.parse_util import parse_json, parse_xml, parse_yaml
+from Autofhm.feature.features import Features
+from Autofhm.ga.ga import GeneticAlgo
+from Autofhm.utils.console import Console
+
+class Autofhm :
+
+    def __init__(self, 
+                config=None, 
+                random_state=42) :
+        """Initialse the class. 
+
+            `config` : Contains the full path to the configration file
+            `random_State` : A random value to be used as seed(Optional)
+        """
+        self._config = config
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None 
+        self.y_test = None
+        self._model = None
+        self.feature = None
+        self.console = Console()
+
+        self.random_state = random_state
+        self.feature_config, self.model_config, self.training_config = self._handle_config()  
+        self.cv = 5 if 'cv' not in self.training_config else self.training_config['cv']
+        self.n_jobs = -1 if 'n_jobs' not in self.training_config else self.training_config['n_jobs']
+        self.scoring_function = None if 'scoring_function' not in self.training_config else self.training_config['scoring_function']
+        self.classification = True if self.training_config['mode']=='classification' else False
+
+    def _handle_config(self) :
+
+        feature_config = None
+        model_config = None
+        training_config = None
+        config = self._config
+
+        if isinstance(config, dict):
+            feature_config = config['feature']
+            model_config = config['model']
+            training_config = config['training_mode']
+
+        elif isinstance(config, str):
+            FOMRAT = ['yaml', 'xml', 'json']
+            file_format = config.split('.')[-1]
+
+            if file_format not in FOMRAT:
+                raise Exception('The given path is invalid or the file format is not supported')
+
+            index = FOMRAT.index(file_format)
+            
+            if index == 0:
+                config_dict, err = parse_yaml(config)
+                if err is not None:
+                    raise Exception(err.args)
+                feature_config = config_dict['feature']
+                model_config = {} if 'model' not in config_dict else config_dict['model']
+                training_config = config_dict['training_mode']
+
+            elif index == 1:
+                config_dict, err = parse_xml(config)
+                if err is not None:
+                    raise Exception(err.args)
+                feature_config = config_dict['feature']
+                model_config = {} if 'model' not in config_dict else config_dict['model']
+                training_config = config_dict['training_mode']
+
+            elif index == 2:
+                config_dict, err = parse_json(config)
+                if err is not None:
+                    raise Exception(err.args)
+                feature_config = config_dict['feature']
+                model_config = {} if 'model' not in config_dict else config_dict['model']
+                training_config = config_dict['training_mode']
+        
+        return feature_config, model_config, training_config
+
+    def get_features(self) :  
+        """ Perform feature engineering on the given dataset. Make sure the dataset path inside configruation file is valid. Full path is preferred.
+        
+        Feature engineered dataset will be stored inside the only in the model itself.
+        """
+        id = self.feature_config['entity_set']['id']
+        target_entity = self.feature_config['entity_set']['target_entity']
+        primitives = [] if 'primitives' not in  self.feature_config else self.feature_config['primitives']
+        entities = self.feature_config['entities']
+        relationships = [] if 'relationship' not in self.feature_config else self.feature_config['relationship']
+        corr_threshold = 0.9 if 'corr_threshold' not in self.feature_config else self.feature_config['corr_threshold']
+        scale = None if 'scale' not in self.feature_config else self.feature_config['scale']
+        transform = None if 'transform' not in self.feature_config else self.feature_config['transform']
+        test_size = 0.2 if 'test_size' not in self.feature_config else self.feature_config['test_size']
+        
+        self.feature = Features(
+            id,
+            target_entity,
+            primitives=primitives,
+            entities=entities,
+            relationships=relationships,
+            corr_threshold=corr_threshold,
+            scale=scale,
+            transforms=transform,
+            test_size=test_size,
+            n_jobs=self.n_jobs,
+            random_state=self.random_state,
+
+        )
+        
+        self.console.start_pb("Feature Engineering")
+        with warnings.catch_warnings() :
+            warnings.filterwarnings('ignore')
+            self.X_train, self.X_test, self.y_train, self.y_test = self.feature.build()
+            self.console.log("Feature Engineering Complete.")
+        self.console.stop_pb()
+
+    def get_test_data(self, n=10) :
+        """ Returns the test data used to evalute the model. 
+        
+        If get_features() is called before hand the this function reflects the modified dataset. 
+        """
+        return pd.merge(self.X_test, self.y_test, left_index=True, right_index=True).sample(n)
+
+    def _get_optimised_model(self) :
+
+        config_dict = self.model_config if self.model_config else None
+
+        training_config = self.training_config
+
+        gen = 20 if 'gen' not in training_config else training_config['gen']
+        population = 100 if 'population' not in training_config else training_config['population']
+        offspring =population if 'offspring' not in training_config else training_config['offspring']
+        mutation_rate = 0.9 if 'mutation_rate' not in training_config else training_config['mutation_rate']
+        crossover_rate = 0.2 if 'crossover_rate' not in training_config else training_config['crossover_rate']
+
+        if self.classification :
+            model = DecisionTreeClassifier(max_depth=5)
+        else :
+            model = DecisionTreeRegressor(max_depth=5)
+
+        try :
+            model.fit(self.X_train, self.y_train)
+        except :
+            raise ValueError('Input Data is not in a valid form')
+
+        ga = GeneticAlgo(
+            generations=gen, 
+            population_size=population, 
+            offspring_size=offspring, 
+            mutation_rate=mutation_rate,
+            crossover_rate=crossover_rate,
+            cv=self.cv, 
+            n_jobs=self.n_jobs,
+            random_state=self.random_state,
+            config_dict=config_dict,
+            classification=self.classification,
+            scoring_function=self.scoring_function,
+            console=self.console
+        )
+
+        return ga.optimise(self.X_train, self.y_train)
+
+    def fit(self, X_train=None, y_train=None):
+        """ Starts the genetic algorithm to perform model selection and hyperparameter optimisations.
+
+        refer source: Autofhm/ga/config_models.py for available models.
+        """
+        if X_train is not None and y_train is not None :
+            self.X_train = X_train
+            self.y_train = y_train
+        elif self.X_train is None and self.y_train is None :
+            raise ValueError('No data is provided')
+
+        if self.random_state is not None :
+            random.seed(self.random_state)
+            np.random.seed(self.random_state)
+
+        self.console.start_pb("Genetic Algorithm ...")
+        self._model = self._get_optimised_model()
+        self.console.log("Genetic Algorithm Complete.")
+        self.console.stop_pb()
+        self.console.stop_all()
+        self.console.print("\nModel = "+str(self._model))
+
+
+    def predict(self, features):
+        """predict for new values given inside features.
+            `features`: If feature engineered this must also reflect the new dataset. 
+
+            Note: Can useget_test_data() to get the test data
+        """
+        if not self._model:
+            raise RuntimeError('Optimised model not found, please call the fit() first or fit_predict() first')
+
+        features = features.astype(np.float64)
+
+        return self._model.predict(features)
+
+    def test(self, X_test=None, y_test=None):
+        """ test the generated model.
+            X_train: optional
+            Y_train: optional
+         
+         Uses the test portion of the provided dataset if none given.
+        """
+        if self.classification :
+            metrics = c_metrics
+        else :
+            metrics = r_metrics
+        if X_test is not None and y_test is not None :
+            self.X_test = X_test
+            self.y_test = y_test
+        elif self.X_test is None and self.y_test is None :
+            raise ValueError('No data is provided')
+        y_pred = self.predict(self.X_test)
+        scores = {}
+        for scoring_function in metrics: 
+            scorer, _ = metrics[scoring_function]
+            score = scorer(self.y_test, y_pred)
+            scores[scoring_function] = score
+
+            self.console.print(f"{scoring_function:<20} {' = ':^15} {str(score):<10}")
+        return scores
+
+    def save_model(self, filename, path) :
+
+        model_name = filename + ".sav"
+        try :
+            pickle.dump(self._model, open(os.path.join(path, model_name), 'wb'))
+        except Exception as e: 
+            raise Exception(e)
+
+        self.console.print(f"Model saved to the folder {path} with name {model_name}")
+
+    def load_model(self, path) :
+
+        try :
+            model = pickle.load(open(path, 'rb'))
+        except  Exception as e :
+            raise Exception(e)
+        self._model = model
+        self.console.print('Done!')
---
+++ packages/Autofhm/Autofhm-2.0.6-py3-none-any.whl/Autofhm/ga/ga.py
@@ -0,0 +1,262 @@
+import warnings
+import random
+from functools import partial
+from inspect import isclass
+from multiprocessing import cpu_count
+
+from joblib import Parallel, delayed
+
+import deap
+import numpy as np
+import pandas as pd
+from deap import gp, tools, creator, base
+from copy import copy
+
+
+from .config_model import config_classifier, config_regressor
+from .ga_operators import cxOnePoint, mutIndividual, eaMuPlusLambda
+from Autofhm.utils.utils import cv_score, Output_Array, pareto_eq, findOperatorClass, expr_to_tree, generate_model_code
+
+
+class GeneticAlgo :
+
+    def __init__(self,generations=20, population_size=50, offspring_size=None,
+                 mutation_rate=0.8, crossover_rate=0.2,
+                 cv=5, n_jobs=-1,random_state=None, 
+                 config_dict=None, classification=True, scoring_function=None, console=None) :
+        
+        self._pareto_front = None
+        self._optimized_model = None
+        self._fitted_model = None    
+        self._pop = None
+
+        self.population_size = population_size
+        self.generations = generations
+        self.offspring_size = population_size if offspring_size is None else offspring_size
+        self.classification = classification   
+        if config_dict is None: 
+            self.config_dict = config_classifier if self.classification else config_regressor
+        else :
+            self.config_dict = config_dict
+
+        self.mutpb = mutation_rate
+        self.cxpb = crossover_rate
+        self.random_state = random_state
+        if scoring_function is not None :
+            self.scoring_function = scoring_function
+        else :
+            if self.classification :
+                self.scoring_function = 'accuracy'
+            else :
+                self.scoring_function = 'explained_variance'
+        self.cv = cv
+        if n_jobs == -1:
+            self.n_jobs = cpu_count()
+        elif n_jobs > cpu_count():
+            print(f"n_jobs given is more than the number of cores available, settinr n_jobs to {cpu_count()}")
+            self.n_jobs = cpu_count()
+        else :
+            self.n_jobs = n_jobs
+        self.console = console
+
+        self.operators = []
+        self.arguments = []
+        for key in sorted(self.config_dict.keys()):
+            op_class, arg_types = findOperatorClass(key, self.config_dict[key])
+            if op_class:
+                self.operators.append(op_class)
+                self.arguments += arg_types
+
+        self._evaluated_individuals = {}
+        self.operators_context = {
+            'copy': copy
+        }
+        self._setup_pset()
+        self._setup_toolbox()
+        
+    def _setup_pset(self): 
+        
+        if self.random_state is not None:
+            random.seed(self.random_state)
+            np.random.seed(self.random_state)
+        self._pset = gp.PrimitiveSetTyped('MAIN', [np.ndarray], Output_Array)
+        self._pset.renameArguments(ARG0='input_matrix')
+
+        for op in self.operators:
+
+            if op.root:
+                p_types = (op.parameter_types()[0], Output_Array)
+                self._pset.addPrimitive(op, *p_types)
+
+            self._pset.addPrimitive(op, *op.parameter_types())
+            for key in sorted(op.import_hash.keys()):
+                module_list = op.import_hash[key][0]
+
+                exec('from {} import {}'.format(key, module_list))
+                for var in op.import_hash[key]:
+                    self.operators_context[var] = eval(var)
+
+               
+        for _type in self.arguments:
+
+            for val in list(_type.values):
+                terminal_name = _type.__name__ + "=" + str(val)
+                self._pset.addTerminal(val, _type, name=terminal_name)
+
+    def _setup_toolbox(self):
+        creator.create('FitnessMulti', base.Fitness, weights=(-1.0, 1.0))
+        creator.create('Individual', gp.PrimitiveTree, fitness=creator.FitnessMulti, statistics=dict)
+
+        self._toolbox = base.Toolbox()
+        self._toolbox.register('expr', self._gen_grow, pset=self._pset, min_=1, max_=3)
+        self._toolbox.register('individual', tools.initIterate, creator.Individual, self._toolbox.expr)
+        self._toolbox.register('population', tools.initRepeat, list, self._toolbox.individual)
+        self._toolbox.register('compile', self._compile_to_sklearn)
+        self._toolbox.register('select', tools.selBest)
+        self._toolbox.register('mate', self._mate_operator)
+        self._toolbox.register('expr_mut', self._gen_grow, min_=1, max_=1)
+        self._toolbox.register('mutate', self._random_mutation_operator)
+
+    def optimise(self, features, classes):
+
+        features = features.astype(np.float64)
+
+        if self.random_state is not None:
+            random.seed(self.random_state)
+            np.random.seed(self.random_state)
+
+        self._toolbox.register('evaluate', self._evaluate_individuals, features=features, classes=classes)
+
+        pop = self._toolbox.population(n=self.population_size)
+
+        self._pareto_front = tools.ParetoFront(similar=pareto_eq)
+
+        try:
+            pop = eaMuPlusLambda(population=pop, toolbox=self._toolbox,
+                mu=self.population_size, lambda_=self.offspring_size,
+                cxpb=self.cxpb, mutpb=self.mutpb,
+                ngen=self.generations, console=self.console, halloffame=self._pareto_front)
+
+        except (KeyboardInterrupt, SystemExit):
+            self.console.log('Keyboard interrupt!. Will use the best model so far')
+
+        finally:
+            if self._pareto_front:
+                top_score = -float('inf')
+                for model, model_scores in zip(self._pareto_front.items, reversed(self._pareto_front.keys)):
+                    if model_scores.wvalues[1] > top_score:
+                        self._optimized_model = model
+                        top_score = model_scores.wvalues[1]
+
+                if not self._optimized_model:
+                    self.console.log('No model is optimized. Please re run the program after checking the config')
+                    return None
+                else:
+                    self._fitted_model = self._toolbox.compile(expr=self._optimized_model)
+
+                    self._fitted_model.fit(features, classes)
+                    return self._fitted_model
+            
+    
+    def _mate_operator(self, ind1, ind2):
+        return cxOnePoint(ind1, ind2)
+
+    def _random_mutation_operator(self, individual):
+        mutation_technique = partial(mutIndividual, pset=self._pset)
+        return mutation_technique(individual)
+
+    def _gen_grow(self, pset, min_, max_, type_=None):
+        return self._generate(pset, type_)
+
+    def _generate(self, pset, type_=None):
+        if type_ is None:
+            type_ = pset.ret
+        expr = []
+        try:
+            prim = np.random.choice(pset.primitives[type_])
+        except IndexError:
+            raise IndexError("Some error occured while adding primitives") 
+        expr.append(prim)
+        for arg in prim.args:
+            try:
+                term = np.random.choice(pset.terminals[arg])
+            except IndexError:
+                raise IndexError("Some error occured while adding terminal primitives")
+            if isclass(term):
+                term = term()
+            expr.append(term)
+        return expr
+
+    def _evaluate_individuals(self, individuals, features, classes, sample_weight = None):
+        
+        fitnesses_dict = {}
+        eval_individuals_str = []
+        sklearn_model_list = []
+        operator_count_list = []
+        test_idx_list = []
+        for indidx, individual in enumerate(individuals):
+            individual = individuals[indidx]
+            individual_str = str(individual)
+            if individual_str in self._evaluated_individuals:
+                fitnesses_dict[indidx] = self._evaluated_individuals[individual_str]
+       
+            else:
+                try:
+                    sklearn_model = self._toolbox.compile(expr=individual)
+                    operator_count = 0
+                    for i in range(len(individual)):
+                        node = individual[i]
+                        if ((type(node) is deap.gp.Terminal) or
+                             type(node) is deap.gp.Primitive ):
+                            continue
+                        operator_count += 1
+                except Exception:
+                    fitnesses_dict[indidx] = (5000., -float('inf'))
+                    continue
+                eval_individuals_str.append(individual_str)
+                operator_count_list.append(operator_count)
+                sklearn_model_list.append(sklearn_model)
+                test_idx_list.append(indidx)
+
+        resulting_score_list = []
+        for chunk_idx in range(0, len(sklearn_model_list),self.n_jobs*4):
+            parallel = Parallel(n_jobs=self.n_jobs, verbose=0, pre_dispatch='2*n_jobs')
+            tmp_result_score = parallel(delayed(cv_score)(sklearn_model, features, classes,
+                                        self.cv, self.scoring_function, self.random_state)
+                                        for sklearn_model in sklearn_model_list[chunk_idx:chunk_idx+self.n_jobs*4])
+            for val in tmp_result_score:
+                if val == 'Timeout':
+                    resulting_score_list.append(-float('inf'))
+                else:
+                    resulting_score_list.append(val)
+
+        for resulting_score, operator_count, individual_str, test_idx in zip(resulting_score_list, operator_count_list, eval_individuals_str, test_idx_list):
+            if type(resulting_score) in [float, np.float64, np.float32]:
+                self._evaluated_individuals[individual_str] = (max(1, operator_count), resulting_score)
+                fitnesses_dict[test_idx] = self._evaluated_individuals[individual_str]
+            else:
+                raise ValueError('Scoring function does not return a float.')
+
+        fitnesses_ordered = []
+        for key in sorted(fitnesses_dict.keys()):
+            fitnesses_ordered.append(fitnesses_dict[key])
+        return fitnesses_ordered
+
+    def _set_param_recursive(self, model_steps, parameter, value):
+
+        for (_, obj) in model_steps:
+            recursive_attrs = ['steps', 'transformer_list', 'estimators']
+            for attr in recursive_attrs:
+                if hasattr(obj, attr):
+                    self._set_param_recursive(getattr(obj, attr), parameter, value)
+            if hasattr(obj, 'estimator'):  
+                est = getattr(obj, 'estimator')
+                if hasattr(est, parameter):
+                    setattr(est, parameter, value)
+            if hasattr(obj, parameter):
+                setattr(obj, parameter, value)
+
+    def _compile_to_sklearn(self, expr):
+        sklearn_model_str = generate_model_code(expr_to_tree(expr, self._pset), self.operators)
+        model = eval(sklearn_model_str, self.operators_context)
+        return model
