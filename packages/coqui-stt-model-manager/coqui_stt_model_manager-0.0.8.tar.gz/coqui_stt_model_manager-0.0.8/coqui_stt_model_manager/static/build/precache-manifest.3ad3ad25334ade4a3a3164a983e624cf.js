b205e3baa38b755d0e1dd9a2d473bf23d0635dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package coqui-stt-model-manager/0.0.8/coqui_stt_model_manager-0.0.8.tar.gz

---
+++ packages/coqui-stt-model-manager/coqui_stt_model_manager-0.0.8.tar.gz/coqui_stt_model_manager-0.0.8/coqui_stt_model_manager/static/build/precache-manifest.3ad3ad25334ade4a3a3164a983e624cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c77b55d79befd2813a3e285d651bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39c6063d3e49cb724bdd",
+    "url": "/static/css/bundle.min.css"
+  },
+  {
+    "revision": "39c6063d3e49cb724bdd",
+    "url": "/static/js/bundle.min.js"
+  },
+  {
+    "revision": "799de9f25b80ded465f02f885354882e",
+    "url": "/static/js/bundle.min.js.LICENSE.txt"
+  }
+]);
