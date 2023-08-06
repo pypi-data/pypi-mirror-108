9c83f88f5f24e23d00c7b91bb32760cdefa446ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:38 +0000

    Add package firstimpression/0.2.28/firstimpression-0.2.28.tar.gz

---
+++ packages/firstimpression/firstimpression-0.2.28.tar.gz/firstimpression-0.2.28/firstimpression/__init__.py
@@ -0,0 +1,11 @@
+from .file import *
+from .html import *
+from .nu_nl import *
+from .rss import *
+from .text import *
+from .bcc import *
+from .scalascript import *
+from tempfile import gettempdir
+
+tempfolder = gettempdir()
+localintegratedfolder = "C:/Users/Public/Documents/Scala/LocalIntegratedContent"
