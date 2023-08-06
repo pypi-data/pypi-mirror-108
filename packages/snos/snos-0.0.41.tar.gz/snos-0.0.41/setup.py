865373429351b9ce48c2634f90d90936aa63eb85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:36 +0000

    Add package snos/0.0.41/snos-0.0.41.tar.gz

---
+++ packages/snos/snos-0.0.41.tar.gz/snos-0.0.41/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='snos',
+      description='Command line tool for keeping notes simple way.',
+      long_description='Command line tool for keeping notes simple way.',
+      version='0.0.41',
+      url='https://github.com/ferdielik/snos',
+      author='Ferdi Elik',
+      author_email='elik.ferdi@gmail.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3'
+      ],
+      scripts=['snos/snos'],
+      packages=find_namespace_packages(include=['snos.*'])
+      )
