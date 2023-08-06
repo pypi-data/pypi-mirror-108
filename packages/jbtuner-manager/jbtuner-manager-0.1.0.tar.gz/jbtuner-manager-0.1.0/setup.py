6f0245d32ea8f91d4d661b5e395f8f3bc441dcc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:02 +0000

    Add package jbtuner-manager/0.1.0/jbtuner-manager-0.1.0.tar.gz

---
+++ packages/jbtuner-manager/jbtuner-manager-0.1.0.tar.gz/jbtuner-manager-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jbtuner_manager']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['adafruit-ampy>=1.1.0,<2.0.0', 'typer[all]>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['jbtman = jbtuner_manager.main:app']}
+
+setup_kwargs = {
+    'name': 'jbtuner-manager',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Ying Shaodong',
+    'author_email': 'gavin.ying@chordx.co',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
