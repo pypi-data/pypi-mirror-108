4e2f679c84749c8834c2f04739e1b9e4e1175d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:27 +0000

    Add package pyign/1.8.2/pyign-1.8.2-py3-none-any.whl

---
+++ packages/pyign/pyign-1.8.2-py3-none-any.whl/pyign/manifest/_engine_config_.py
@@ -0,0 +1,121 @@
+###### TIME AUTOMATION ########
+
+##### Hot Fire Press Window Time ######
+INTER_PRESS_HOTFIRE = 2.0
+PRE_PRESS_HOTFIRE = 3.0 
+BURN_PRESS_HOTFIRE = 6.0
+#######################################
+
+
+
+
+######### Valve Turn Timing ###########
+FUEL_STAY_TIME = 0.20 
+FUEL_MAIN_DT = 0.67
+OX_STAY_TIME = 0.00 
+OX_MAIN_DT = 0.64
+#######################################
+
+
+
+
+######### Hotfire Timing #############
+START_IGNITER_HOTFIRE = 2.0 
+START_PURGE_HOTFIRE = 0.5 
+IGNITER_HOTFIRE = 0.5 
+
+FUEL_ISO_HOTFIRE = 5.15 
+FUEL_PRESS_HOTFIRE = 5.15 
+FUEL_VENT_HOTFIRE = 5.25 
+FUEL_MAIN_HOTFIRE = 5.0 
+
+FUEL_PURGE_HOTFIRE = 10.5 
+
+OX_ISO_HOTFIRE = 5.15 
+OX_PRESS_HOTFIRE = 5.15 
+OX_VENT_HOTFIRE = 5.25 
+OX_CHILL_HOTFIRE = 5.25 
+OX_MAIN_HOTFIRE = 5.0 
+
+T_MINUS_HOTFIRE = 15.0 
+T_PLUS_HOTFIRE = 18.0 
+#######################################
+
+
+
+
+
+
+########### Fuel Cold Flow ############
+START_FUEL_PURGE_COLDFLOW = 0.5 
+FUEL_ISO_COLDFLOW = 4.0 
+FUEL_PRESS_COLDFLOW = 4.25 
+FUEL_VENT_COLDFLOW = 4.5
+FUEL_MAIN_COLDFLOW = 5.0 
+FUEL_PURGE_COLDFLOW = 6.0 
+T_MINUS_FUEL_COLDFLOW = 10.0
+
+### Cryo Cold Flow ###
+CRYO_ISO_COLDFLOW = 4.0 
+CRYO_PRESS_COLDFLOW = 4.25 
+CRYO_VENT_COLDFLOW = 4.5 
+CRYO_CHILL_COLDFLOW = 5.25 
+CRYO_MAIN_COLDFLOW = 5.0 
+T_MINUS_CRYO_COLDFLOW = 10.0
+
+### Cryo Cold Flow Press Time ###
+INTER_PRESS_COLDFLOW = 2.0
+PRE_PRESS_COLDFLOW = 3.0
+BURN_PRESS_COLDFLOW = 6.0
+#######################################
+
+
+
+########## Igniter Checkout ###########
+IGN_TEST_TOTAL = 10.0
+IGN_TEST_ONE = 0.0
+IGN_TEST_TWO = 5.0
+#######################################
+
+
+
+######## 1 Second Purge Timing ########
+PURGE1 = 1.0
+
+### 2 Second Purge Timing ###
+PURGE2 = 2.0
+#######################################
+
+
+
+
+###### Ox Vent & Fuel Vent Timing #####
+VENT = 2.5
+#######################################
+
+
+
+######### System Cycle Timing #########
+CYCLE_ONE = 2.0
+CYCLE_TWO = 4.0
+CYCLE_THREE = 6.0
+CYCLE_FOUR = 8.0
+CYCLE_FIVE = 10.0
+CYCLE_SIX = 12.0
+CYCLE_SEVEN = 14.0
+CYCLE_EIGHT = 16.0
+CYCLCE_NINE = 18.0
+#######################################
+
+
+
+
+####### Valve Firing Sim Timing #######
+CYCLE_ONE_OPEN = 2.0
+CYCLE_ONE_CLOSE = 4.0
+CYCLE_TWO_OPEN = 8.0
+CYCLE_TWO_CLOSE = 10.0
+CYCLE_THREE_OPEN = 14.0
+CYCLE_THREE_CLOSE = 16.0
+CYCLE_COMPLETE = 18.0
+#######################################
