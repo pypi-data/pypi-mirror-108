3eae2fd4825e2d4fe21cd5bd86db42e66f8587f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:49 +0000

    Add package memorywrap/1.0.0/memorywrap-1.0.0.tar.gz

---
+++ packages/memorywrap/memorywrap-1.0.0.tar.gz/memorywrap-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="memorywrap",
+    version="1.0.0",
+    author="La Rosa Biagio",
+    author_email="larosa@diag.uniroma1.it",
+    description="Memory Wrap: an extension for image classification models",
+    install_requires=[
+          'entmax',
+          'torch>1.5'
+      ],
+    #long_description_content_type="text/markdown",
+    #url="https://github.com/username/demopackage",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
