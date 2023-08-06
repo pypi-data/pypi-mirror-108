a809bc4973ea9e5b0205e60a5934c75597d4bb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:13 +0000

    Add package welleng/0.2.19/welleng-0.2.19.tar.gz

---
+++ packages/welleng/welleng-0.2.19.tar.gz/welleng-0.2.19/welleng/exchange/csv.py
@@ -0,0 +1,76 @@
+import numpy as np
+from welleng.version import __version__
+
+
+def export_csv(survey, filename, rtol=0.1, atol=0.1):
+    """
+    Function to export a minimalist (only the control points - i.e. the begining
+    and end points of hold and/or turn sections) survey to input into third
+    party trajectory planning software.
+
+    Parameters
+    ----------
+    survey: welleng.survey.Survey object
+    filename: str
+        The path and filename for saving the text file.
+    """
+    sections = survey._get_sections(rtol=rtol, atol=atol)
+
+    data = [[
+        tp.md,
+        tp.inc,
+        tp.azi,
+        tp.location[1],
+        tp.location[0],
+        tp.location[2],
+        tp.dls,
+        tp.toolface,
+        tp.build_rate,
+        tp.turn_rate,
+    ] for tp in sections]
+
+    data = np.vstack(data[1:])
+
+    headers = ','.join([
+        'MD',
+        'INC (deg)',
+        'AZI (deg)',
+        'NORTHING (m)',
+        'EASTING (m)',
+        'TVDSS (m)',
+        'DLS',
+        'TOOLFACE',
+        'BUILD RATE',
+        'TURN RATE'
+    ])
+
+    if filename is None:
+        try:
+            import pandas as pd
+
+            df = pd.DataFrame(
+                data,
+                columns=headers.split(',')
+            )
+            return df
+        except ImportError:
+            print("Missing pandas dependency")
+
+    comments = [
+        f"welleng, version: {__version__}\n"
+        f"author, Jonny Corcutt\n"
+    ]
+    comments.extend([
+        f"{k}, {v}\n" for k, v in vars(survey.header).items()
+    ])
+    comments += f"\n"
+    comments = ''.join(comments)
+
+    np.savetxt(
+        filename,
+        data,
+        delimiter=',',
+        fmt='%.3f',
+        header=headers,
+        comments=comments
+    )
