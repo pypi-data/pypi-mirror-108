d9c4d287715b99152d443d1f7e4b58370dd9cca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:53 +0000

    Add package lan-echarts/0.0.6/lan_echarts-0.0.6.tar.gz

---
+++ packages/lan-echarts/lan_echarts-0.0.6.tar.gz/lan_echarts-0.0.6/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="lan_echarts",
+    version="0.0.6",
+    keywords=["pip", "echarts"],
+    description="python echarts",
+    long_description="python echarts",
+    license="MIT Licence",
+
+    url="https://github.com/",
+    author="lan_echarts",
+    author_email="shiwolang@live.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["tornado", "requests"]
+)
