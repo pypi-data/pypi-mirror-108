9d8635953405ab1bed9593d7333b8c185055dafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:00 +0000

    Add package PyPipackaging/0.0.6/PyPipackaging-0.0.6.tar.gz

---
+++ packages/PyPipackaging/PyPipackaging-0.0.6.tar.gz/PyPipackaging-0.0.6/src/hellototi.py
@@ -0,0 +1,14 @@
+#def say_hello():
+ #   print('Hello World')
+    
+#name = input('What is your name?\n')
+#print ('Hi, %s.' % name)
+
+def Funsum(x,y): # this is only here to test the package upload
+    return x+y
+
+def Funminus(x,y): # this is only here to test the package upload
+    return x-y
+
+def Funmultiply(x,y): # this is only here to test the package upload
+    return x*y
