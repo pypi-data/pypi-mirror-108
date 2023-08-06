10a0e09e66f1402df26df39c252348b5692a1b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:56 +0000

    Add package dell-warranty-api/1.10.0/dell_warranty_api-1.10.0-py3-none-any.whl

---
+++ packages/dell-warranty-api/dell_warranty_api-1.10.0-py3-none-any.whl/dell_api/__init__.py
@@ -0,0 +1,5 @@
+"""
+A command line interface (CLI) for fetch Dell APIs (TechDirect asset warranty service) data and represent them in human friendly view.
+"""
+# Version of the dell_api package
+__version__ = "1.2.0"
