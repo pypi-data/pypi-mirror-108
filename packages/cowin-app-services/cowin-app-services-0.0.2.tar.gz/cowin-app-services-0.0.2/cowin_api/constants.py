c7c403f5b2cd4180fa769e8840e0ea6f2971b719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:07 +0000

    Add package cowin-app-services/0.0.2/cowin-app-services-0.0.2.tar.gz

---
+++ packages/cowin-app-services/cowin-app-services-0.0.2.tar.gz/cowin-app-services-0.0.2/cowin_api/constants.py
@@ -0,0 +1,13 @@
+class Constants:
+    base_url = "https://cdn-api.co-vin.in/api/v2"
+
+    states_list_url = f"{base_url}/admin/location/states"
+    districts_list_url = f"{base_url}/admin/location/districts"
+
+    availability_by_pin_code_url = f"{base_url}/appointment/sessions/public/calendarByPin"
+    availability_by_district_url = f"{base_url}/appointment/sessions/public/calendarByDistrict"
+
+    DD_MM_YYYY = "%d-%m-%Y"
+
+    total_retries = 3
+    backoff_factor = 1
---
+++ packages/cowin-app-services/cowin-app-services-0.0.2.tar.gz/cowin-app-services-0.0.2/setup.py
@@ -0,0 +1,41 @@
+import pathlib
+
+from setuptools import setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='cowin-app-services',
+    version='0.0.2',
+    description='Python Library for CoWin API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=' ',
+    author='Taranvir Singh Saini',
+    author_email='taranvir.554@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Environment :: Console',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='cowin, covid, vaccine',
+    packages=['cowin_api'],
+    include_package_data=True,
+    python_requires='>=3.6, <4',
+    install_requires=[
+        'fake-useragent==0.1.11',
+        'pytest==6.2.3',
+        'requests==2.25.1'
+    ]
+)
