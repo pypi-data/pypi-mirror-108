a3a19dd400e645cc920a1bad33a56d2501e18031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:23 +0000

    Add package cbmshell/1.0/cbmshell-1.0.tar.gz

---
+++ packages/cbmshell/cbmshell-1.0.tar.gz/cbmshell-1.0/cbm_shell/image_path.py
@@ -0,0 +1,23 @@
+import string
+
+
+class Drive(int):
+    pass
+
+
+class ImagePath:
+    @staticmethod
+    def is_image_path(path):
+        return len(path) > 1 and path[0] in string.digits and path[1] == ':'
+
+    @staticmethod
+    def split(drive_name, encoding):
+        parts = drive_name.split(':', 1)
+        if '~' in parts[1]:
+            pos = parts[1].rindex('~')
+            suffix = parts[1][pos:].encode()
+            return int(parts[0]), parts[1][:pos].encode(encoding)+suffix
+        return int(parts[0]), parts[1].encode(encoding)
+
+    def __repr__(self):
+        return "ImagePath({}:{})".format(self.drive, self.encoded_name)
