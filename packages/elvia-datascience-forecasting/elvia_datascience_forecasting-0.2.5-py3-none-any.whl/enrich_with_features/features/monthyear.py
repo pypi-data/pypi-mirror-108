5d58c67fba26ee1744c932bd5c36da333a6d081e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:34 +0000

    Add package elvia-datascience-forecasting/0.2.5/elvia_datascience_forecasting-0.2.5-py3-none-any.whl

---
+++ packages/elvia-datascience-forecasting/elvia_datascience_forecasting-0.2.5-py3-none-any.whl/enrich_with_features/features/monthyear.py
@@ -0,0 +1,25 @@
+import time
+import numpy as np
+import pandas as pd
+
+
+def monthyear(df: pd.DataFrame, substation_id: str = 'trafo') -> pd.DataFrame:
+    """
+     This function adds day of week as a feature to the dataframe  and 
+     returns just the new feature and substation_id as dataframe
+
+     # Parameters
+     --------------
+     df: Dataframe with datetime index
+     substation_id: refers to column name that keeps substation IDs
+
+     # Returns
+     --------------
+     Pandas dataframe with substation_id and monthyear features
+    
+     """
+    # Create months of of a year as a new feature (for example: 1-Januar, 12-December)
+    df['monthyear'] = df.index.month
+    df['monthyear'] = df['monthyear'].astype(np.float32)
+
+    return df
