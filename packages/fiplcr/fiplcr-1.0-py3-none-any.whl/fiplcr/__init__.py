4f74c2c63db147a16d18b90725f966ff238e936b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:34 +0000

    Add package fiplcr/1.0/fiplcr-1.0-py3-none-any.whl

---
+++ packages/fiplcr/fiplcr-1.0-py3-none-any.whl/fiplcr/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+'''Functions to make UV spectroscopy data analysis for the Solar Corona
+focusing on FIP bias determination'''
+
+from .fip_map import fip_map
+from .specline import *
+from .linear_combination import *
+
+
