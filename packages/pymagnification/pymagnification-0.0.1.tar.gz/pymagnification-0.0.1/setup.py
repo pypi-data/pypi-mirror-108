13f26adbdb1e6e919492441cc41884314734d83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:12 +0000

    Add package pymagnification/0.0.1/pymagnification-0.0.1.tar.gz

---
+++ packages/pymagnification/pymagnification-0.0.1.tar.gz/pymagnification-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='pymagnification',
+    packages=['pymagnification'],
+    version='0.0.1',
+    license='MIT',
+    description='python wrapper to the windows magnification api',
+    author='Aviv Ostrovsky',
+    author_email='ostrovsky.aviv@gmail.com',
+    url='https://github.com/avivost/pymagnification',
+    download_url='https://github.com/avivost/pymagnification/archive/refs/tags/v0.0.1.zip',
+    keywords=['winuse32', 'pywin32', 'magnification', 'pymagnification'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
