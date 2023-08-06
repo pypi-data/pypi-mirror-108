0c488b61a2f9b323b763def833517cb166c6924a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:11 +0000

    Add package vm-booking-app/0.1.64/vm_booking_app-0.1.64-py2.py3-none-any.whl

---
+++ packages/vm-booking-app/vm_booking_app-0.1.64-py2.py3-none-any.whl/migrations/apply.py
@@ -0,0 +1,27 @@
+# Python script that will apply the migrations up to head
+import alembic.config
+import os
+
+from flask import current_app
+from vm_booking_api import create_app, db
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+try:
+    app = current_app._get_current_object()
+except RuntimeError:
+    app = create_app()
+
+alembic_args = ["-c", os.path.join(here, "alembic.ini"), "upgrade", "head"]
+target_metadata = db.metadata
+
+
+def main():
+    with app.app_context():
+        alembic.config.main(argv=alembic_args)
+
+
+def run():
+    alembic.config.main(argv=alembic_args)
