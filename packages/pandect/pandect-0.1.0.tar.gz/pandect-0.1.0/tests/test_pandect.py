41d4fbe6a1a201521cd57e37698e16a808bd03a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:41 +0000

    Add package pandect/0.1.0/pandect-0.1.0.tar.gz

---
+++ packages/pandect/pandect-0.1.0.tar.gz/pandect-0.1.0/tests/test_pandect.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `pandect` package."""
+
+
+import unittest
+
+from pandect import pandect
+
+
+class TestPandect(unittest.TestCase):
+    """Tests for `pandect` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
