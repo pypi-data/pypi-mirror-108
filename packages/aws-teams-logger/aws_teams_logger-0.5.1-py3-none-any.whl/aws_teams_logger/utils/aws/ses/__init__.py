1f502d4b9260005fdd5cced3249e21b6a5c49b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:00 +0000

    Add package aws-teams-logger/0.5.1/aws_teams_logger-0.5.1-py3-none-any.whl

---
+++ packages/aws-teams-logger/aws_teams_logger-0.5.1-py3-none-any.whl/aws_teams_logger/utils/aws/ses/__init__.py
@@ -0,0 +1,12 @@
+"""
+High-level implementation of the boto3 library for SES
+
+Read the Docs here:
+    - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ses.html
+
+"""
+
+__all__ = ['SESHelper']
+
+
+from .ses import SESHelper
