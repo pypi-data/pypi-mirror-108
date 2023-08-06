499e7bd3851020cdda751a3c17279e88258b2a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:54 +0000

    Add package vm-booking-app/0.1.61/vm-booking-app-0.1.61.tar.gz

---
+++ packages/vm-booking-app/vm-booking-app-0.1.61.tar.gz/vm-booking-app-0.1.61/migrations/apply.py
@@ -0,0 +1,23 @@
+# Python script that will apply the migrations up to head
+import alembic.config
+import os
+
+from flask import current_app
+from vm_booking_api import create_app, db
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+alembic_args = ["-c", os.path.join(here, "alembic.ini"), "upgrade", "head"]
+
+try:
+    app = current_app._get_current_object()
+except RuntimeError:
+    app = create_app()
+
+alembic.config.set_main_option("sqlalchemy.url", app.config["SQLALCHEMY_DATABASE_URI"])
+target_metadata = db.metadata
+
+
+def main():
+    with app.app_context():
+        alembic.config.main(argv=alembic_args)
