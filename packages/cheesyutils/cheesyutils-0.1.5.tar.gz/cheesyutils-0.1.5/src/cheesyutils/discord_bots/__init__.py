72d2672967e94f59a36462dce8dd489cb76ae12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:44 +0000

    Add package cheesyutils/0.1.5/cheesyutils-0.1.5.tar.gz

---
+++ packages/cheesyutils/cheesyutils-0.1.5.tar.gz/cheesyutils-0.1.5/src/cheesyutils/discord_bots/__init__.py
@@ -0,0 +1,5 @@
+from .bot import DiscordBot
+from .utils import chunkify_string, get_discord_color, get_image_url, paginate, truncate
+from .paginator import Paginator
+from .constants import *
+from .embed import *
