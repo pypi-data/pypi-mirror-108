869ce78aa7e7feef449a04580767b7349b5c7b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:19 +0000

    Add package wikinet/0.1.6/wikinet-0.1.6-py3-none-any.whl

---
+++ packages/wikinet/wikinet-0.1.6-py3-none-any.whl/wikinet/__init__.py
@@ -0,0 +1,15 @@
+"""
+wikinet
+=======
+
+wikinet is a Python package for the creation, manipulation, and study of networks of Wikipedia articles.
+
+See https:// for complete documentation.
+"""
+
+from .dump import *
+from .corpus import *
+from .graph_container import *
+from .persistent_homology import *
+from .model import *
+from .net import *
