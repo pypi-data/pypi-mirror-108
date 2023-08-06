0bbb0da04f08d3e7a7809e33cb054da6094f53fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:44 +0000

    Add package oeis-seq/0.1.2/oeis-seq-0.1.2.tar.gz

---
+++ packages/oeis-seq/oeis-seq-0.1.2.tar.gz/oeis-seq-0.1.2/oeis/leonardo.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from typing import Iterable
+from oeis.registry import registry
+
+
+@registry.register("A001595")
+def leonardo() -> Iterable[int]:
+    yield 1  # leonardo(0)
+    yield 1  # leonardo(1)
+    prev: int = 1
+    curr: int = 1
+    while True:
+        prev, curr = curr, curr + prev + 1
+        yield curr
---
+++ packages/oeis-seq/oeis-seq-0.1.2.tar.gz/oeis-seq-0.1.2/oeis/totient.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from itertools import count
+from typing import Iterable, List
+
+from oeis.registry import registry
+
+from .utils import coprime
+
+
+@registry.register("A000010")
+def totient() -> Iterable[int]:
+    yield 1  # totient(0)
+    for n in count(start=2):
+        totients: List[bool] = [coprime(n, p) for p in range(1, n)]
+        yield totients.count(True)
