114570b1820adfb2418ce597d3b2df11322507aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:46 +0000

    Add package django-logger-panel/0.1.5/django_logger_panel-0.1.5-py2.py3-none-any.whl

---
+++ packages/django-logger-panel/django_logger_panel-0.1.5-py2.py3-none-any.whl/django_logger_panel/__init__.py
@@ -0,0 +1,19 @@
+import re
+import logging
+from django.conf import settings
+
+LOGGER = logging.getLogger(__name__)
+
+_DEFAULT_BASE_URL = "/loggers/"
+
+if hasattr(settings, "LOGGER_PANEL_BASE_URL"):
+    BASE_URL = settings.LOGGER_PANEL_BASE_URL
+    if BASE_URL[-1] != "/":
+        BASE_URL = f"{BASE_URL}/"
+    LOGGER.debug("Using settings.LOGGER_PANEL_BASE_URL='%s' as BASE_URL", BASE_URL)
+else:
+    BASE_URL = _DEFAULT_BASE_URL
+    LOGGER.debug("Using default value '%s' as BASE_URL", BASE_URL)
+
+# Used by urls.py
+BASE_URL_WITHOUT_LEADING_SLASH = re.sub("^/", "", BASE_URL)
