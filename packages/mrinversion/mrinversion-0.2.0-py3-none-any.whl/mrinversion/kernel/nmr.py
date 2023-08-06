eb96982a5cbf83d73a05214e452f68095bff16d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:29 +0000

    Add package mrinversion/0.2.0/mrinversion-0.2.0-py3-none-any.whl

---
+++ packages/mrinversion/mrinversion-0.2.0-py3-none-any.whl/mrinversion/kernel/nmr.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from mrinversion.kernel.csa_aniso import MAF  # NOQA
+from mrinversion.kernel.csa_aniso import ShieldingPALineshape  # NOQA
+from mrinversion.kernel.csa_aniso import SpinningSidebands  # NOQA
+
+# from mrinversion.kernel.quad_aniso import MQMAS  # NOQA
+
+# from mrinversion.kernel.lineshape import DAS  # NOQA
