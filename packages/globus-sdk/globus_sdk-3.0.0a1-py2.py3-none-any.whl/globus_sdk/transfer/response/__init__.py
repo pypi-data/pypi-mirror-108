fa79e370417e76649a20fc0d6e1ed9da96396df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:26 +0000

    Add package globus-sdk/3.0.0a1/globus_sdk-3.0.0a1-py2.py3-none-any.whl

---
+++ packages/globus-sdk/globus_sdk-3.0.0a1-py2.py3-none-any.whl/globus_sdk/transfer/response/__init__.py
@@ -0,0 +1,4 @@
+from globus_sdk.transfer.response.activation import ActivationRequirementsResponse
+from globus_sdk.transfer.response.iterable import IterableTransferResponse
+
+__all__ = ["IterableTransferResponse", "ActivationRequirementsResponse"]
