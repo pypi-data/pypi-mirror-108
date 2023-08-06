75cdf53be50f797ffa4d2bbdf1aaf9de67610726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:45 +0000

    Add package benzole/0.0.2/benzole-0.0.2.tar.gz

---
+++ packages/benzole/benzole-0.0.2.tar.gz/benzole-0.0.2/README.md
@@ -0,0 +1,3 @@
+<p align="center"><img src="benzole_logo.svg" width="200px"/></p>
+
+# benzole
---
+++ packages/benzole/benzole-0.0.2.tar.gz/benzole-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from pkg_resources import Requirement
+import setuptools
+from myp import MYPReader
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+myp = MYPReader()
+
+requirements = myp.get_dependencies()
+
+setuptools.setup(
+    name=myp.get_data("name"),
+    version=myp.get_data("version"),
+    author=myp.get_data("author"),
+    author_email=myp.get_data("author_email"),
+    description=myp.get_data("description"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=myp.get_data("url"),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Natural Language :: English"
+    ],
+    install_requires=requirements,
+    setup_requires=requirements,
+    python_requires='>=3.6'
+)
