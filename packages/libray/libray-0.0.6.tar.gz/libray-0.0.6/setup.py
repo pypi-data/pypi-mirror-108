b34354acb4028cb47514ffcd67e24a9d8a95adc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:43 +0000

    Add package libray/0.0.6/libray-0.0.6.tar.gz

---
+++ packages/libray/libray-0.0.6.tar.gz/libray-0.0.6/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+setup(
+  name="libray",
+  version="0.0.6",
+  description='A Libre (FLOSS) Python application for unencrypting, extracting, repackaging, and encrypting PS3 ISOs',
+  author="Nichlas Severinsen",
+  author_email="ns@nsz.no",
+  url="https://notabug.org/necklace/libray",
+  packages=['libray'],
+  scripts=['libray/libray'],
+  install_requires=[
+    'tqdm==4.32.2',
+    'pycryptodome==3.9.8',
+    'requests==2.22.0',
+    'beautifulsoup4==4.7.1',
+  ],
+  include_package_data=True,
+  package_data={'': ['data/keys.db']},
+)
