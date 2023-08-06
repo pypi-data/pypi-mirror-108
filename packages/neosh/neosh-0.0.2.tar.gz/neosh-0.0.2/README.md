c0e5005137c1cf3bbf45c474dcc0eeff491a33d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:20 +0000

    Add package neosh/0.0.2/neosh-0.0.2.tar.gz

---
+++ packages/neosh/neosh-0.0.2.tar.gz/neosh-0.0.2/README.md
@@ -0,0 +1,5 @@
+# Neo SHell emulator (NEOSH)
+
+It is an attempt to try making a shell in python. Has autocomplete as well as a fancy new theme. If there are any issues please add it in the issues section. 
+
+First do pip3 install -r requirements.txt then do python3 main.py
---
+++ packages/neosh/neosh-0.0.2.tar.gz/neosh-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import pathlib
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name = "neosh",
+    packages = ["neosh", "neosh.neosh", "neosh.themes"],
+    version = "0.0.2",
+    description = "Yet Another SHell written in python",
+    long_description = README,
+    long_description_content_type = "text/markdown",
+    url="https://github.com/neodrags/yash-shell",
+    author="Prateek Kesavarapu",
+    author_email="kesavarapu.prateek@gmail.com",
+    license="MIT",
+    include_package_data = True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=["prompt-toolkit", "pygments"],
+    entry_points = {
+        "console_scripts": [
+            "neosh = neosh.neosh.shell:shell"
+        ]
+    }
+)
