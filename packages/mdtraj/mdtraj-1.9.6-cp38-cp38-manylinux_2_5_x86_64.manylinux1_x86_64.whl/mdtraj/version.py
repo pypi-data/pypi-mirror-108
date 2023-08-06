0105fe577ae42a9c661cebbf8533f9b8dfe574c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:43 +0000

    Add package mdtraj/1.9.6/mdtraj-1.9.6-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/mdtraj/mdtraj-1.9.6-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.whl/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.9.6'
+short_version = '1.9.6'
+full_version = '1.9.6'
+git_revision = 'e3007013e97fda40a7675a9ed26676ba59b8f00a'
+release = True
