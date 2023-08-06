9789a16604f6a666282262e4daa6fa9aac2c6522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:57 +0000

    Add package ducts/0.0.32/ducts-0.0.32-py2.py3-none-any.whl

---
+++ packages/ducts/ducts-0.0.32-py2.py3-none-any.whl/ducts/handler/evt_0510_blobs_group_list.py
@@ -0,0 +1,31 @@
+from ducts.spi import EventHandler, webapi
+
+from io import BytesIO
+from hashlib import md5
+from datetime import datetime
+
+from aiohttp import web
+
+
+import logging
+logger = logging.getLogger(__name__)
+
+class Handler(EventHandler):
+
+    def __init__(self):
+        super().__init__()
+
+    def setup(self, handler_spec, manager):
+        self.manager = manager
+        self.helper = manager.load_helper_module('helper_blobs')
+        handler_spec.set_description('Get Value from Redis Server')
+        return handler_spec
+
+    async def handle(self, event):
+        return await self.list_groups()
+    
+    async def list_groups(self):
+        groups = await self.manager.redis.execute_str('ZRANGEBYLEX', self.helper.zset_key_for_group_names(), '-', '+')
+        return list( (vl[-1], '::'.join(vl[0:-1])) for vl in (v.split('::') for v in groups) )
+
+    
---
+++ packages/ducts/ducts-0.0.32-py2.py3-none-any.whl/ducts/handler/evt_0511_blobs_group_versions.py
@@ -0,0 +1,42 @@
+from ducts.spi import EventHandler, webapi
+
+from io import BytesIO
+from hashlib import md5
+from datetime import datetime
+
+from aiohttp import web
+
+
+import logging
+logger = logging.getLogger(__name__)
+
+class Handler(EventHandler):
+
+    def __init__(self):
+        super().__init__()
+
+    def setup(self, handler_spec, manager):
+        self.manager = manager
+        self.helper = manager.load_helper_module('helper_blobs')
+        handler_spec.set_description('Get Value from Redis Server')
+        return handler_spec
+
+    async def handle(self, event):
+        call = self.list_versions
+        if isinstance(event.data, str):
+            return await call(event.data)
+        elif isinstance(event.data, list):
+            return await call(*event.data)
+        elif isinstance(event.data, map):
+            return await call(**event.data)
+        elif event.data:
+            raise ValueError('invalid argument. data=[{}]'.format(event.data))
+        else:
+            raise ValueError('group_key is required.')
+        
+    async def list_versions(self, group_key : str, version : str = '+', count : int = 10) -> list:
+        ret = await self.manager.redis.xrevrange_str(
+            self.helper.stream_key_for_group_metadata(group_key)
+            , start = version, stop = '-', count = count)
+        return list(ret)
+        
