37e7697b5114edbe2ecf208a8f03bfb18e0504ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:23 +0000

    Add package cbmshell/1.0/cbmshell-1.0-py3-none-any.whl

---
+++ packages/cbmshell/cbmshell-1.0-py3-none-any.whl/cbm_shell/disk_image.py
@@ -0,0 +1,36 @@
+from d64 import DiskImage as D64DiskImage
+
+from cbm_shell.disk_image_path import DiskImagePath
+
+
+class DiskImage:
+    @staticmethod
+    def is_valid_image(filepath):
+        return D64DiskImage.is_valid_image(filepath)
+
+    def __init__(self, path, mode, drive):
+        self.drive = drive
+        self._image = D64DiskImage(path).open(mode)
+
+    @property
+    def writeable(self):
+        return self._image.writeable
+
+    def close(self):
+        self._image.close()
+
+    def path(self, name):
+        return DiskImagePath(self.drive, self._image.path(name))
+
+    def glob(self, name):
+        for path in self._image.glob(name):
+            yield DiskImagePath(self.drive, path)
+
+    def expand(self, name):
+        yield from self.glob(name+b'*')
+
+    def directory(self, **kwargs):
+        yield from self._image.directory(**kwargs)
+
+    def __str__(self):
+        return self._image.__str__()
