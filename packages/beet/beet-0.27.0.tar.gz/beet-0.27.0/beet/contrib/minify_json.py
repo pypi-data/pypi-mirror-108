e0206c664f0c6551eddd173eebbd96dc1bb621dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:55 +0000

    Add package beet/0.27.0/beet-0.27.0.tar.gz

---
+++ packages/beet/beet-0.27.0.tar.gz/beet-0.27.0/beet/contrib/minify_json.py
@@ -0,0 +1,26 @@
+"""Plugin that minifies json files."""
+
+
+import json
+from typing import Any, Iterator, Union
+
+from beet import Context, DataPack, JsonFileBase, ResourcePack
+
+
+def beet_default(ctx: Context):
+    for pack in ctx.packs:
+        for json_file in find_json_files(pack):
+            json_file.text = json.dumps(json_file.data, separators=(",", ":"))
+
+
+def find_json_files(pack: Union[ResourcePack, DataPack]) -> Iterator[JsonFileBase[Any]]:
+    for extra_file in pack.extra.values():
+        if isinstance(extra_file, JsonFileBase):
+            yield extra_file
+    for namespace in pack.values():
+        for extra_file in namespace.extra.values():
+            if isinstance(extra_file, JsonFileBase):
+                yield extra_file
+        for file_type, container in namespace.items():
+            if issubclass(file_type, JsonFileBase):  # type: ignore
+                yield from container.values()  # type: ignore
