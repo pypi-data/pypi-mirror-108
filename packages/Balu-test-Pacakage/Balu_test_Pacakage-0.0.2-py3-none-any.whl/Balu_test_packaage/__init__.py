00f8f7c1e8f74e3a0ede31ce3a108818e143a6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:06 +0000

    Add package Balu-test-Pacakage/0.0.2/Balu_test_Pacakage-0.0.2-py3-none-any.whl

---
+++ packages/Balu-test-Pacakage/Balu_test_Pacakage-0.0.2-py3-none-any.whl/Balu_test_packaage/__init__.py
@@ -0,0 +1,3 @@
+from .airthmetic import balu_add, balu_sub
+from .sub_pkg.check import check_number_equal
+
---
+++ packages/Balu-test-Pacakage/Balu_test_Pacakage-0.0.2-py3-none-any.whl/Balu_test_packaage/sub_pkg/check.py
@@ -0,0 +1,5 @@
+def check_number_equal(a, b):
+    if a == b:
+        print("yes,m they are equal")
+    else:
+        print("NO")
