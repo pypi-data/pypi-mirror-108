5b6aa05a86e6bd8df4789f4c34aaa9a92a6f49f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:03 +0000

    Add package linkhub/1.3.1/linkhub-1.3.1.tar.gz

---
+++ packages/linkhub/linkhub-1.3.1.tar.gz/linkhub-1.3.1/linkhub/__init__.py
@@ -0,0 +1,21 @@
+__version__ = '1.3.1'
+Version = __version__  # for backward compatibility
+__all__ = ["Token","LinkhubException"]
+from .linkhub import Token, LinkhubException
+
+TokenInstance = Token()
+
+def generateToken(LinkID, SecretKey, ServiceID, AccessID, Scope, forwardIP = None, UseStaticIP=False, UseLocalTimeYN=True):
+    return TokenInstance.get(LinkID, SecretKey, ServiceID, AccessID, Scope, forwardIP, UseStaticIP, UseLocalTimeYN)
+
+def getBalance(_Token, UseStaticIP=False):
+    return TokenInstance.balance(_Token, UseStaticIP)
+
+def getPartnerBalance(_Token, UseStaticIP=False):
+    return TokenInstance.partnerBalance(_Token, UseStaticIP)
+
+def getPartnerURL(_Token, TOGO, UseStaticIP=False):
+    return TokenInstance.getPartnerURL(_Token, TOGO, UseStaticIP)
+
+def getTime(UseStaticIP=False, UseLocalTimeYN=True):
+    return TokenInstance.getTime(UseStaticIP, UseLocalTimeYN)
