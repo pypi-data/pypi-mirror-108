bb9db11f7429c9846feed5d8bd16a0951970dbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:13 +0000

    Add package jhpythondev/0.1/jhpythondev-0.1.tar.gz

---
+++ packages/jhpythondev/jhpythondev-0.1.tar.gz/jhpythondev-0.1/setup.py
@@ -0,0 +1,11 @@
+ #!/usr/bin/env python
+
+from setuptools import setup
+setup(name='jhpythondev',
+version='0.1',
+description='jhzq python package',
+author='jhzq',
+author_email='70134917@qq.com',
+url='http://www.jhzq.com.cn/',
+packages=['jhpythondev', 'jhpythondev.adv'],
+)
