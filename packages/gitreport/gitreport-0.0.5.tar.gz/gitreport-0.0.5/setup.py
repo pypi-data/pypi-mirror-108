67267e74ed903589851a163dd47aba8ecb3e830b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:02 +0000

    Add package gitreport/0.0.5/gitreport-0.0.5.tar.gz

---
+++ packages/gitreport/gitreport-0.0.5.tar.gz/gitreport-0.0.5/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name="gitreport",
+    version="0.0.5",
+    author="monopeelz",
+    author_email="monopeelz@gmail.com",
+    description="Productivity git report to avoid stupid job in every month",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/peelz/git-report",
+    project_urls={
+        "Bug Tracker": "https://github.com/peelz/git-report/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'gitreport = src.cli:main'
+        ]
+    },
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.6",
+    zip_safe=False
+)
---
+++ packages/gitreport/gitreport-0.0.5.tar.gz/gitreport-0.0.5/src/cli.py
@@ -0,0 +1,59 @@
+import argparse
+import os.path
+from datetime import timedelta, date
+
+import git
+import xlsxwriter
+
+basedir = os.path.dirname(__file__)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--projects", "-p", nargs="+", help="project dir can be list", required=True)
+    parser.add_argument("--author", "-a", help="author pattern name use for filter", required=True)
+    parser.add_argument("--month", "-m", type=int, help="number month of year", required=True)
+    parser.add_argument("--output-file", "-o", help="export file name", required=True)
+    args = parser.parse_args()
+
+    # f = open(os.path.join(basedir, "test.txt"), 'w')
+    records = {}
+    since = date(2021, args.month, 1)
+    until = date(since.year + since.month // 12, since.month % 12 + 1, 1) - timedelta(days=1)
+    limit = (f"--since={since}", f"--until={until}")
+
+    for i in range(until.day):
+        dt = date(2021, args.month, 1).replace(day=i + 1)
+        records[dt.strftime("%Y-%m-%d")] = ""
+    # prepare data
+    for project in args.projects:
+        repo = git.Git(project)
+        result = repo.log(f"--author={args.author}", "--format=%ai|%an|%s", "--all", *limit)
+        lines = result.split("\n")
+        for line in lines:
+            try:
+                _date, name, activity = line.split("|")
+                _date = _date.split(" ")[0]
+                if records.get(_date):
+                    records[_date] = records.get(_date) + "\n" + activity
+                else:
+                    records[_date] = activity
+            except:
+                pass
+
+    # Create an new Excel file and add a worksheet.
+    workbook = xlsxwriter.Workbook(args.output_file)
+    worksheet = workbook.add_worksheet()
+
+    # Widen the first column to make the text clearer.
+    worksheet.set_column('A:A', 10)
+    worksheet.set_column('B:B', 50)
+    # Add a bold format to use to highlight cells.
+    worksheet.write(0, 0, "Date")
+    worksheet.write(0, 1, "Activity")
+    for i, (k, v) in enumerate(records.items()):
+        worksheet.write(i + 1, 0, k)
+        worksheet.write(i + 1, 1, v)
+    workbook.close()
+
+    print("Export success to", args.output_file)
