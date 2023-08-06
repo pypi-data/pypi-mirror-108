f295c175654a154ec704b9710be0378b37e947ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:42 +0000

    Add package saruca-uuv/0.0.8/saruca_uuv-0.0.8-py3-none-any.whl

---
+++ packages/saruca-uuv/saruca_uuv-0.0.8-py3-none-any.whl/saruca_uuv/comprehend_system_layer/Methods/localization.py
@@ -0,0 +1,13 @@
+# localization
+
+from saruca_uuv.control_system_layer.Methods.assignment import assignment
+from saruca_uuv.control_system_layer.Methods.assignment import EntityLayer
+
+vehicleLocation = assignment()
+
+
+def localization():
+    if EntityLayer.getPositionVector() == vehicleLocation:
+        return 1
+    else:
+        return 0
