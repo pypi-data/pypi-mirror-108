7bef2417ee5cc9dcf789ab39ca4d7f789c52363b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:53 +0000

    Add package cs-models/0.0.204/cs_models-0.0.204-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.204-py3-none-any.whl/cs_models/resources/HLTTermTA/models.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    UniqueConstraint,
+    ForeignKey,
+)
+
+from ...database import Base
+
+
+class HLTTermTAModel(Base):
+    __tablename__ = "hlt_ta"
+
+    id = Column(Integer, primary_key=True)
+    hlt_id = Column(
+        Integer,
+        ForeignKey('hlt_terms.id'),
+        nullable=False,
+    )
+    therapeutic_area_id = Column(
+        Integer,
+        ForeignKey('therapeutic_areas.id'),
+        nullable=False,
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
+
+    __table_args__ = (UniqueConstraint("hlt_id", "therapeutic_area_id"),)
