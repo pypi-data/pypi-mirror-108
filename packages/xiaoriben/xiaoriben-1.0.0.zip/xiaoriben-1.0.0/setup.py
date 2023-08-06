118aa6c7c96b8a72f6b3461f01cadf3e6412db10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:40 +0000

    Add package xiaoriben/1.0.0/xiaoriben-1.0.0.zip

---
+++ packages/xiaoriben/xiaoriben-1.0.0.zip/xiaoriben-1.0.0/setup.py
@@ -0,0 +1,14 @@
+# 项目信息的配置文件
+# 这里面最重要一个就是执行setup函数，通过这个函数来指明信息
+# from distutils.core import setup
+from setuptools import setup
+
+
+def readme_file():
+    with open('README.rst', encoding='utf-8') as rf:
+        return rf.read()
+
+
+setup(name='xiaoriben', version='1.0.0', description='this is a niubi lib',
+      packages=['sztestlib'], py_modules=['dos'], author='Sz', author_email='651757088@qq.com',
+      long_description=readme_file(), url='http://upload.pypi.org/legacy/', license='MIT')
---
+++ packages/xiaoriben/xiaoriben-1.0.0.zip/xiaoriben-1.0.0/sztestlib/szlibone.py
@@ -0,0 +1,5 @@
+# 新手测试  拿铁
+import sys
+
+def run():
+    print('sz lib test  --- run')
