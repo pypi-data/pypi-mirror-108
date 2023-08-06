628c4b4551e9a194a912bbb8a650e37273f14944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:32 +0000

    Add package fdn/2021.6.5.2248/fdn-2021.6.5.2248-py2.py3-none-any.whl

---
+++ packages/fdn/fdn-2021.6.5.2248-py2.py3-none-any.whl/tests/test_fnx.py
@@ -0,0 +1,62 @@
+# def test_create_name_log():
+#     assert False
+#
+#
+# def test_is_hidden():
+#     assert False
+#
+#
+# def test_mask_RUW():
+#     assert False
+#
+#
+# def test_replace_char():
+#     assert False
+#
+#
+# def test_process_head_tail_sep_char():
+#     assert False
+#
+#
+# def test_process_head_tail():
+#     assert False
+#
+#
+# def test_process_white_space():
+#     assert False
+#
+#
+# def test_check_starts_with_terminology():
+#     assert False
+#
+#
+# def test_process_terminology():
+#     assert False
+#
+#
+# def test_process_word():
+#     assert False
+#
+#
+# def test_depth_walk():
+#     assert False
+#
+#
+# def test_rich_style():
+#     assert False
+#
+#
+# def test_one_file_ufn():
+#     assert False
+#
+#
+# def test_one_dir_ufn():
+#     assert False
+#
+#
+# def test_ufn():
+#     assert False
+#
+#
+# def test_file_name_log():
+#     assert False
