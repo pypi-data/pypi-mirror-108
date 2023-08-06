b3376b7d653158b33e61b7d2c5b6891191b4ec41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:47 +0000

    Add package slai-cli/0.3.6/slai_cli-0.3.6.tar.gz

---
+++ packages/slai-cli/slai_cli-0.3.6.tar.gz/slai_cli-0.3.6/slai_cli/errors.py
@@ -0,0 +1,12 @@
+from slai_cli import log
+
+ERROR_MAP = {
+    "existing_deployments_processing": "There are existing deployments processing, please try again in a few minutes.",  # noqa
+    "no_available_model_servers": "There are currently no available model servers, please try again in a few minutes.",  # noqa
+}
+
+
+def handle_error(*, error_msg):
+    log.warn(
+        f"ERROR: {ERROR_MAP.get(error_msg, f'Unknown error occured: {error_msg}')}"
+    )
