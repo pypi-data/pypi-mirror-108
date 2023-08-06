8667dce97d51ddcc8a000dc6fd42eea5104e0f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:45 +0000

    Add package antipetros_discordbot/2.0.0/antipetros_discordbot-2.0.0.tar.gz

---
+++ packages/antipetros_discordbot/antipetros_discordbot-2.0.0.tar.gz/antipetros_discordbot-2.0.0/antipetros_discordbot/abstracts/connect_signal.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+import asyncio
+from typing import Callable
+
+
+class AbstractConnectSignal(ABC):
+
+    def __init__(self) -> None:
+        self.targets = []
+
+    def connect(self, target: Callable):
+        self.targets.append(target)
+        self.targets = list(set(self.targets))
+
+    @abstractmethod
+    async def emit(self, *args, **kwargs):
+        # IDEA maybe as asyncio.task
+        await self._emit_to_targets(*args, **kwargs)
+
+    async def _emit_to_targets(self, *args, **kwargs):
+        for target in self.targets:
+            if asyncio.iscoroutinefunction(target):
+                await target(*args, **kwargs)
+            else:
+                target(*args, **kwargs)
