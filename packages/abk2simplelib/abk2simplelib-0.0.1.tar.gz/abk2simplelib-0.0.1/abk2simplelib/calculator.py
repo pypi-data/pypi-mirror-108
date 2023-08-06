a407c7440407471a7e90626772f48075a3d0a4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:16 +0000

    Add package abk2simplelib/0.0.1/abk2simplelib-0.0.1.tar.gz

---
+++ packages/abk2simplelib/abk2simplelib-0.0.1.tar.gz/abk2simplelib-0.0.1/abk2simplelib/calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+	def __init__(self, accurate=True):
+		self.accurate = accurate
+
+	def add_numbers(self, num1, num2):
+		return num1 + num2
+
+	def subtract_numbers(self, num1, num2):
+		return num1 - num2
+
+	def multiply_numbers(self, num1, num2):
+		return num1 * num2
+
+	def divide_numbers(self, num1, num2):
+		return num1 / num2
+
---
+++ packages/abk2simplelib/abk2simplelib-0.0.1.tar.gz/abk2simplelib-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='abk2simplelib',
+	version='0.0.1',
+	description='A simple calculator',
+	author='Andrew King',
+	author_email='andrewking1597@gmail.com',
+	url='',
+	packages=['abk2simplelib'])
