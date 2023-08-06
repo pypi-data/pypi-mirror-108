80066178e002c59b495e8b8eb7a0166f71f0b9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:03 +0000

    Add package Balu-test-Pacakage/0.0.1/Balu%20test%20Pacakage-0.0.1.tar.gz

---
+++ packages/Balu-test-Pacakage/Balu%20test%20Pacakage-0.0.1.tar.gz/Balu test Pacakage-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+setup(
+    name="Balu test Pacakage",
+    version="0.0.1",
+    author="Balaji Chippada",
+    author_email="BALAJI.CHIPPADA@t-systems.com",
+    description=("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license="BSD",
+    keywords="example documentation tutorial",
+    url="http://packages.python.org/an_example_pypi_project",
+    packages=['Balu_test_packaage'],
+    long_description="",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
+
