dec4b96b0be3dc7013dd54402d38ef7e1bc8661d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:44 +0000

    Add package seagrass/0.2.1/seagrass-0.2.1-py3-none-any.whl

---
+++ packages/seagrass/seagrass-0.2.1-py3-none-any.whl/test/hooks/test_timer_hook.py
@@ -0,0 +1,35 @@
+# Tests for the TimerHook auditing hook
+
+import time
+import unittest
+from test.base import HookTestCaseBase
+from seagrass.hooks import TimerHook
+
+
+class TimerHookTestCase(HookTestCaseBase):
+
+    hook_class = TimerHook
+
+    def test_hook_function(self):
+        ausleep = self.auditor.wrap(time.sleep, "test.time.sleep", hooks=[self.hook])
+
+        ausleep(0.01)
+        with self.auditor.audit():
+            ausleep(0.01)
+        ausleep(0.01)
+
+        recorded_time = self.hook.event_times["test.time.sleep"]
+        self.assertAlmostEqual(recorded_time, 0.01, delta=0.005)
+
+        # Check logging output
+        self.auditor.log_results()
+        self.logging_output.seek(0)
+        output = [line.rstrip() for line in self.logging_output.readlines()]
+        self.assertEqual(output[0], "(INFO) TimerHook results:")
+        self.assertEqual(
+            output[1], "(INFO)     Time spent in test.time.sleep: %f" % recorded_time
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
