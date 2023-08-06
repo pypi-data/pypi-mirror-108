4ca298bd189092b853189a78e6f97288c0b92f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:09 +0000

    Add package ijjj-distributions/0.1/ijjj_distributions-0.1.tar.gz

---
+++ packages/ijjj-distributions/ijjj_distributions-0.1.tar.gz/ijjj_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ijjj_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ijjj_distributions'],
+      zip_safe=False)
