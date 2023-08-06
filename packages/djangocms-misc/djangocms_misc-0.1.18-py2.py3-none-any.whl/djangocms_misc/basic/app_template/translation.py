c12c7b145e47a7be19dc791014951052f47adcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:03 +0000

    Add package djangocms-misc/0.1.18/djangocms_misc-0.1.18-py2.py3-none-any.whl

---
+++ packages/djangocms-misc/djangocms_misc-0.1.18-py2.py3-none-any.whl/djangocms_misc/basic/app_template/translation.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from modeltranslation.translator import TranslationOptions, translator
+
+from djangocms_baseplugins.baseplugin import defaults
+from djangocms_baseplugins.baseplugin.utils import check_in_migration_modules
+
+from .models import PluginTemplate
+from . import conf
+
+
+translation_fields = defaults.TRANSLATED_FIELDS \
+                     + conf.TRANSLATED_FIELDS
+
+
+class AppTemplateTranslationOptions(TranslationOptions):
+    fields = translation_fields
