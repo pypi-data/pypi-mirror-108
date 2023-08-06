6d45b4c2acac2724e4f390909e9b2f6a0fd000e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:41 +0000

    Add package kindle/0.3.2/kindle-0.3.2.tar.gz

---
+++ packages/kindle/kindle-0.3.2.tar.gz/kindle-0.3.2/kindle/modules/__init__.py
@@ -0,0 +1,21 @@
+"""PyTorch Modules."""
+
+from kindle.modules.activation import Activation
+from kindle.modules.add import Add
+from kindle.modules.bottleneck import Bottleneck
+from kindle.modules.concat import Concat
+from kindle.modules.conv import Conv
+from kindle.modules.dwconv import DWConv
+from kindle.modules.linear import Linear
+from kindle.modules.poolings import GlobalAvgPool
+
+__all__ = [
+    "Add",
+    "Bottleneck",
+    "Concat",
+    "Conv",
+    "DWConv",
+    "Linear",
+    "GlobalAvgPool",
+    "Activation",
+]
