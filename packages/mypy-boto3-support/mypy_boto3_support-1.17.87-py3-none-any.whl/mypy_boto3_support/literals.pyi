c6a8bf0574cc558139ea722f1c1f6c4b6b89c21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:42 +0000

    Add package mypy-boto3-support/1.17.87/mypy_boto3_support-1.17.87-py3-none-any.whl

---
+++ packages/mypy-boto3-support/mypy_boto3_support-1.17.87-py3-none-any.whl/mypy_boto3_support/literals.pyi
@@ -0,0 +1,24 @@
+"""
+Type annotations for support service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_support.literals import DescribeCasesPaginatorName
+
+    data: DescribeCasesPaginatorName = "describe_cases"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("DescribeCasesPaginatorName", "DescribeCommunicationsPaginatorName")
+
+DescribeCasesPaginatorName = Literal["describe_cases"]
+DescribeCommunicationsPaginatorName = Literal["describe_communications"]
