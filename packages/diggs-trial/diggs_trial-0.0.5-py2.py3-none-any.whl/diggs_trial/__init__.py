f05bb88dd66b791f81bde197772b33295a5ef651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:58 +0000

    Add package diggs-trial/0.0.5/diggs_trial-0.0.5-py2.py3-none-any.whl

---
+++ packages/diggs-trial/diggs_trial-0.0.5-py2.py3-none-any.whl/diggs_trial/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for diggs-trial."""
+
+__author__ = """Xin Peng"""
+__email__ = 'xin_peng@outlook.com'
+__version__ = '0.0.5'
+
+from .diggs_trial import *
---
+++ packages/diggs-trial/diggs_trial-0.0.5-py2.py3-none-any.whl/diggs_trial/cli.py
@@ -0,0 +1,15 @@
+"""Console script for diggs_trial."""
+import argparse
+import sys
+from diggs_trial import validation
+from rich import print as rprint
+
+
+def main():
+
+    """Console script for py-diggs."""
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
