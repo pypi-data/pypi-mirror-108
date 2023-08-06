96a8ce3a12b4c4fff961b9edf11ab3a7c95f7f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:24 +0000

    Add package livemark/0.2.0/livemark-0.2.0.tar.gz

---
+++ packages/livemark/livemark-0.2.0.tar.gz/livemark-0.2.0/livemark/renderer.py
@@ -0,0 +1,63 @@
+import bs4
+import json
+import yaml
+import marko
+from jinja2 import Template
+from marko.ext.gfm import GFM
+from marko.html_renderer import HTMLRenderer
+from frictionless import Resource, Detector
+from . import config
+
+
+class LivemarkRendererMixin(HTMLRenderer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__tables = 0
+        self.__charts = 0
+
+    # Render
+
+    def render_html_block(self, element):
+        markdown = marko.Markdown()
+        markdown.use(GFM)
+        markdown.use(LivemarkExtension)
+        html = bs4.BeautifulSoup(element.children, features="html.parser")
+        for node in html.select(".markdown"):
+            if len(node.contents) == 1:
+                text = node.contents[0]
+                if isinstance(text, bs4.element.NavigableString):
+                    text = markdown.convert(text)
+                    inner = bs4.BeautifulSoup(text, features="html.parser")
+                    node.string.replace_with(inner)
+        return str(html)
+
+    def render_fenced_code(self, element):
+        if element.lang == "table":
+            spec_yaml = str(element.children[0].children).strip()
+            spec_python = yaml.safe_load(spec_yaml)
+            spec_python["licenseKey"] = "non-commercial-and-evaluation"
+            detector = Detector(field_float_numbers=True)
+            resource = Resource(spec_python.get("data", []), detector=detector)
+            header, *lists = resource.to_snap(json=True)
+            spec_python["colHeaders"] = header
+            spec_python["data"] = lists
+            spec = json.dumps(spec_python, ensure_ascii=False)
+            spec = spec.replace("'", "\\'")
+            template = Template(config.TABLE)
+            self.__tables += 1
+            text = template.render(spec=spec, elem=f"livemark-table-{self.__tables}")
+            return text
+        if element.lang == "chart":
+            spec_yaml = str(element.children[0].children).strip()
+            spec_python = yaml.safe_load(spec_yaml)
+            spec = json.dumps(spec_python, ensure_ascii=False)
+            spec = spec.replace("'", "\\'")
+            template = Template(config.CHART)
+            self.__charts += 1
+            text = template.render(spec=spec, elem=f"livemark-chart-{self.__charts}")
+            return text
+        return super().render_fenced_code(element)
+
+
+class LivemarkExtension:
+    renderer_mixins = [LivemarkRendererMixin]
