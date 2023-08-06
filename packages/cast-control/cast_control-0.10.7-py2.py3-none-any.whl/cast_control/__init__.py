25bd9cedeb6513fca944c2ee334564ab6c93934b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:49 +0000

    Add package cast-control/0.10.7/cast_control-0.10.7-py2.py3-none-any.whl

---
+++ packages/cast-control/cast_control-0.10.7-py2.py3-none-any.whl/cast_control/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.10.7'
+__author__ = 'Alex DeLorenzo <alex@alexdelorenzo.dev>'
+
+
+HOMEPAGE: str = "https://github.com/alexdelorenzo/cast_control"
+LICENSE: str = 'AGPL-3.0'
+NAME: str = 'cast_control'
+SHORT_NAME: str = 'castctl'
+DESCRIPTION: str = '📺 Control Chromecasts from Linux and D-Bus'
+
+ENTRYPOINT_NAME: str = 'cli'
